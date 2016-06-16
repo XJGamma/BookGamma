7ad003fb759a843c2bf037c15fdf8059d9fbe8f5
DUAN Yufei <duanyufeichn@gmail.com>
2016-06-16 12:54:45 +0800

    fix: DBHelperTest

--- app/src/androidTest/java/cn/edu/xjtu/se/dao/DBHelperTest.java
+++ app/src/androidTest/java/cn/edu/xjtu/se/dao/DBHelperTest.java
@@ -4,14 +4,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 /**
- * Created by Fay on 2016/6/16.
+ * Created by DUAN Yufei on 2016/6/16.
  */
 public class DBHelperTest extends AndroidTestCase{
 
@@ -21,17 +15,13 @@ public class DBHelperTest extends AndroidTestCase{
     public void setUp() throws Exception {
         super.setUp();
         RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
-        DBHelper xgdbHelper = new DBHelper(context);
-        db = xgdbHelper.getWritableDatabase();
+        DBHelper dbHelper = new DBHelper(context);
+        db = dbHelper.getWritableDatabase();
     }
 
     @Override
     public void tearDown() throws Exception {
         db.close();
         super.tearDown();
-    }
-
-    //@Test
-    public void testAddEntry(){
     }
 }
