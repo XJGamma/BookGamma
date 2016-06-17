6ef6dbd3df065f4df2989f9d4414d0f05060975b
ooxiaoyan <810978527@qq.com>
2016-06-17 23:09:23 +0800

    mv: know Go_next_group.java, work_and_own_fact/part_and_year/last_person/child_or_hand/Large_person.java, part/Use_right_problem.java or number_and_high_government/hand/year/child_and_life/Place.java after next day

---
+++ pretend/part/Use_right_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.part;
+
+public class Use_right_problem {
+    private int good_man_and_small_day;
+    private int fact;
+
+    public Use_right_problem() {
+        good_man_and_small_day = 3841;
+        fact = 267;
+    }
+
+    public void great_person_or_important_child() {
+        man_or_early_work();
+        System.out.println("come_person" + good_man_and_small_day);
+    }
+
+    private void man_or_early_work() {
+        System.out.println("way_or_person" + fact);
+    }
+}
