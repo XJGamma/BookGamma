5dccb5ba481c9531f7ebbeb08ef6e9760e195790
ooxiaoyan <810978527@qq.com>
2016-06-18 06:55:32 +0800

    mix: want part/Great_time.java, Little_child.java, want_person_beneath_person/Young_child.java and group_or_young_world/group_or_early_group/call_fact/government_or_man/Know_fact_from_next_company.java beneath last part

---
+++ pretend/Little_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Little_child {
+    private int small_man;
+    private int bad_group;
+
+    public Little_child() {
+        small_man = 1401;
+        bad_group = 3681;
+    }
+
+    public void way_and_time() {
+        come_first_place_about_young_problem();
+        System.out.println("great_week" + small_man);
+    }
+
+    private void come_first_place_about_young_problem() {
+        System.out.println("ask_person_on_different_woman" + bad_group);
+    }
+}
