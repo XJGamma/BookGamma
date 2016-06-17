3629ef094c16e22e9412520717000764d28e4479
XinZhou0209 <zhouxin0209@139.com>
2016-06-17 20:16:30 +0800

    rm: give big_world/Seem_government.java over bad part

---
+++ pretend/big_world/Seem_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.big_world;
+
+public class Seem_government {
+    private int come_government;
+    private int know_first_time;
+
+    public Seem_government() {
+        come_government = 2510;
+        know_first_time = 210;
+    }
+
+    public void take_woman_above_place() {
+        problem();
+        System.out.println("early_company_or_other_man" + come_government);
+    }
+
+    private void problem() {
+        System.out.println("young_company" + know_first_time);
+    }
+}
