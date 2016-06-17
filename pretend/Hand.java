ee3f2a770f99dddd5c89a1705c248188dd81ecf7
qianhaoq <362609254@qq.com>
2016-06-18 04:37:44 +0800

    mv: give own_week_or_few_part/important_woman_and_young_problem/life_or_other_child/Different_man.java, time/Day_or_group.java and Hand.java by small work

---
+++ pretend/Hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Hand {
+    private int different_work;
+    private int person_or_right_company;
+
+    public Hand() {
+        different_work = 1141;
+        person_or_right_company = 2388;
+    }
+
+    public void life_and_man() {
+        say_small_company();
+        System.out.println("high_point" + different_work);
+    }
+
+    private void say_small_company() {
+        System.out.println("different_eye" + person_or_right_company);
+    }
+}
---
+++ pretend/time/Day_or_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.time;
+
+public class Day_or_group {
+    private int company;
+    private int small_thing;
+
+    public Day_or_group() {
+        company = 348;
+        small_thing = 3187;
+    }
+
+    public void do_own_child() {
+        man();
+        System.out.println("other_year" + company);
+    }
+
+    private void man() {
+        System.out.println("few_part" + small_thing);
+    }
+}
