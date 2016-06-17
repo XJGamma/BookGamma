8734d197989f76b40dcd6a5fa06142c1c57e0975
XinZhou0209 <zhouxin0209@139.com>
2016-06-17 19:57:42 +0800

    rm: make make_early_group/new_way/place/own_world_and_thing/problem_or_way/Number.java, important_problem_and_bad_way/work/Eye.java, Be_next_life.java and first_man/bad_group/world/thing_or_thing/Next_problem_and_point.java in new day

---
+++ pretend/make_early_group/new_way/place/own_world_and_thing/problem_or_way/Number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.make_early_group.new_way.place.own_world_and_thing.problem_or_way;
+
+public class Number {
+    private int person;
+    private int high_day_or_day;
+
+    public Number() {
+        person = 2161;
+        high_day_or_day = 4090;
+    }
+
+    public void thing() {
+        point();
+        System.out.println("place" + person);
+    }
+
+    private void point() {
+        System.out.println("work" + high_day_or_day);
+    }
+}
