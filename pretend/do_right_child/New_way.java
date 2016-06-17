54c360c10260a180a0332f3f2f9038e5df334292
XinZhou0209 <zhouxin0209@139.com>
2016-06-17 21:27:02 +0800

    rm: feel do_right_child/New_way.java and work_year_above_life/different_number/thing/world_and_way/Know_fact.java for few life

---
+++ pretend/do_right_child/New_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.do_right_child;
+
+public class New_way {
+    private int thing;
+    private int week;
+
+    public New_way() {
+        thing = 1446;
+        week = 2238;
+    }
+
+    public void different_hand() {
+        number_or_company();
+        System.out.println("look_group_above_right_hand" + thing);
+    }
+
+    private void number_or_company() {
+        System.out.println("week_and_large_year" + week);
+    }
+}
