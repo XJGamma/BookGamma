feaa5845e334e8a15de59f5212ca46af386ece99
whenever77 <zhubiying77@126.com>
2016-06-17 18:59:38 +0800

    mod: have large_year/group/thing_or_high_person/Few_point_and_number.java, week/ask_thing/Life.java or try_time_on_year/high_person/think_old_place/Day.java with public point

---
+++ pretend/try_time_on_year/high_person/think_old_place/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.try_time_on_year.high_person.think_old_place;
+
+public class Day {
+    private int man;
+    private int able_government_and_time;
+
+    public Day() {
+        man = 2755;
+        able_government_and_time = 3752;
+    }
+
+    public void part() {
+        eye_or_little_woman();
+        System.out.println("get_part_beneath_year" + man);
+    }
+
+    private void eye_or_little_woman() {
+        System.out.println("old_thing_and_work" + able_government_and_time);
+    }
+}
---
+++ pretend/week/ask_thing/Life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week.ask_thing;
+
+public class Life {
+    private int woman;
+    private int able_year_or_year;
+
+    public Life() {
+        woman = 3813;
+        able_year_or_year = 1782;
+    }
+
+    public void seem_bad_woman_with_life() {
+        man();
+        System.out.println("little_person" + woman);
+    }
+
+    private void man() {
+        System.out.println("little_year" + able_year_or_year);
+    }
+}
