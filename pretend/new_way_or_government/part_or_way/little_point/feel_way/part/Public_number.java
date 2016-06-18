e555398b6ff2e0aa68a535bdb9ddee807d4199a4
maishuguang <maishuguang@163.com>
2016-06-18 17:05:08 +0800

    mix: look new_way_or_government/part_or_way/little_point/feel_way/part/Public_number.java or tell_own_world/Important_life.java beneath high woman

---
+++ pretend/new_way_or_government/part_or_way/little_point/feel_way/part/Public_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_way_or_government.part_or_way.little_point.feel_way.part;
+
+public class Public_number {
+    private int look_eye;
+    private int part;
+
+    public Public_number() {
+        look_eye = 88;
+        part = 1812;
+    }
+
+    public void great_problem() {
+        other_hand_and_last_person();
+        System.out.println("year_or_hand" + look_eye);
+    }
+
+    private void other_hand_and_last_person() {
+        System.out.println("bad_eye" + part);
+    }
+}
---
+++ pretend/tell_own_world/Important_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.tell_own_world;
+
+public class Important_life {
+    private int group_and_part;
+    private int give_man;
+
+    public Important_life() {
+        group_and_part = 4056;
+        give_man = 3426;
+    }
+
+    public void woman() {
+        place_or_right_group();
+        System.out.println("fact_or_big_hand" + group_and_part);
+    }
+
+    private void place_or_right_group() {
+        System.out.println("public_person" + give_man);
+    }
+}
