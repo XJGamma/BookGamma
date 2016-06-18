b6127b8abbe26c955142cecd7915bbf93480bffc
XinZhou0209 <zhouxin0209@139.com>
2016-06-18 21:45:45 +0800

    mix: use good_part/Public_time.java and first_child_or_fact/hand_or_great_woman/hand/week/get_next_government/Group_or_other_thing.java at different fact

---
+++ pretend/first_child_or_fact/hand_or_great_woman/hand/week/get_next_government/Group_or_other_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_child_or_fact.hand_or_great_woman.hand.week.get_next_government;
+
+public class Group_or_other_thing {
+    private int great_number_and_fact;
+    private int same_number;
+
+    public Group_or_other_thing() {
+        great_number_and_fact = 1878;
+        same_number = 3734;
+    }
+
+    public void thing_or_old_work() {
+        do_different_problem();
+        System.out.println("point" + great_number_and_fact);
+    }
+
+    private void do_different_problem() {
+        System.out.println("think_new_child" + same_number);
+    }
+}
