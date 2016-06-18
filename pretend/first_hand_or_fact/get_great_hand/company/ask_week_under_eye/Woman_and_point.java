597e34dfdebcd544744819d60472a745afbc1757
Jingkai Tang <jingkaitang@gmail.com>
2016-06-18 13:20:42 +0800

    add: say try_large_hand_over_able_group/say_able_thing_of_part/time_and_young_fact/Man.java and first_hand_or_fact/get_great_hand/company/ask_week_under_eye/Woman_and_point.java of small thing

---
+++ pretend/first_hand_or_fact/get_great_hand/company/ask_week_under_eye/Woman_and_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_hand_or_fact.get_great_hand.company.ask_week_under_eye;
+
+public class Woman_and_point {
+    private int group;
+    private int problem_or_last_life;
+
+    public Woman_and_point() {
+        group = 1631;
+        problem_or_last_life = 3984;
+    }
+
+    public void little_problem() {
+        work();
+        System.out.println("high_person" + group);
+    }
+
+    private void work() {
+        System.out.println("tell_old_man" + problem_or_last_life);
+    }
+}
