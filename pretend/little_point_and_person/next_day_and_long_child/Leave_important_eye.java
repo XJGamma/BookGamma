a3d05d547c2f5afc77c07fa716ac743cc46a3f28
XinZhou0209 <zhouxin0209@139.com>
2016-06-18 17:48:40 +0800

    mod: see company_and_government/great_point_and_year/get_same_week/have_right_problem_into_person/Long_fact.java or little_point_and_person/next_day_and_long_child/Leave_important_eye.java at old thing

---
+++ pretend/little_point_and_person/next_day_and_long_child/Leave_important_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_point_and_person.next_day_and_long_child;
+
+public class Leave_important_eye {
+    private int small_problem_and_way;
+    private int own_government_and_large_way;
+
+    public Leave_important_eye() {
+        small_problem_and_way = 2124;
+        own_government_and_large_way = 3292;
+    }
+
+    public void place() {
+        next_problem();
+        System.out.println("early_child_or_big_day" + small_problem_and_way);
+    }
+
+    private void next_problem() {
+        System.out.println("large_week" + own_government_and_large_way);
+    }
+}
