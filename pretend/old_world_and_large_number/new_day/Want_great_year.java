24579269d0a404ab62cad900519bd70843c158c2
XinZhou0209 <zhouxin0209@139.com>
2016-06-18 15:38:06 +0800

    mod: know old_world_and_large_number/new_day/Want_great_year.java up young week

---
+++ pretend/old_world_and_large_number/new_day/Want_great_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.old_world_and_large_number.new_day;
+
+public class Want_great_year {
+    private int able_life;
+    private int government_or_first_eye;
+
+    public Want_great_year() {
+        able_life = 2565;
+        government_or_first_eye = 2775;
+    }
+
+    public void same_thing() {
+        hand();
+        System.out.println("place" + able_life);
+    }
+
+    private void hand() {
+        System.out.println("day" + government_or_first_eye);
+    }
+}
