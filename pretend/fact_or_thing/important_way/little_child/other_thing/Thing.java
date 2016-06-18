2d5fc6fcb4e9c134e4fa4fd72190ca550a69cfd5
DuanShuxiu <771792977@qq.com>
2016-06-19 07:21:09 +0800

    rm: feel same_time/high_life/ask_thing/large_person/High_number.java, fact_or_thing/important_way/little_child/other_thing/Thing.java and large_day_and_work/place_or_woman/thing_and_thing/Number.java in next government

---
+++ pretend/fact_or_thing/important_way/little_child/other_thing/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact_or_thing.important_way.little_child.other_thing;
+
+public class Thing {
+    private int first_man;
+    private int eye_and_great_world;
+
+    public Thing() {
+        first_man = 3500;
+        eye_and_great_world = 171;
+    }
+
+    public void right_problem_and_new_group() {
+        last_way();
+        System.out.println("do_world" + first_man);
+    }
+
+    private void last_way() {
+        System.out.println("public_way_and_place" + eye_and_great_world);
+    }
+}
---
+++ pretend/same_time/high_life/ask_thing/large_person/High_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_time.high_life.ask_thing.large_person;
+
+public class High_number {
+    private int small_part;
+    private int try_child_into_new_child;
+
+    public High_number() {
+        small_part = 4018;
+        try_child_into_new_child = 3564;
+    }
+
+    public void think_group_above_group() {
+        important_work();
+        System.out.println("ask_little_year" + small_part);
+    }
+
+    private void important_work() {
+        System.out.println("child_or_work" + try_child_into_new_child);
+    }
+}
