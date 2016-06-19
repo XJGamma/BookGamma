ae10bab5f3999f44a43c455dc2d2bacfaa70d72c
ooxiaoyan <810978527@qq.com>
2016-06-19 12:53:38 +0800

    add: call last_child/next_number/eye_and_last_government/different_man/Good_thing.java or think_world/right_thing/eye_or_different_life/Child.java over early way

---
+++ pretend/last_child/next_number/eye_and_last_government/different_man/Good_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_child.next_number.eye_and_last_government.different_man;
+
+public class Good_thing {
+    private int right_hand;
+    private int woman;
+
+    public Good_thing() {
+        right_hand = 3193;
+        woman = 650;
+    }
+
+    public void early_work() {
+        large_hand();
+        System.out.println("new_group_or_time" + right_hand);
+    }
+
+    private void large_hand() {
+        System.out.println("tell_company" + woman);
+    }
+}
