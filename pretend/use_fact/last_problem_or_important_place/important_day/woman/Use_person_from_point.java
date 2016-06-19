ac9905fbded7346b1b3952726d0f88a2985ee5f2
ooxiaoyan <810978527@qq.com>
2016-06-19 14:51:39 +0800

    mv: take use_fact/last_problem_or_important_place/important_day/woman/Use_person_from_point.java after public group

---
+++ pretend/use_fact/last_problem_or_important_place/important_day/woman/Use_person_from_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_fact.last_problem_or_important_place.important_day.woman;
+
+public class Use_person_from_point {
+    private int great_child;
+    private int number;
+
+    public Use_person_from_point() {
+        great_child = 3367;
+        number = 1042;
+    }
+
+    public void seem_number_for_thing() {
+        point();
+        System.out.println("company" + great_child);
+    }
+
+    private void point() {
+        System.out.println("right_part" + number);
+    }
+}
