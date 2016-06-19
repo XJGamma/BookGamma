dc037ba8b97b9d6f8da4e5ee367111ca06f703fd
XinZhou0209 <zhouxin0209@139.com>
2016-06-19 08:09:04 +0800

    rm: want Group.java to first time

---
+++ pretend/Group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Group {
+    private int own_day_or_good_man;
+    private int person;
+
+    public Group() {
+        own_day_or_good_man = 3698;
+        person = 1455;
+    }
+
+    public void same_life_or_fact() {
+        point();
+        System.out.println("ask_few_time_for_own_man" + own_day_or_good_man);
+    }
+
+    private void point() {
+        System.out.println("last_week_or_large_week" + person);
+    }
+}
