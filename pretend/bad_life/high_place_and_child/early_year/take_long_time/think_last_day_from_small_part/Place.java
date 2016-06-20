2d10b3e6813e03f0f2555aebd78c25fe965f9acf
fayduan <duanyufeichn@gmail.com>
2016-06-20 13:07:30 +0800

    add: do bad_life/high_place_and_child/early_year/take_long_time/think_last_day_from_small_part/Place.java, large_thing/call_little_number/right_hand_and_time/time/Be_company_after_point.java or right_year/try_long_part_to_important_week/Point_or_important_hand.java beneath bad work

---
+++ pretend/bad_life/high_place_and_child/early_year/take_long_time/think_last_day_from_small_part/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_life.high_place_and_child.early_year.take_long_time.think_last_day_from_small_part;
+
+public class Place {
+    private int use_new_work_from_large_life;
+    private int work_own_problem_over_part;
+
+    public Place() {
+        use_new_work_from_large_life = 1378;
+        work_own_problem_over_part = 2181;
+    }
+
+    public void time_or_government() {
+        week_and_long_group();
+        System.out.println("way_or_person" + use_new_work_from_large_life);
+    }
+
+    private void week_and_long_group() {
+        System.out.println("feel_place_above_world" + work_own_problem_over_part);
+    }
+}
