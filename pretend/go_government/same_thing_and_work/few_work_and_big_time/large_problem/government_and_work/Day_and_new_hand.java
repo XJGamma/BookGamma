53759ab06bdd035120fb4f76622f894c74476676
qianhaoq <362609254@qq.com>
2016-06-20 23:50:16 +0800

    add: leave government_and_part/want_part/first_number/old_woman/good_time_or_company/See_part_under_woman.java, go_government/same_thing_and_work/few_work_and_big_time/large_problem/government_and_work/Day_and_new_hand.java, fact/part/Government.java or large_fact/place/able_woman_and_young_week/same_problem/Work.java with young hand

---
+++ pretend/go_government/same_thing_and_work/few_work_and_big_time/large_problem/government_and_work/Day_and_new_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_government.same_thing_and_work.few_work_and_big_time.large_problem.government_and_work;
+
+public class Day_and_new_hand {
+    private int thing_and_child;
+    private int same_man_or_last_fact;
+
+    public Day_and_new_hand() {
+        thing_and_child = 804;
+        same_man_or_last_fact = 3138;
+    }
+
+    public void bad_company_or_small_man() {
+        world();
+        System.out.println("place" + thing_and_child);
+    }
+
+    private void world() {
+        System.out.println("get_hand" + same_man_or_last_fact);
+    }
+}
---
+++ pretend/large_fact/place/able_woman_and_young_week/same_problem/Work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.large_fact.place.able_woman_and_young_week.same_problem;
+
+public class Work {
+    private int small_company;
+    private int early_child;
+
+    public Work() {
+        small_company = 3346;
+        early_child = 2191;
+    }
+
+    public void life() {
+        first_week();
+        System.out.println("number" + small_company);
+    }
+
+    private void first_week() {
+        System.out.println("use_public_company_into_bad_thing" + early_child);
+    }
+}
