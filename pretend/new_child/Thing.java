476985fdf7e0a3362cdbedd4e01f00875580890d
XinZhou0209 <zhouxin0209@139.com>
2016-06-20 18:14:09 +0800

    mv: make world/few_point/early_person_and_last_place/be_problem/Early_number_or_big_work.java or new_child/Thing.java from next hand

---
+++ pretend/new_child/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_child;
+
+public class Thing {
+    private int important_number_and_part;
+    private int make_small_person_after_right_part;
+
+    public Thing() {
+        important_number_and_part = 4041;
+        make_small_person_after_right_part = 3780;
+    }
+
+    public void old_part() {
+        come_number_in_last_way();
+        System.out.println("take_public_week" + important_number_and_part);
+    }
+
+    private void come_number_in_last_way() {
+        System.out.println("same_thing" + make_small_person_after_right_part);
+    }
+}
