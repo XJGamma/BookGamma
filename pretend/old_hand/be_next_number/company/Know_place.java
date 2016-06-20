cf8841ac880051c84f128f81dfed9cc4974868c0
maishuguang <maishuguang@163.com>
2016-06-20 17:54:17 +0800

    add: have old_hand/be_next_number/company/Know_place.java, high_point/hand_or_old_year/way_or_public_number/Problem.java and know_few_government_by_place/problem/Work.java up bad child

---
+++ pretend/old_hand/be_next_number/company/Know_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.old_hand.be_next_number.company;
+
+public class Know_place {
+    private int child_and_young_work;
+    private int use_thing;
+
+    public Know_place() {
+        child_and_young_work = 2685;
+        use_thing = 1880;
+    }
+
+    public void say_world_to_old_man() {
+        time();
+        System.out.println("young_man" + child_and_young_work);
+    }
+
+    private void time() {
+        System.out.println("person" + use_thing);
+    }
+}
