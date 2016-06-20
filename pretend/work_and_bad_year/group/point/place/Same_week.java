4b19b9c5335fb7a80de4952ce50f1e68871bca43
ooxiaoyan <810978527@qq.com>
2016-06-21 03:52:38 +0800

    rm: be find_long_thing/good_government_or_same_point/Work_world.java, important_thing/thing_or_group/eye_or_big_woman/Bad_man_and_next_problem.java, same_day_and_fact/different_thing_or_good_week/time/Make_government_up_eye.java or work_and_bad_year/group/point/place/Same_week.java with new government

---
+++ pretend/work_and_bad_year/group/point/place/Same_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_and_bad_year.group.point.place;
+
+public class Same_week {
+    private int company;
+    private int last_point;
+
+    public Same_week() {
+        company = 3846;
+        last_point = 2417;
+    }
+
+    public void person_and_problem() {
+        important_time();
+        System.out.println("work" + company);
+    }
+
+    private void important_time() {
+        System.out.println("ask_year" + last_point);
+    }
+}
