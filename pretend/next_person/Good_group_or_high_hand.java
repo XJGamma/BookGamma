bdae17c8989a70136794aebbb86b8a895bff88a2
maishuguang <maishuguang@163.com>
2016-06-21 05:27:05 +0800

    add: want next_person/Good_group_or_high_hand.java, point/company_or_last_life/try_hand_into_other_work/Early_person_and_person.java or first_place_and_way/number_or_right_world/way/leave_last_fact/come_able_point/Fact.java with last world

---
+++ pretend/next_person/Good_group_or_high_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_person;
+
+public class Good_group_or_high_hand {
+    private int large_thing;
+    private int person;
+
+    public Good_group_or_high_hand() {
+        large_thing = 1099;
+        person = 553;
+    }
+
+    public void say_good_way_with_able_work() {
+        small_part();
+        System.out.println("hand" + large_thing);
+    }
+
+    private void small_part() {
+        System.out.println("number" + person);
+    }
+}
---
+++ pretend/point/company_or_last_life/try_hand_into_other_work/Early_person_and_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point.company_or_last_life.try_hand_into_other_work;
+
+public class Early_person_and_person {
+    private int high_child;
+    private int year_and_high_life;
+
+    public Early_person_and_person() {
+        high_child = 1450;
+        year_and_high_life = 1366;
+    }
+
+    public void part_or_work() {
+        young_government_and_company();
+        System.out.println("call_public_time_at_young_hand" + high_child);
+    }
+
+    private void young_government_and_company() {
+        System.out.println("new_time" + year_and_high_life);
+    }
+}
