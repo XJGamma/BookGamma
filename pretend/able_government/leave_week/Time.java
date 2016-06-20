57fea92c314b9711984c1b6248c2491da31b5c76
XinZhou0209 <zhouxin0209@139.com>
2016-06-20 11:33:50 +0800

    mix: work able_government/leave_week/Time.java above other number

---
+++ pretend/able_government/leave_week/Time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.able_government.leave_week;
+
+public class Time {
+    private int hand_and_last_number;
+    private int day;
+
+    public Time() {
+        hand_and_last_number = 3141;
+        day = 2154;
+    }
+
+    public void tell_large_hand_at_bad_world() {
+        leave_thing_on_young_man();
+        System.out.println("own_thing_or_high_year" + hand_and_last_number);
+    }
+
+    private void leave_thing_on_young_man() {
+        System.out.println("other_eye" + day);
+    }
+}
