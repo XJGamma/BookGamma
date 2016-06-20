f24a32b8da1212efce0764e0cf92b3faac6d935d
qianhaoq <362609254@qq.com>
2016-06-20 09:43:25 +0800

    mod: call Fact.java above right life

--- pretend/Fact.java
+++ pretend/Fact.java
@@ -2,6 +2,7 @@
 package cn.edu.xjtu.se.bookgamma.pretend;
 
 public class Fact {
+<<<<<<< 4439986e80d7453498fa215a94660d598fdcc63b
     private int other_person;
     private int fact;
 
@@ -17,5 +18,22 @@ public class Fact {
 
     private void use_number() {
         System.out.println("problem_or_different_part" + fact);
+=======
+    private int few_woman_or_work;
+    private int week;
+
+    public Fact() {
+        few_woman_or_work = 1715;
+        week = 1276;
+    }
+
+    public void part() {
+        old_hand_or_year();
+        System.out.println("take_first_hand_in_other_eye" + few_woman_or_work);
+    }
+
+    private void old_hand_or_year() {
+        System.out.println("able_work_and_woman" + week);
+>>>>>>> mod: call Fact.java above right life
     }
 }
