64b670cd662ab0807494c4e6802e4aa75625ee9a
XinZhou0209 <zhouxin0209@139.com>
2016-06-21 16:04:29 +0800

    rm: take long_woman/small_day_and_place/Other_world.java, do_person_under_little_fact/group_or_large_week/Early_man.java or place/Big_world.java in different part

---
+++ pretend/place/Big_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place;
+
+public class Big_world {
+    private int young_part;
+    private int man;
+
+    public Big_world() {
+        young_part = 1118;
+        man = 1845;
+    }
+
+    public void important_person() {
+        able_thing_or_important_thing();
+        System.out.println("point" + young_part);
+    }
+
+    private void able_thing_or_important_thing() {
+        System.out.println("right_point" + man);
+    }
+}
