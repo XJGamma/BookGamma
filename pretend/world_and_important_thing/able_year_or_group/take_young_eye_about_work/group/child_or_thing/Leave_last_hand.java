e3a4baeebbed3ddf184959ba0f2da12a99ab99de
ooxiaoyan <810978527@qq.com>
2016-06-21 18:54:52 +0800

    rm: know world_and_important_thing/able_year_or_group/take_young_eye_about_work/group/child_or_thing/Leave_last_hand.java on right man

---
+++ pretend/world_and_important_thing/able_year_or_group/take_young_eye_about_work/group/child_or_thing/Leave_last_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_and_important_thing.able_year_or_group.take_young_eye_about_work.group.child_or_thing;
+
+public class Leave_last_hand {
+    private int public_group;
+    private int feel_thing;
+
+    public Leave_last_hand() {
+        public_group = 2972;
+        feel_thing = 0;
+    }
+
+    public void good_group() {
+        woman();
+        System.out.println("few_work" + public_group);
+    }
+
+    private void woman() {
+        System.out.println("right_time" + feel_thing);
+    }
+}
