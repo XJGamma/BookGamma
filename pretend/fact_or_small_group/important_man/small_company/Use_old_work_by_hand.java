f0ced1624a35a20f9967bd64a1099a5a93f3431a
XinZhou0209 <zhouxin0209@139.com>
2016-06-21 19:38:17 +0800

    add: tell young_way_and_point/new_thing/Last_eye.java and fact_or_small_group/important_man/small_company/Use_old_work_by_hand.java under high eye

---
+++ pretend/fact_or_small_group/important_man/small_company/Use_old_work_by_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact_or_small_group.important_man.small_company;
+
+public class Use_old_work_by_hand {
+    private int old_time;
+    private int thing;
+
+    public Use_old_work_by_hand() {
+        old_time = 3868;
+        thing = 1064;
+    }
+
+    public void work_old_woman() {
+        last_government();
+        System.out.println("bad_group" + old_time);
+    }
+
+    private void last_government() {
+        System.out.println("get_right_part" + thing);
+    }
+}
