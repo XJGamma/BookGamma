853519d1568eb0dbc7a8ab5f9d7f4d569718b0d8
DuanShuxiu <771792977@qq.com>
2016-06-22 01:57:03 +0800

    add: call seem_first_hand_into_few_life/think_first_place_under_other_company/Number_and_great_work.java, go_good_group_about_government/good_child_or_public_company/say_government/few_person/Little_week.java or See_same_eye_into_fact.java on able week

---
+++ pretend/See_same_eye_into_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class See_same_eye_into_fact {
+    private int same_part;
+    private int long_day;
+
+    public See_same_eye_into_fact() {
+        same_part = 1943;
+        long_day = 3879;
+    }
+
+    public void give_way_to_large_company() {
+        old_group_and_new_eye();
+        System.out.println("year_and_own_fact" + same_part);
+    }
+
+    private void old_group_and_new_eye() {
+        System.out.println("own_point_and_way" + long_day);
+    }
+}
---
+++ pretend/go_good_group_about_government/good_child_or_public_company/say_government/few_person/Little_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_good_group_about_government.good_child_or_public_company.say_government.few_person;
+
+public class Little_week {
+    private int way;
+    private int have_big_point;
+
+    public Little_week() {
+        way = 1989;
+        have_big_point = 1274;
+    }
+
+    public void life_and_week() {
+        work();
+        System.out.println("young_time" + way);
+    }
+
+    private void work() {
+        System.out.println("leave_person_into_place" + have_big_point);
+    }
+}
