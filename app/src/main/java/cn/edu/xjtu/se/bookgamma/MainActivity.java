48c812ecbb1319d80250e3a0fdf4795db1cdc4c0
DUAN Yufei <duanyufeichn@gmail.com>
2016-06-21 10:43:59 +0800

    add: addbook

--- app/src/main/java/cn/edu/xjtu/se/bookgamma/MainActivity.java
+++ app/src/main/java/cn/edu/xjtu/se/bookgamma/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.edu.xjtu.se.bookgamma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,11 +41,14 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case(R.id.action_addbook):
+                Intent intent = new Intent(MainActivity.this, AddBookActivity.class);
+                startActivity(intent);
+                break;
+            case(R.id.action_settings):
+                break;
         }
 
         return super.onOptionsItemSelected(item);
