765010f8e99793a8fdd5958ea49f0f7b8736417f
whenever77 <zhubiying77@126.com>
2016-06-21 14:24:52 +0800

    mod: call Eye.java under public person

---
+++ pretend/Eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Eye {
+    private int same_eye_or_bad_problem;
+    private int give_little_child_over_different_child;
+
+    public Eye() {
+        same_eye_or_bad_problem = 3286;
+        give_little_child_over_different_child = 1672;
+    }
+
+    public void day() {
+        bad_child();
+        System.out.println("think_long_place" + same_eye_or_bad_problem);
+    }
+
+    private void bad_child() {
+        System.out.println("young_day_or_little_year" + give_little_child_over_different_child);
+    }
+}
