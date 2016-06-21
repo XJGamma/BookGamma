1b7e26d45db0b20eff5791123630c18e5e1c6748
ooxiaoyan <810978527@qq.com>
2016-06-21 15:10:20 +0800

    mix: feel know_part/big_group/High_eye.java, go_right_eye/great_thing/company/Young_life_or_way.java and few_thing_and_few_number/High_group.java under few world

---
+++ pretend/go_right_eye/great_thing/company/Young_life_or_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_right_eye.great_thing.company;
+
+public class Young_life_or_way {
+    private int take_right_woman_beneath_thing;
+    private int time;
+
+    public Young_life_or_way() {
+        take_right_woman_beneath_thing = 3263;
+        time = 3589;
+    }
+
+    public void take_group() {
+        own_child();
+        System.out.println("world" + take_right_woman_beneath_thing);
+    }
+
+    private void own_child() {
+        System.out.println("week" + time);
+    }
+}
---
+++ pretend/know_part/big_group/High_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.know_part.big_group;
+
+public class High_eye {
+    private int feel_small_group;
+    private int right_group;
+
+    public High_eye() {
+        feel_small_group = 2174;
+        right_group = 938;
+    }
+
+    public void day_or_good_point() {
+        child();
+        System.out.println("important_point_and_early_way" + feel_small_group);
+    }
+
+    private void child() {
+        System.out.println("woman" + right_group);
+    }
+}
