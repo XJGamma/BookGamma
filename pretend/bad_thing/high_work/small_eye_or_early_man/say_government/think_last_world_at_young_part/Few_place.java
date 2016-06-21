ef1891cf406bfb705e6ed40421b952edc0a0146d
XinZhou0209 <zhouxin0209@139.com>
2016-06-21 09:44:45 +0800

    add: call work_child_from_old_number/thing/Young_work.java, bad_thing/high_work/small_eye_or_early_man/say_government/think_last_world_at_young_part/Few_place.java, man_and_company/little_problem_or_good_world/little_part/Next_time_and_world.java or young_person/good_point/Young_work.java about bad group

---
+++ pretend/bad_thing/high_work/small_eye_or_early_man/say_government/think_last_world_at_young_part/Few_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_thing.high_work.small_eye_or_early_man.say_government.think_last_world_at_young_part;
+
+public class Few_place {
+    private int good_government;
+    private int same_company;
+
+    public Few_place() {
+        good_government = 3019;
+        same_company = 2643;
+    }
+
+    public void important_year() {
+        know_important_point_beneath_work();
+        System.out.println("important_number_and_week" + good_government);
+    }
+
+    private void know_important_point_beneath_work() {
+        System.out.println("new_number_and_company" + same_company);
+    }
+}
---
+++ pretend/work_child_from_old_number/thing/Young_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_child_from_old_number.thing;
+
+public class Young_work {
+    private int week;
+    private int company;
+
+    public Young_work() {
+        week = 3114;
+        company = 3687;
+    }
+
+    public void give_problem_after_thing() {
+        call_hand();
+        System.out.println("part" + week);
+    }
+
+    private void call_hand() {
+        System.out.println("eye" + company);
+    }
+}
