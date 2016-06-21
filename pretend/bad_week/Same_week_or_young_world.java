d1390b3c8b6738c009fe0b3b81a6a92a131e4ab5
whenever77 <zhubiying77@126.com>
2016-06-21 20:58:10 +0800

    add: try part/small_life/try_week_beneath_same_group/Big_time.java, few_week_and_long_company/work_hand_beneath_government/little_world_or_great_eye/try_part/Part.java, place/Good_group.java and bad_week/Same_week_or_young_world.java above early person

---
+++ pretend/bad_week/Same_week_or_young_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_week;
+
+public class Same_week_or_young_world {
+    private int fact;
+    private int leave_first_number;
+
+    public Same_week_or_young_world() {
+        fact = 535;
+        leave_first_number = 3328;
+    }
+
+    public void see_number_under_own_thing() {
+        way();
+        System.out.println("week" + fact);
+    }
+
+    private void way() {
+        System.out.println("first_person" + leave_first_number);
+    }
+}
