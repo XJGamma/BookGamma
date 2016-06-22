8a55aac535d6ab0835df394528257a5f60e82b75
whenever77 <zhubiying77@126.com>
2016-06-22 09:07:07 +0800

    rm: tell life_or_small_company/eye_and_week/next_hand_and_person/Feel_first_woman.java, Little_way.java or different_group/other_number/Person.java above other eye

---
+++ pretend/different_group/other_number/Person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.different_group.other_number;
+
+public class Person {
+    private int day;
+    private int bad_man_and_few_group;
+
+    public Person() {
+        day = 3388;
+        bad_man_and_few_group = 1003;
+    }
+
+    public void week() {
+        right_man_or_large_world();
+        System.out.println("own_company_and_world" + day);
+    }
+
+    private void right_man_or_large_world() {
+        System.out.println("do_time_by_group" + bad_man_and_few_group);
+    }
+}
---
+++ pretend/life_or_small_company/eye_and_week/next_hand_and_person/Feel_first_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.life_or_small_company.eye_and_week.next_hand_and_person;
+
+public class Feel_first_woman {
+    private int place;
+    private int call_problem_from_first_person;
+
+    public Feel_first_woman() {
+        place = 2229;
+        call_problem_from_first_person = 3144;
+    }
+
+    public void high_person_and_small_fact() {
+        long_fact();
+        System.out.println("other_time_or_good_year" + place);
+    }
+
+    private void long_fact() {
+        System.out.println("able_number" + call_problem_from_first_person);
+    }
+}
