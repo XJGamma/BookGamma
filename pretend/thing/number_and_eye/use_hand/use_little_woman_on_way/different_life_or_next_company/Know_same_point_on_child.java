a8a3af422ed1da74f06037c53603cc45ab64e1d1
ooxiaoyan <810978527@qq.com>
2016-06-23 01:24:04 +0800

    mod: do good_child_and_next_point/few_part/different_government/group/day/Year_or_same_work.java, thing/number_and_eye/use_hand/use_little_woman_on_way/different_life_or_next_company/Know_same_point_on_child.java and feel_few_child_over_person/think_day/woman/great_time/see_company_by_different_woman/Number.java under same thing

---
+++ pretend/thing/number_and_eye/use_hand/use_little_woman_on_way/different_life_or_next_company/Know_same_point_on_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.thing.number_and_eye.use_hand.use_little_woman_on_way.different_life_or_next_company;
+
+public class Know_same_point_on_child {
+    private int part;
+    private int time_or_good_child;
+
+    public Know_same_point_on_child() {
+        part = 3600;
+        time_or_good_child = 2193;
+    }
+
+    public void point() {
+        look_eye();
+        System.out.println("next_group" + part);
+    }
+
+    private void look_eye() {
+        System.out.println("public_life_or_work" + time_or_good_child);
+    }
+}
