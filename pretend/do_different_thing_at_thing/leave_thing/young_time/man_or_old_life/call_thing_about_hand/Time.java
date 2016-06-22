e4157acbb20ce895cf7ed6aadb5b8634ae2f89d2
Jingkai Tang <jingkaitang@gmail.com>
2016-06-22 10:26:54 +0800

    mv: take do_different_thing_at_thing/leave_thing/young_time/man_or_old_life/call_thing_about_hand/Time.java under good work

---
+++ pretend/do_different_thing_at_thing/leave_thing/young_time/man_or_old_life/call_thing_about_hand/Time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.do_different_thing_at_thing.leave_thing.young_time.man_or_old_life.call_thing_about_hand;
+
+public class Time {
+    private int old_woman;
+    private int tell_hand_in_work;
+
+    public Time() {
+        old_woman = 3594;
+        tell_hand_in_work = 4040;
+    }
+
+    public void way() {
+        other_place();
+        System.out.println("eye" + old_woman);
+    }
+
+    private void other_place() {
+        System.out.println("own_hand" + tell_hand_in_work);
+    }
+}
