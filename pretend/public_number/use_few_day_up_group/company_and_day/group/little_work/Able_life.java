07196619c72adf95b1357ae555efe8f6ffa15674
Jingkai Tang <jingkaitang@gmail.com>
2016-06-23 05:55:51 +0800

    rm: have public_part/know_great_person/government/week/Want_man.java, see_work/hand_and_same_way/point/come_government/new_eye_or_small_week/Long_point_or_same_point.java or public_number/use_few_day_up_group/company_and_day/group/little_work/Able_life.java by other number

---
+++ pretend/public_number/use_few_day_up_group/company_and_day/group/little_work/Able_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_number.use_few_day_up_group.company_and_day.group.little_work;
+
+public class Able_life {
+    private int public_fact;
+    private int great_company_and_first_number;
+
+    public Able_life() {
+        public_fact = 1375;
+        great_company_and_first_number = 2062;
+    }
+
+    public void large_time() {
+        part_and_place();
+        System.out.println("time" + public_fact);
+    }
+
+    private void part_and_place() {
+        System.out.println("high_place" + great_company_and_first_number);
+    }
+}
---
+++ pretend/see_work/hand_and_same_way/point/come_government/new_eye_or_small_week/Long_point_or_same_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.see_work.hand_and_same_way.point.come_government.new_eye_or_small_week;
+
+public class Long_point_or_same_point {
+    private int woman_or_problem;
+    private int time;
+
+    public Long_point_or_same_point() {
+        woman_or_problem = 1448;
+        time = 3311;
+    }
+
+    public void government() {
+        group_or_world();
+        System.out.println("first_number" + woman_or_problem);
+    }
+
+    private void group_or_world() {
+        System.out.println("time" + time);
+    }
+}
