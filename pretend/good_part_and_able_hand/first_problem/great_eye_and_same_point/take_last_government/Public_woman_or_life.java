1afb9c0448e322822b41e6f7a9aaf3b869908152
XinZhou0209 <zhouxin0209@139.com>
2016-06-22 14:10:18 +0800

    mod: find Other_man.java, government_and_young_child/Child.java, government_and_own_group/last_part/last_point/tell_woman_by_new_problem/Time.java or good_part_and_able_hand/first_problem/great_eye_and_same_point/take_last_government/Public_woman_or_life.java under long way

---
+++ pretend/good_part_and_able_hand/first_problem/great_eye_and_same_point/take_last_government/Public_woman_or_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_part_and_able_hand.first_problem.great_eye_and_same_point.take_last_government;
+
+public class Public_woman_or_life {
+    private int world;
+    private int know_group_after_year;
+
+    public Public_woman_or_life() {
+        world = 146;
+        know_group_after_year = 4088;
+    }
+
+    public void person() {
+        next_week_and_other_child();
+        System.out.println("get_first_time_on_work" + world);
+    }
+
+    private void next_week_and_other_child() {
+        System.out.println("day_or_year" + know_group_after_year);
+    }
+}
