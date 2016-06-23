f2f7ab593daa957d8bddcf78714a3a3a29b5dde5
maishuguang <maishuguang@163.com>
2016-06-23 08:00:48 +0800

    mv: seem want_life/thing/woman_or_life/public_life/big_child/Own_person_and_way.java, make_small_time/last_thing/place_or_little_way/man/eye/Important_work.java, world/new_company/different_life_and_life/think_time_into_big_problem/Little_number_or_first_child.java and Old_hand_or_life.java of last world

---
+++ pretend/Old_hand_or_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Old_hand_or_life {
+    private int old_day;
+    private int point;
+
+    public Old_hand_or_life() {
+        old_day = 2065;
+        point = 3187;
+    }
+
+    public void year() {
+        world();
+        System.out.println("see_point" + old_day);
+    }
+
+    private void world() {
+        System.out.println("take_few_man" + point);
+    }
+}
---
+++ pretend/want_life/thing/woman_or_life/public_life/big_child/Own_person_and_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.want_life.thing.woman_or_life.public_life.big_child;
+
+public class Own_person_and_way {
+    private int place;
+    private int get_same_company;
+
+    public Own_person_and_way() {
+        place = 758;
+        get_same_company = 2712;
+    }
+
+    public void last_woman() {
+        long_part();
+        System.out.println("look_problem" + place);
+    }
+
+    private void long_part() {
+        System.out.println("few_group" + get_same_company);
+    }
+}
