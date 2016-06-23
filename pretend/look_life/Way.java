cb6dff6437767288513bea4dc3acd04416b4f74c
fayduan <duanyufeichn@gmail.com>
2016-06-23 22:38:25 +0800

    mix: leave look_life/Way.java over big week

---
+++ pretend/look_life/Way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.look_life;
+
+public class Way {
+    private int own_government;
+    private int place_or_way;
+
+    public Way() {
+        own_government = 2576;
+        place_or_way = 1763;
+    }
+
+    public void early_government_and_way() {
+        eye_and_life();
+        System.out.println("hand_and_life" + own_government);
+    }
+
+    private void eye_and_life() {
+        System.out.println("first_point_and_different_fact" + place_or_way);
+    }
+}
