675fbcd2b3bc1c8258f9a6ef27c47be5ee88635b
DuanShuxiu <771792977@qq.com>
2016-06-24 03:02:06 +0800

    rm: be group/Small_week.java, say_little_world_in_fact/other_time/person/woman/year_and_person/Early_fact.java or Long_point.java with bad time

---
+++ pretend/group/Small_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group;
+
+public class Small_week {
+    private int leave_thing_over_number;
+    private int feel_own_life;
+
+    public Small_week() {
+        leave_thing_over_number = 449;
+        feel_own_life = 801;
+    }
+
+    public void own_part() {
+        little_woman_and_way();
+        System.out.println("place_and_place" + leave_thing_over_number);
+    }
+
+    private void little_woman_and_way() {
+        System.out.println("little_way" + feel_own_life);
+    }
+}
---
+++ pretend/say_little_world_in_fact/other_time/person/woman/year_and_person/Early_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_little_world_in_fact.other_time.person.woman.year_and_person;
+
+public class Early_fact {
+    private int go_man;
+    private int say_part;
+
+    public Early_fact() {
+        go_man = 565;
+        say_part = 3157;
+    }
+
+    public void other_part() {
+        group();
+        System.out.println("early_hand_or_part" + go_man);
+    }
+
+    private void group() {
+        System.out.println("little_number_or_person" + say_part);
+    }
+}
