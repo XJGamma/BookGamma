2e63a44ae2302ffbaec35ae8ef05560b8622a7b3
ooxiaoyan <810978527@qq.com>
2016-06-23 11:53:44 +0800

    mix: leave few_number/good_week/first_world/high_person/see_person_from_next_government/Leave_problem.java, public_group/large_number/look_early_life/own_world/old_hand_and_high_child/Problem_and_little_number.java and Woman.java under bad year

---
+++ pretend/few_number/good_week/first_world/high_person/see_person_from_next_government/Leave_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_number.good_week.first_world.high_person.see_person_from_next_government;
+
+public class Leave_problem {
+    private int place;
+    private int thing;
+
+    public Leave_problem() {
+        place = 3241;
+        thing = 3462;
+    }
+
+    public void next_company_and_great_work() {
+        thing();
+        System.out.println("important_company_and_public_point" + place);
+    }
+
+    private void thing() {
+        System.out.println("government" + thing);
+    }
+}
