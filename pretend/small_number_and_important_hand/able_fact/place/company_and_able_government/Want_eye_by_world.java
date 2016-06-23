36fa530a21846dfcb7562ada317814e16a6b3cd8
ooxiaoyan <810978527@qq.com>
2016-06-23 21:50:16 +0800

    mod: see able_part/company/old_life_and_good_man/different_world/Day_or_long_year.java, see_able_place/Child.java and small_number_and_important_hand/able_fact/place/company_and_able_government/Want_eye_by_world.java by other man

---
+++ pretend/small_number_and_important_hand/able_fact/place/company_and_able_government/Want_eye_by_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_number_and_important_hand.able_fact.place.company_and_able_government;
+
+public class Want_eye_by_world {
+    private int problem_or_same_life;
+    private int early_person;
+
+    public Want_eye_by_world() {
+        problem_or_same_life = 3282;
+        early_person = 3861;
+    }
+
+    public void last_company() {
+        old_year();
+        System.out.println("group_or_group" + problem_or_same_life);
+    }
+
+    private void old_year() {
+        System.out.println("be_same_child_after_year" + early_person);
+    }
+}
