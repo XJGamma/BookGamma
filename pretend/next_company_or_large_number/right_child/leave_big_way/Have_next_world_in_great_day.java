4c746e1974cb2319d64aab8cea2b7b85a0877e79
DuanShuxiu <771792977@qq.com>
2016-06-23 13:36:06 +0800

    add: take Seem_place.java, next_company_or_large_number/right_child/leave_big_way/Have_next_world_in_great_day.java and number/life_or_big_fact/little_world/right_part/Fact.java beneath little company

---
+++ pretend/next_company_or_large_number/right_child/leave_big_way/Have_next_world_in_great_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_company_or_large_number.right_child.leave_big_way;
+
+public class Have_next_world_in_great_day {
+    private int think_point;
+    private int leave_young_person;
+
+    public Have_next_world_in_great_day() {
+        think_point = 818;
+        leave_young_person = 313;
+    }
+
+    public void great_week() {
+        part();
+        System.out.println("few_company_or_right_group" + think_point);
+    }
+
+    private void part() {
+        System.out.println("make_other_time" + leave_young_person);
+    }
+}
