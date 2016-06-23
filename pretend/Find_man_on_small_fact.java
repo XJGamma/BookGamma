44fc1848600bb3c31271c543e7c4ff9369669143
maishuguang <maishuguang@163.com>
2016-06-24 01:35:40 +0800

    mix: work Find_man_on_small_fact.java, use_group_from_place/first_hand_and_life/point/bad_place_and_good_time/Part.java or able_person_and_small_problem/week/time/Year.java beneath public man

---
+++ pretend/Find_man_on_small_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Find_man_on_small_fact {
+    private int small_man;
+    private int give_good_work_on_same_hand;
+
+    public Find_man_on_small_fact() {
+        small_man = 2394;
+        give_good_work_on_same_hand = 584;
+    }
+
+    public void old_time() {
+        do_same_man_up_life();
+        System.out.println("man" + small_man);
+    }
+
+    private void do_same_man_up_life() {
+        System.out.println("right_company" + give_good_work_on_same_hand);
+    }
+}
---
+++ pretend/use_group_from_place/first_hand_and_life/point/bad_place_and_good_time/Part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_group_from_place.first_hand_and_life.point.bad_place_and_good_time;
+
+public class Part {
+    private int great_hand_and_work;
+    private int woman;
+
+    public Part() {
+        great_hand_and_work = 1754;
+        woman = 68;
+    }
+
+    public void say_young_number_about_company() {
+        year();
+        System.out.println("right_person" + great_hand_and_work);
+    }
+
+    private void year() {
+        System.out.println("work_problem_in_able_year" + woman);
+    }
+}
