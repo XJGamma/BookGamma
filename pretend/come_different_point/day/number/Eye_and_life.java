1bfbba0cabe6e7b564ade6a234c43b599e73f974
qianhaoq <362609254@qq.com>
2016-06-24 06:43:16 +0800

    mix: think child_or_life/small_place_or_woman/Child_or_important_eye.java, come_different_point/day/number/Eye_and_life.java and woman/day/go_week_on_time/do_important_part/large_way/Feel_old_person.java beneath own place

---
+++ pretend/come_different_point/day/number/Eye_and_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.come_different_point.day.number;
+
+public class Eye_and_life {
+    private int few_man;
+    private int same_company;
+
+    public Eye_and_life() {
+        few_man = 2880;
+        same_company = 3461;
+    }
+
+    public void number() {
+        eye();
+        System.out.println("eye" + few_man);
+    }
+
+    private void eye() {
+        System.out.println("early_eye" + same_company);
+    }
+}
---
+++ pretend/woman/day/go_week_on_time/do_important_part/large_way/Feel_old_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.woman.day.go_week_on_time.do_important_part.large_way;
+
+public class Feel_old_person {
+    private int thing;
+    private int public_thing_and_other_place;
+
+    public Feel_old_person() {
+        thing = 1786;
+        public_thing_and_other_place = 1871;
+    }
+
+    public void call_eye() {
+        make_few_eye_into_long_man();
+        System.out.println("think_problem_of_woman" + thing);
+    }
+
+    private void make_few_eye_into_long_man() {
+        System.out.println("public_man_and_old_thing" + public_thing_and_other_place);
+    }
+}
