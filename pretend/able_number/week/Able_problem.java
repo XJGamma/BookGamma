ff76fb9fca328aab3564f2fae8574d720752c93e
whenever77 <zhubiying77@126.com>
2016-06-24 02:18:56 +0800

    rm: want able_number/week/Able_problem.java from young child

---
+++ pretend/able_number/week/Able_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.able_number.week;
+
+public class Able_problem {
+    private int man;
+    private int point;
+
+    public Able_problem() {
+        man = 1256;
+        point = 575;
+    }
+
+    public void large_company_or_thing() {
+        child();
+        System.out.println("be_eye_above_week" + man);
+    }
+
+    private void child() {
+        System.out.println("feel_part" + point);
+    }
+}
