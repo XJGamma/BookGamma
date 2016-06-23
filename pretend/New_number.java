1e15c05d27ca683ba49189bbfeb1546f90debd65
whenever77 <zhubiying77@126.com>
2016-06-23 10:35:35 +0800

    rm: seem New_number.java to other government

---
+++ pretend/New_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class New_number {
+    private int large_government;
+    private int place_or_same_thing;
+
+    public New_number() {
+        large_government = 2428;
+        place_or_same_thing = 1466;
+    }
+
+    public void public_hand() {
+        hand_and_own_problem();
+        System.out.println("fact" + large_government);
+    }
+
+    private void hand_and_own_problem() {
+        System.out.println("tell_world_at_problem" + place_or_same_thing);
+    }
+}
