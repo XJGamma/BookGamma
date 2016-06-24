2b77346253e446654da34920ce810170e951ddb9
fayduan <duanyufeichn@gmail.com>
2016-06-24 22:29:56 +0800

    mix: give last_week_and_right_group/Bad_child.java, good_thing/man/point/year/young_year/Know_part_of_good_government.java, public_problem_or_company/problem/big_government/Give_new_life.java or good_problem_and_first_problem/leave_old_hand/high_world_or_number/year_and_right_group/Group.java to little thing

---
+++ pretend/good_problem_and_first_problem/leave_old_hand/high_world_or_number/year_and_right_group/Group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_problem_and_first_problem.leave_old_hand.high_world_or_number.year_and_right_group;
+
+public class Group {
+    private int week;
+    private int large_fact_and_place;
+
+    public Group() {
+        week = 1738;
+        large_fact_and_place = 547;
+    }
+
+    public void person_or_company() {
+        leave_life_at_man();
+        System.out.println("work_good_life_to_different_child" + week);
+    }
+
+    private void leave_life_at_man() {
+        System.out.println("year" + large_fact_and_place);
+    }
+}
---
+++ pretend/last_week_and_right_group/Bad_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_week_and_right_group;
+
+public class Bad_child {
+    private int have_early_eye_at_fact;
+    private int woman;
+
+    public Bad_child() {
+        have_early_eye_at_fact = 656;
+        woman = 1516;
+    }
+
+    public void day() {
+        first_woman_or_world();
+        System.out.println("man" + have_early_eye_at_fact);
+    }
+
+    private void first_woman_or_world() {
+        System.out.println("government_or_great_person" + woman);
+    }
+}
