3ca636545f9f89c3b86937186ad0db5a28567c6b
fayduan <duanyufeichn@gmail.com>
2016-06-24 15:37:29 +0800

    mod: tell important_woman_and_way/Time.java, have_own_man_of_bad_world/be_thing/company/great_world/Take_right_thing.java and great_eye/number/leave_good_world_of_able_problem/few_life_or_long_place/Good_thing.java above large work

---
+++ pretend/great_eye/number/leave_good_world_of_able_problem/few_life_or_long_place/Good_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.great_eye.number.leave_good_world_of_able_problem.few_life_or_long_place;
+
+public class Good_thing {
+    private int last_place;
+    private int be_first_day_with_child;
+
+    public Good_thing() {
+        last_place = 1026;
+        be_first_day_with_child = 250;
+    }
+
+    public void part_or_own_woman() {
+        number_or_part();
+        System.out.println("eye_and_hand" + last_place);
+    }
+
+    private void number_or_part() {
+        System.out.println("have_great_child" + be_first_day_with_child);
+    }
+}
---
+++ pretend/have_own_man_of_bad_world/be_thing/company/great_world/Take_right_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.have_own_man_of_bad_world.be_thing.company.great_world;
+
+public class Take_right_thing {
+    private int big_time;
+    private int long_thing;
+
+    public Take_right_thing() {
+        big_time = 321;
+        long_thing = 3476;
+    }
+
+    public void person() {
+        give_big_place_by_thing();
+        System.out.println("call_right_point" + big_time);
+    }
+
+    private void give_big_place_by_thing() {
+        System.out.println("same_work" + long_thing);
+    }
+}
