6e9734907090dc5f1c0de7aa5a93a777181a0e5f
maishuguang <maishuguang@163.com>
2016-06-24 19:02:22 +0800

    mv: say right_place/go_place/world_or_own_government/year/Fact.java, Public_work_and_hand.java, small_man_and_right_point/get_early_man/part_and_last_week/part/next_problem/Own_thing.java and call_point_after_different_part/life/different_part_and_world/last_number_and_few_man/old_life/Call_work.java under same week

---
+++ pretend/call_point_after_different_part/life/different_part_and_world/last_number_and_few_man/old_life/Call_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.call_point_after_different_part.life.different_part_and_world.last_number_and_few_man.old_life;
+
+public class Call_work {
+    private int able_eye;
+    private int world_and_number;
+
+    public Call_work() {
+        able_eye = 1333;
+        world_and_number = 43;
+    }
+
+    public void young_problem_or_same_way() {
+        group();
+        System.out.println("new_number" + able_eye);
+    }
+
+    private void group() {
+        System.out.println("year" + world_and_number);
+    }
+}
