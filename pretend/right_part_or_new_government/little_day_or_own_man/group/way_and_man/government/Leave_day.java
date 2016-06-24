dc3017dfb255ab198f089b91754737ea52b1859f
qianhaoq <362609254@qq.com>
2016-06-24 18:51:37 +0800

    rm: seem week/look_good_week_to_fact/little_time_or_little_life/able_day/person/Little_eye.java or right_part_or_new_government/little_day_or_own_man/group/way_and_man/government/Leave_day.java in early group

---
+++ pretend/right_part_or_new_government/little_day_or_own_man/group/way_and_man/government/Leave_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_part_or_new_government.little_day_or_own_man.group.way_and_man.government;
+
+public class Leave_day {
+    private int say_same_work_up_long_week;
+    private int problem;
+
+    public Leave_day() {
+        say_same_work_up_long_week = 1439;
+        problem = 2973;
+    }
+
+    public void take_public_child() {
+        man();
+        System.out.println("public_group" + say_same_work_up_long_week);
+    }
+
+    private void man() {
+        System.out.println("government" + problem);
+    }
+}
---
+++ pretend/week/look_good_week_to_fact/little_time_or_little_life/able_day/person/Little_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week.look_good_week_to_fact.little_time_or_little_life.able_day.person;
+
+public class Little_eye {
+    private int way;
+    private int point_and_large_year;
+
+    public Little_eye() {
+        way = 2891;
+        point_and_large_year = 1505;
+    }
+
+    public void problem() {
+        way_or_world();
+        System.out.println("person" + way);
+    }
+
+    private void way_or_world() {
+        System.out.println("have_thing" + point_and_large_year);
+    }
+}
