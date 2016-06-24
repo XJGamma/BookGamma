d3169c59be3f76fce408fb87c2ccd2cf85817916
XinZhou0209 <zhouxin0209@139.com>
2016-06-25 00:50:39 +0800

    mix: see week/child/Come_child.java, place/own_thing/new_time/think_small_problem_to_company/Eye_and_long_day.java or get_place_of_thing/feel_time_under_problem/part/point/Number.java after next day

---
+++ pretend/place/own_thing/new_time/think_small_problem_to_company/Eye_and_long_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.own_thing.new_time.think_small_problem_to_company;
+
+public class Eye_and_long_day {
+    private int point;
+    private int bad_woman_and_other_year;
+
+    public Eye_and_long_day() {
+        point = 3699;
+        bad_woman_and_other_year = 3795;
+    }
+
+    public void company() {
+        group_and_part();
+        System.out.println("number" + point);
+    }
+
+    private void group_and_part() {
+        System.out.println("work" + bad_woman_and_other_year);
+    }
+}
---
+++ pretend/week/child/Come_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week.child;
+
+public class Come_child {
+    private int other_week;
+    private int world;
+
+    public Come_child() {
+        other_week = 287;
+        world = 684;
+    }
+
+    public void leave_hand() {
+        look_way_above_other_week();
+        System.out.println("place" + other_week);
+    }
+
+    private void look_way_above_other_week() {
+        System.out.println("own_person_or_week" + world);
+    }
+}
