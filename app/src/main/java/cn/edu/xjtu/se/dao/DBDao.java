9913ec7e87fdbf5ea121225fcdc9c6752bff3108
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-06-24 10:17:13 +0800

    add: DBDao test

--- app/src/main/java/cn/edu/xjtu/se/dao/DBDao.java
+++ app/src/main/java/cn/edu/xjtu/se/dao/DBDao.java
@@ -1,7 +1,6 @@
 package cn.edu.xjtu.se.dao;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
@@ -15,19 +14,19 @@ import cn.edu.xjtu.se.util.XGApplication;
  */
 public class DBDao {
 
-    static public long addBook(String bookName, int pages, Date finish_time, String isbn, String image){
+    static public long addBook(String bookName, int pages, Date finish_time, String isbn, String image) {
         DBHelper dbHelper = new DBHelper(XGApplication.getContext());
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put("name",bookName);
-        values.put("image",image);
-        values.put("isbn",isbn);
-        values.put("pages",pages);
-        values.put("current_page",0);
-        values.put("finish_time",dateFormat.format(finish_time));
-        values.put("total_reading_time",0);
-        Log.i("DBDao","insert...");
-        return db.insert("Books",null,values);
+        values.put("name", bookName);
+        values.put("image", image);
+        values.put("isbn", isbn);
+        values.put("pages", pages);
+        values.put("current_page", 0);
+        values.put("finish_time", dateFormat.format(finish_time));
+        values.put("total_reading_time", 0);
+        Log.i("DBDao", "insert...");
+        return db.insert("Books", null, values);
     }
 }
