52c1d77c708d4b39b40cfa6efc8d88ea8170f0e7
DuanShuxiu <771792977@qq.com>
2016-06-25 04:09:55 +0800

    mod: do World.java and group/group/think_bad_government/Work_great_day.java for good thing

---
+++ pretend/World.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class World {
+    private int give_same_problem_into_high_eye;
+    private int look_week_up_place;
+
+    public World() {
+        give_same_problem_into_high_eye = 135;
+        look_week_up_place = 736;
+    }
+
+    public void day_or_same_year() {
+        right_problem_or_great_part();
+        System.out.println("little_part_or_work" + give_same_problem_into_high_eye);
+    }
+
+    private void right_problem_or_great_part() {
+        System.out.println("few_place" + look_week_up_place);
+    }
+}
