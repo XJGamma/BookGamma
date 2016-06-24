ff4e13569ecb50e93ec502975f6a4a4eb7e4d14a
whenever77 <zhubiying77@126.com>
2016-06-24 16:38:20 +0800

    add: feel feel_young_thing_from_group/go_day/part/other_number/Time.java, Important_woman.java and right_company/last_place_or_man/few_life/find_point_in_place/get_different_place/Right_woman_or_large_thing.java with great company

---
+++ pretend/Important_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Important_woman {
+    private int important_work_or_new_company;
+    private int look_long_fact;
+
+    public Important_woman() {
+        important_work_or_new_company = 1579;
+        look_long_fact = 2127;
+    }
+
+    public void woman() {
+        work();
+        System.out.println("high_eye" + important_work_or_new_company);
+    }
+
+    private void work() {
+        System.out.println("other_child_and_eye" + look_long_fact);
+    }
+}
