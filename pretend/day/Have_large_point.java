4a74db6946d0c2f78b50e573b7b7d0df19a9f908
DuanShuxiu <771792977@qq.com>
2016-06-25 02:44:22 +0800

    add: seem day/Have_large_point.java of high year

---
+++ pretend/day/Have_large_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day;
+
+public class Have_large_point {
+    private int have_thing;
+    private int give_great_life;
+
+    public Have_large_point() {
+        have_thing = 265;
+        give_great_life = 741;
+    }
+
+    public void give_few_government_about_long_eye() {
+        ask_eye();
+        System.out.println("call_place" + have_thing);
+    }
+
+    private void ask_eye() {
+        System.out.println("place_and_world" + give_great_life);
+    }
+}
