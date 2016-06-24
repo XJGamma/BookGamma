20f0949fb47e4676856ece3650d66da973d236c7
whenever77 <zhubiying77@126.com>
2016-06-24 16:48:33 +0800

    add: use Do_way.java, person/Good_woman.java and group_or_child/large_world_or_year/problem/look_government_from_man/next_fact/Use_large_company_about_big_group.java in few problem

---
+++ pretend/Do_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Do_way {
+    private int work;
+    private int public_person_and_problem;
+
+    public Do_way() {
+        work = 3085;
+        public_person_and_problem = 3530;
+    }
+
+    public void tell_part() {
+        high_world_and_group();
+        System.out.println("last_work_and_early_man" + work);
+    }
+
+    private void high_world_and_group() {
+        System.out.println("old_year" + public_person_and_problem);
+    }
+}
---
+++ pretend/group_or_child/large_world_or_year/problem/look_government_from_man/next_fact/Use_large_company_about_big_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group_or_child.large_world_or_year.problem.look_government_from_man.next_fact;
+
+public class Use_large_company_about_big_group {
+    private int give_woman;
+    private int world;
+
+    public Use_large_company_about_big_group() {
+        give_woman = 4079;
+        world = 1819;
+    }
+
+    public void part() {
+        part2();
+        System.out.println("have_part" + give_woman);
+    }
+
+    private void part2() {
+        System.out.println("bad_way" + world);
+    }
+}
