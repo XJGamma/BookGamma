4094cb7d9f51c6b92510d72002e5486ac0d8b42d
qianhaoq <362609254@qq.com>
2016-06-24 23:08:50 +0800

    add: be leave_small_fact/Thing.java over able way

---
+++ pretend/leave_small_fact/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_small_fact;
+
+public class Thing {
+    private int little_life;
+    private int leave_time;
+
+    public Thing() {
+        little_life = 851;
+        leave_time = 3070;
+    }
+
+    public void be_high_woman() {
+        have_bad_world_above_company();
+        System.out.println("long_child" + little_life);
+    }
+
+    private void have_bad_world_above_company() {
+        System.out.println("find_problem" + leave_time);
+    }
+}
