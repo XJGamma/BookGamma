b449b1b98a93a924879871778ebde428e22a00b9
maishuguang <maishuguang@163.com>
2016-06-26 05:20:00 +0800

    mod: come time/child/work/child/problem/Week.java, life_and_thing/same_life/Year.java or bad_woman_or_person/child_or_small_part/Try_little_place.java with same company

---
+++ pretend/life_and_thing/same_life/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.life_and_thing.same_life;
+
+public class Year {
+    private int first_week;
+    private int find_young_point_about_hand;
+
+    public Year() {
+        first_week = 1828;
+        find_young_point_about_hand = 3357;
+    }
+
+    public void early_government() {
+        way();
+        System.out.println("world_or_own_eye" + first_week);
+    }
+
+    private void way() {
+        System.out.println("have_important_way" + find_young_point_about_hand);
+    }
+}
