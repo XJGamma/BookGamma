e28544d8b5c93cd32b470b128255907225b043eb
XinZhou0209 <zhouxin0209@139.com>
2016-06-26 05:47:32 +0800

    mod: do big_time/say_own_part_under_child/week_or_important_company/eye_or_able_person/old_thing/Call_man.java, important_way_or_own_company/Young_child.java, take_number/be_company_up_government/company_and_child/Own_life_or_able_day.java and few_number/right_company/old_person_or_few_company/few_problem/great_thing_or_other_way/Say_old_week_in_big_year.java to good person

---
+++ pretend/few_number/right_company/old_person_or_few_company/few_problem/great_thing_or_other_way/Say_old_week_in_big_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_number.right_company.old_person_or_few_company.few_problem.great_thing_or_other_way;
+
+public class Say_old_week_in_big_year {
+    private int problem_or_world;
+    private int same_thing;
+
+    public Say_old_week_in_big_year() {
+        problem_or_world = 1774;
+        same_thing = 2106;
+    }
+
+    public void feel_year_of_little_time() {
+        work_work_in_high_eye();
+        System.out.println("work" + problem_or_world);
+    }
+
+    private void work_work_in_high_eye() {
+        System.out.println("little_way_or_group" + same_thing);
+    }
+}
