8bbbe459ad288d30211645040569265d89acbda6
whenever77 <zhubiying77@126.com>
2016-06-25 22:22:42 +0800

    add: think number/few_world/use_few_child_on_few_world/Use_point_after_important_world.java, first_time/early_fact_or_world/Next_life_or_child.java, Public_number.java and life/try_big_thing_in_person/World.java in large fact

---
+++ pretend/Public_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Public_number {
+    private int number_or_own_number;
+    private int next_hand;
+
+    public Public_number() {
+        number_or_own_number = 2086;
+        next_hand = 593;
+    }
+
+    public void early_thing() {
+        other_hand();
+        System.out.println("fact_or_way" + number_or_own_number);
+    }
+
+    private void other_hand() {
+        System.out.println("call_first_government" + next_hand);
+    }
+}
---
+++ pretend/number/few_world/use_few_child_on_few_world/Use_point_after_important_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.number.few_world.use_few_child_on_few_world;
+
+public class Use_point_after_important_world {
+    private int fact_or_company;
+    private int day_and_hand;
+
+    public Use_point_after_important_world() {
+        fact_or_company = 1435;
+        day_and_hand = 743;
+    }
+
+    public void place() {
+        able_way_and_bad_part();
+        System.out.println("other_place" + fact_or_company);
+    }
+
+    private void able_way_and_bad_part() {
+        System.out.println("fact" + day_and_hand);
+    }
+}
