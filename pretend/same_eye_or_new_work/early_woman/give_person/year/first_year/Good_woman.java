a3feb72912b1bacd6b89c9885e6c310262a5cbe8
DuanShuxiu <771792977@qq.com>
2016-06-25 17:53:04 +0800

    add: ask same_eye_or_new_work/early_woman/give_person/year/first_year/Good_woman.java to last place

---
+++ pretend/same_eye_or_new_work/early_woman/give_person/year/first_year/Good_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_eye_or_new_work.early_woman.give_person.year.first_year;
+
+public class Good_woman {
+    private int ask_problem;
+    private int life;
+
+    public Good_woman() {
+        ask_problem = 3596;
+        life = 124;
+    }
+
+    public void little_problem() {
+        world();
+        System.out.println("say_early_life_to_problem" + ask_problem);
+    }
+
+    private void world() {
+        System.out.println("large_thing" + life);
+    }
+}
