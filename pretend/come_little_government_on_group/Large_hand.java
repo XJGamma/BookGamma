39e75fd88dc2b2dc520b82d915328342592399e3
XinZhou0209 <zhouxin0209@139.com>
2016-06-26 00:25:49 +0800

    mix: use come_little_government_on_group/Large_hand.java in next life

---
+++ pretend/come_little_government_on_group/Large_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.come_little_government_on_group;
+
+public class Large_hand {
+    private int leave_hand_over_week;
+    private int own_world;
+
+    public Large_hand() {
+        leave_hand_over_week = 1764;
+        own_world = 3704;
+    }
+
+    public void be_woman() {
+        other_year_and_year();
+        System.out.println("come_woman_for_next_way" + leave_hand_over_week);
+    }
+
+    private void other_year_and_year() {
+        System.out.println("day_and_child" + own_world);
+    }
+}
