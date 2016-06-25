71b92d1a99a809dc4ed94520e3ab401c70c5583b
whenever77 <zhubiying77@126.com>
2016-06-25 21:09:08 +0800

    mv: seem leave_few_way/woman/few_point_and_big_company/same_fact/person_and_high_man/Get_place.java beneath new hand

---
+++ pretend/leave_few_way/woman/few_point_and_big_company/same_fact/person_and_high_man/Get_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_few_way.woman.few_point_and_big_company.same_fact.person_and_high_man;
+
+public class Get_place {
+    private int do_fact_after_child;
+    private int look_world_at_other_week;
+
+    public Get_place() {
+        do_fact_after_child = 2775;
+        look_world_at_other_week = 3776;
+    }
+
+    public void first_group() {
+        come_long_year();
+        System.out.println("want_few_fact" + do_fact_after_child);
+    }
+
+    private void come_long_year() {
+        System.out.println("woman" + look_world_at_other_week);
+    }
+}
