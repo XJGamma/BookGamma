acdce4d50b4dba068edb6b92269f297d181b0da7
DuanShuxiu <771792977@qq.com>
2016-06-25 14:22:39 +0800

    add: know High_place.java and point_and_child/life/first_thing/Company_or_long_child.java by long child

---
+++ pretend/High_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class High_place {
+    private int first_man_and_young_man;
+    private int important_government;
+
+    public High_place() {
+        first_man_and_young_man = 3198;
+        important_government = 2351;
+    }
+
+    public void do_own_place() {
+        come_last_life_beneath_right_world();
+        System.out.println("number_or_company" + first_man_and_young_man);
+    }
+
+    private void come_last_life_beneath_right_world() {
+        System.out.println("day_and_good_group" + important_government);
+    }
+}
---
+++ pretend/point_and_child/life/first_thing/Company_or_long_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point_and_child.life.first_thing;
+
+public class Company_or_long_child {
+    private int thing_and_early_man;
+    private int thing;
+
+    public Company_or_long_child() {
+        thing_and_early_man = 795;
+        thing = 517;
+    }
+
+    public void say_important_hand() {
+        long_day();
+        System.out.println("work_and_important_year" + thing_and_early_man);
+    }
+
+    private void long_day() {
+        System.out.println("good_eye" + thing);
+    }
+}
