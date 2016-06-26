22ad27881758f1d56ddf3a78b2e37691798ecc9b
whenever77 <zhubiying77@126.com>
2016-06-26 09:20:43 +0800

    add: give year/man/Hand.java, Try_thing_for_first_day.java and be_man/Year_or_high_hand.java into last eye

---
+++ pretend/be_man/Year_or_high_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.be_man;
+
+public class Year_or_high_hand {
+    private int tell_fact;
+    private int thing_and_big_year;
+
+    public Year_or_high_hand() {
+        tell_fact = 2372;
+        thing_and_big_year = 657;
+    }
+
+    public void same_person_and_great_part() {
+        work_and_first_number();
+        System.out.println("able_time" + tell_fact);
+    }
+
+    private void work_and_first_number() {
+        System.out.println("child" + thing_and_big_year);
+    }
+}
