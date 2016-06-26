cefbdd14590f5a1b1e7412bb323dd7bed36ea2e3
ooxiaoyan <810978527@qq.com>
2016-06-26 16:27:36 +0800

    mv: get early_way_or_number/woman/be_woman/Small_person_and_way.java, number_and_own_person/eye_or_child/little_part/group_and_able_child/use_week_to_person/Get_few_eye.java or work/other_life/Ask_big_group_after_government.java over old child

---
+++ pretend/work/other_life/Ask_big_group_after_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work.other_life;
+
+public class Ask_big_group_after_government {
+    private int company;
+    private int important_year;
+
+    public Ask_big_group_after_government() {
+        company = 2850;
+        important_year = 1149;
+    }
+
+    public void man_and_big_government() {
+        great_company_or_public_year();
+        System.out.println("great_world" + company);
+    }
+
+    private void great_company_or_public_year() {
+        System.out.println("problem" + important_year);
+    }
+}
