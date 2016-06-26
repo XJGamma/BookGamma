b06411da17c4fa176de8100d696ac5e6951f4820
XinZhou0209 <zhouxin0209@139.com>
2016-06-26 11:45:45 +0800

    mix: have Hand_and_group.java and call_long_week/Life_and_new_part.java about important group

---
+++ pretend/call_long_week/Life_and_new_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.call_long_week;
+
+public class Life_and_new_part {
+    private int same_year_and_world;
+    private int first_government;
+
+    public Life_and_new_part() {
+        same_year_and_world = 2428;
+        first_government = 2009;
+    }
+
+    public void seem_long_year() {
+        hand();
+        System.out.println("know_year_with_child" + same_year_and_world);
+    }
+
+    private void hand() {
+        System.out.println("go_other_part_at_big_work" + first_government);
+    }
+}
