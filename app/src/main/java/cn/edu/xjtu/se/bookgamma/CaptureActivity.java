20890476a448ced375e2bec0469f6d89590ac039
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-06-26 11:50:10 +0800

    add: capture activity and layout

---
+++ app/src/main/java/cn/edu/xjtu/se/bookgamma/CaptureActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.xjtu.se.bookgamma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+}
