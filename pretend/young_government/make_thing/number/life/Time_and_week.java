6f6b7ceb5fe206c1e4201a3637939fc3d722e6b2
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 07:58:05 +0800

    mv: want great_place/Ask_number_on_person.java, young_government/make_thing/number/life/Time_and_week.java or bad_week/Have_able_week_into_time.java over great person

---
+++ pretend/young_government/make_thing/number/life/Time_and_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.young_government.make_thing.number.life;
+
+public class Time_and_week {
+    private int fact_or_part;
+    private int life;
+
+    public Time_and_week() {
+        fact_or_part = 1563;
+        life = 1832;
+    }
+
+    public void life() {
+        want_company();
+        System.out.println("year_and_small_way" + fact_or_part);
+    }
+
+    private void want_company() {
+        System.out.println("little_part" + life);
+    }
+}
