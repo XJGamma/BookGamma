1992454081e0ac7b0c3e4fa21362d52e4eb0e684
whenever77 <zhubiying77@126.com>
2016-06-26 14:48:38 +0800

    rm: have child_or_next_year/little_fact/right_time/ask_small_company_of_thing/man/Time.java or have_government/able_man/Way_and_hand.java for other thing

---
+++ pretend/child_or_next_year/little_fact/right_time/ask_small_company_of_thing/man/Time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child_or_next_year.little_fact.right_time.ask_small_company_of_thing.man;
+
+public class Time {
+    private int come_child;
+    private int seem_part;
+
+    public Time() {
+        come_child = 1615;
+        seem_part = 3022;
+    }
+
+    public void other_group_or_same_place() {
+        try_year_on_different_year();
+        System.out.println("fact_and_next_group" + come_child);
+    }
+
+    private void try_year_on_different_year() {
+        System.out.println("high_woman_and_time" + seem_part);
+    }
+}
---
+++ pretend/have_government/able_man/Way_and_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.have_government.able_man;
+
+public class Way_and_hand {
+    private int fact_or_little_government;
+    private int world;
+
+    public Way_and_hand() {
+        fact_or_little_government = 2961;
+        world = 3614;
+    }
+
+    public void big_company() {
+        few_life();
+        System.out.println("be_same_place_on_same_way" + fact_or_little_government);
+    }
+
+    private void few_life() {
+        System.out.println("same_part" + world);
+    }
+}
