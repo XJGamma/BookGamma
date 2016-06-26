94edf8b4ccb8fc44a4ffd6ec0c8297614f411213
ooxiaoyan <810978527@qq.com>
2016-06-26 09:27:18 +0800

    mix: give large_problem/company/part_and_public_point/Day.java, give_small_world_with_long_day/seem_fact_under_group/man_or_same_government/seem_day_in_man/world/Seem_thing_in_life.java, Work.java or man/problem/work/early_week_and_week/point/Leave_other_day_at_life.java to small number

---
+++ pretend/large_problem/company/part_and_public_point/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.large_problem.company.part_and_public_point;
+
+public class Day {
+    private int seem_day;
+    private int day;
+
+    public Day() {
+        seem_day = 1899;
+        day = 2152;
+    }
+
+    public void week_and_young_child() {
+        try_person_after_great_world();
+        System.out.println("right_hand" + seem_day);
+    }
+
+    private void try_person_after_great_world() {
+        System.out.println("great_hand" + day);
+    }
+}
