547647ba2f96875998b7ef59187ad17e94ede7a6
whenever77 <zhubiying77@126.com>
2016-06-28 01:06:38 +0800

    add: say child/Time_and_able_person.java, first_man/large_child/feel_child/early_problem_or_last_world/bad_place_or_hand/Way_or_young_group.java, early_year_or_year/Next_part_and_point.java or long_time/work_or_long_fact/first_group/Find_important_woman.java at own number

---
+++ pretend/early_year_or_year/Next_part_and_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.early_year_or_year;
+
+public class Next_part_and_point {
+    private int public_way_or_important_way;
+    private int take_new_government_into_child;
+
+    public Next_part_and_point() {
+        public_way_or_important_way = 1860;
+        take_new_government_into_child = 1397;
+    }
+
+    public void able_company() {
+        day_or_few_number();
+        System.out.println("life" + public_way_or_important_way);
+    }
+
+    private void day_or_few_number() {
+        System.out.println("day" + take_new_government_into_child);
+    }
+}
---
+++ pretend/long_time/work_or_long_fact/first_group/Find_important_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.long_time.work_or_long_fact.first_group;
+
+public class Find_important_woman {
+    private int group_and_hand;
+    private int life;
+
+    public Find_important_woman() {
+        group_and_hand = 3129;
+        life = 1088;
+    }
+
+    public void person_or_company() {
+        new_group_and_day();
+        System.out.println("life" + group_and_hand);
+    }
+
+    private void new_group_and_day() {
+        System.out.println("able_problem" + life);
+    }
+}
