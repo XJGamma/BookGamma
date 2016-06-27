6242432bf7c61d8fc9e1c3075b93d0ac7ac3d925
ooxiaoyan <810978527@qq.com>
2016-06-28 06:30:04 +0800

    rm: think Eye.java up small man

---
+++ pretend/Eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Eye {
+    private int know_number_about_large_week;
+    private int fact;
+
+    public Eye() {
+        know_number_about_large_week = 3507;
+        fact = 2678;
+    }
+
+    public void work_work() {
+        hand();
+        System.out.println("know_way" + know_number_about_large_week);
+    }
+
+    private void hand() {
+        System.out.println("make_long_company" + fact);
+    }
+}
