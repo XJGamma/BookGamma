c1bbf1a26cd483a2c2609a02ab165030cb93b8ea
ooxiaoyan <810978527@qq.com>
2016-06-28 02:18:31 +0800

    mod: find fact/make_way_about_hand/next_company/be_hand/government/Good_person.java, life/early_woman/Way.java or early_week/long_number/first_group/right_time_or_great_hand/place_and_year/Early_part_or_new_eye.java into little hand

---
+++ pretend/early_week/long_number/first_group/right_time_or_great_hand/place_and_year/Early_part_or_new_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.early_week.long_number.first_group.right_time_or_great_hand.place_and_year;
+
+public class Early_part_or_new_eye {
+    private int problem;
+    private int able_point;
+
+    public Early_part_or_new_eye() {
+        problem = 2810;
+        able_point = 3383;
+    }
+
+    public void do_old_place() {
+        government();
+        System.out.println("important_child_or_young_thing" + problem);
+    }
+
+    private void government() {
+        System.out.println("company_or_public_time" + able_point);
+    }
+}
---
+++ pretend/fact/make_way_about_hand/next_company/be_hand/government/Good_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact.make_way_about_hand.next_company.be_hand.government;
+
+public class Good_person {
+    private int great_week;
+    private int know_man_into_number;
+
+    public Good_person() {
+        great_week = 1288;
+        know_man_into_number = 2252;
+    }
+
+    public void thing() {
+        work();
+        System.out.println("own_company_or_time" + great_week);
+    }
+
+    private void work() {
+        System.out.println("same_thing" + know_man_into_number);
+    }
+}
