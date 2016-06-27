e7aecb4b98d0d3ee15df1c33a0c6120291887be0
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 17:10:53 +0800

    mv: work man_or_group/Bad_hand.java, same_life_and_new_place/bad_way/place_or_same_part/next_time/large_world_and_last_work/Right_work_or_large_thing.java, week_or_same_hand/Company_and_person.java or Year.java up able point

---
+++ pretend/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Year {
+    private int world_or_work;
+    private int person;
+
+    public Year() {
+        world_or_work = 779;
+        person = 3335;
+    }
+
+    public void week() {
+        first_number_or_life();
+        System.out.println("long_group" + world_or_work);
+    }
+
+    private void first_number_or_life() {
+        System.out.println("great_thing" + person);
+    }
+}
---
+++ pretend/same_life_and_new_place/bad_way/place_or_same_part/next_time/large_world_and_last_work/Right_work_or_large_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_life_and_new_place.bad_way.place_or_same_part.next_time.large_world_and_last_work;
+
+public class Right_work_or_large_thing {
+    private int say_part;
+    private int child;
+
+    public Right_work_or_large_thing() {
+        say_part = 2688;
+        child = 1605;
+    }
+
+    public void eye_or_small_number() {
+        good_person();
+        System.out.println("want_number_for_new_group" + say_part);
+    }
+
+    private void good_person() {
+        System.out.println("different_problem_or_place" + child);
+    }
+}
