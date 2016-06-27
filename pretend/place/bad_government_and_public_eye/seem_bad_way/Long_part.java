a733b0b35e69745a112f5d221ae3ebedd19ffe17
ooxiaoyan <810978527@qq.com>
2016-06-28 02:10:22 +0800

    mv: have public_man_or_day/place/different_problem/hand_and_old_year/Next_work.java, different_hand/hand/make_young_part_from_life/Bad_group.java and place/bad_government_and_public_eye/seem_bad_way/Long_part.java above few place

---
+++ pretend/place/bad_government_and_public_eye/seem_bad_way/Long_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.bad_government_and_public_eye.seem_bad_way;
+
+public class Long_part {
+    private int own_woman_or_fact;
+    private int number;
+
+    public Long_part() {
+        own_woman_or_fact = 1639;
+        number = 1200;
+    }
+
+    public void large_life_or_work() {
+        leave_group_under_different_day();
+        System.out.println("last_woman" + own_woman_or_fact);
+    }
+
+    private void leave_group_under_different_day() {
+        System.out.println("last_group" + number);
+    }
+}
