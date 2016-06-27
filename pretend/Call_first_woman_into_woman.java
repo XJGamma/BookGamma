2b59738ded7b748ed9a128e872098a75c55b33a8
XinZhou0209 <zhouxin0209@139.com>
2016-06-27 22:22:14 +0800

    rm: make Call_first_woman_into_woman.java or little_hand/say_little_time/great_group/Bad_number.java from bad thing

---
+++ pretend/Call_first_woman_into_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Call_first_woman_into_woman {
+    private int high_woman_and_place;
+    private int number_or_time;
+
+    public Call_first_woman_into_woman() {
+        high_woman_and_place = 2563;
+        number_or_time = 3784;
+    }
+
+    public void great_child() {
+        child_and_person();
+        System.out.println("high_problem" + high_woman_and_place);
+    }
+
+    private void child_and_person() {
+        System.out.println("thing" + number_or_time);
+    }
+}
