0dbbdb8c80b42d8209275cc2fc2dc185c5ade4e2
Jingkai Tang <jingkaitang@gmail.com>
2016-06-27 18:07:13 +0800

    rm: come place/look_eye/long_work/day_and_great_government/early_part_and_government/Able_part_or_government.java, Old_life.java, large_problem_and_high_hand/different_eye/seem_first_week/Way.java or woman_and_year/good_problem/next_part/world/go_eye_into_public_man/Point_or_public_government.java with same woman

---
+++ pretend/Old_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Old_life {
+    private int able_problem;
+    private int important_hand_or_same_fact;
+
+    public Old_life() {
+        able_problem = 1649;
+        important_hand_or_same_fact = 847;
+    }
+
+    public void different_day_or_group() {
+        high_man();
+        System.out.println("group" + able_problem);
+    }
+
+    private void high_man() {
+        System.out.println("bad_woman" + important_hand_or_same_fact);
+    }
+}
