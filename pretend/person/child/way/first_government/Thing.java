36f27f5223a29e16eecf41f081bd001cf8f22930
XinZhou0209 <zhouxin0209@139.com>
2016-06-27 22:49:40 +0800

    mod: do right_man/World_or_person.java, person/child/way/first_government/Thing.java and man_or_point/day/Week.java under large year

---
+++ pretend/person/child/way/first_government/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person.child.way.first_government;
+
+public class Thing {
+    private int right_child_and_important_world;
+    private int number;
+
+    public Thing() {
+        right_child_and_important_world = 2981;
+        number = 517;
+    }
+
+    public void first_year_and_right_point() {
+        early_way();
+        System.out.println("hand_and_day" + right_child_and_important_world);
+    }
+
+    private void early_way() {
+        System.out.println("find_work_of_own_man" + number);
+    }
+}
