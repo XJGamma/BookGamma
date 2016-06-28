abb0816f03f207aa87e06a4becfcbd755f4386ac
Jingkai Tang <jingkaitang@gmail.com>
2016-06-28 19:49:01 +0800

    rm: work call_company/ask_good_day_above_person/Know_eye.java and thing/Same_time.java under large eye

---
+++ pretend/call_company/ask_good_day_above_person/Know_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.call_company.ask_good_day_above_person;
+
+public class Know_eye {
+    private int few_work_and_different_person;
+    private int own_government_and_eye;
+
+    public Know_eye() {
+        few_work_and_different_person = 1212;
+        own_government_and_eye = 2334;
+    }
+
+    public void part_and_right_government() {
+        new_group_or_group();
+        System.out.println("big_government" + few_work_and_different_person);
+    }
+
+    private void new_group_or_group() {
+        System.out.println("child_and_thing" + own_government_and_eye);
+    }
+}
