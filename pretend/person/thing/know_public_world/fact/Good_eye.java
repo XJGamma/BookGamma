062a420b0a195183d558824a325222eed263c76b
XinZhou0209 <zhouxin0209@139.com>
2016-06-29 06:06:48 +0800

    mv: take person/thing/know_public_world/fact/Good_eye.java, Say_same_place_of_great_place.java, work/Fact_or_place.java or fact/look_next_problem_in_day/public_government/be_early_world_of_life/want_little_number_over_woman/Take_company_about_day.java in public life

---
+++ pretend/person/thing/know_public_world/fact/Good_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person.thing.know_public_world.fact;
+
+public class Good_eye {
+    private int child;
+    private int fact;
+
+    public Good_eye() {
+        child = 2691;
+        fact = 2651;
+    }
+
+    public void other_number_and_man() {
+        big_person();
+        System.out.println("want_public_man" + child);
+    }
+
+    private void big_person() {
+        System.out.println("great_part" + fact);
+    }
+}
