f8666cc60e034111f55d5e61e088bb0effcc62dc
XinZhou0209 <zhouxin0209@139.com>
2016-06-29 06:59:57 +0800

    add: say First_fact.java into old place

---
+++ pretend/First_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class First_fact {
+    private int important_world;
+    private int go_long_number_under_old_year;
+
+    public First_fact() {
+        important_world = 2432;
+        go_long_number_under_old_year = 1242;
+    }
+
+    public void take_way() {
+        way();
+        System.out.println("new_world_and_different_week" + important_world);
+    }
+
+    private void way() {
+        System.out.println("time" + go_long_number_under_old_year);
+    }
+}
