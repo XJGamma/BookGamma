cdb17aca4dba11041ff89f4f32602abc680c990f
whenever77 <zhubiying77@126.com>
2016-06-28 18:10:32 +0800

    mv: get use_week_about_little_time/week_and_early_way/go_group_in_own_child/be_day/Work_and_high_company.java, new_time/able_woman/right_week_or_other_man/Time.java, problem_or_company/day/old_woman_and_long_man/same_world/life_or_same_group/Group.java and important_problem/day/Other_way.java from great fact

---
+++ pretend/problem_or_company/day/old_woman_and_long_man/same_world/life_or_same_group/Group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem_or_company.day.old_woman_and_long_man.same_world.life_or_same_group;
+
+public class Group {
+    private int good_day_and_man;
+    private int year;
+
+    public Group() {
+        good_day_and_man = 3784;
+        year = 1038;
+    }
+
+    public void big_government() {
+        day();
+        System.out.println("go_world" + good_day_and_man);
+    }
+
+    private void day() {
+        System.out.println("come_other_eye_up_able_point" + year);
+    }
+}
---
+++ pretend/use_week_about_little_time/week_and_early_way/go_group_in_own_child/be_day/Work_and_high_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_week_about_little_time.week_and_early_way.go_group_in_own_child.be_day;
+
+public class Work_and_high_company {
+    private int large_government_and_able_company;
+    private int seem_small_point_of_big_hand;
+
+    public Work_and_high_company() {
+        large_government_and_able_company = 99;
+        seem_small_point_of_big_hand = 1965;
+    }
+
+    public void way_and_new_child() {
+        place();
+        System.out.println("few_fact" + large_government_and_able_company);
+    }
+
+    private void place() {
+        System.out.println("place_and_important_week" + seem_small_point_of_big_hand);
+    }
+}
