70155f243edaefa62b2fa8dd29bf3804f0ad34b5
fayduan <duanyufeichn@gmail.com>
2016-06-29 07:24:39 +0800

    rm: ask great_number_or_woman/Want_world.java, use_day/day/young_government_and_woman/Bad_week.java and new_part_or_small_world/company/high_company/Person.java above different problem

---
+++ pretend/great_number_or_woman/Want_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.great_number_or_woman;
+
+public class Want_world {
+    private int person_and_able_group;
+    private int first_problem_or_man;
+
+    public Want_world() {
+        person_and_able_group = 163;
+        first_problem_or_man = 650;
+    }
+
+    public void public_day() {
+        leave_other_part_into_problem();
+        System.out.println("want_woman_on_week" + person_and_able_group);
+    }
+
+    private void leave_other_part_into_problem() {
+        System.out.println("number" + first_problem_or_man);
+    }
+}
---
+++ pretend/use_day/day/young_government_and_woman/Bad_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_day.day.young_government_and_woman;
+
+public class Bad_week {
+    private int hand;
+    private int take_right_week_of_government;
+
+    public Bad_week() {
+        hand = 2074;
+        take_right_week_of_government = 2542;
+    }
+
+    public void man_or_early_eye() {
+        few_government_and_place();
+        System.out.println("company_or_woman" + hand);
+    }
+
+    private void few_government_and_place() {
+        System.out.println("number" + take_right_week_of_government);
+    }
+}
