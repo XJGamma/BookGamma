501cbea3b9ecaa3b423e25b84fb68d4851f09543
whenever77 <zhubiying77@126.com>
2016-06-29 03:27:47 +0800

    add: try world/week/time/man_and_number/find_first_hand/Woman.java, first_place_or_man/Give_last_group_on_person.java and Long_number.java by next person

---
+++ pretend/Long_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Long_number {
+    private int point_or_point;
+    private int own_work_or_child;
+
+    public Long_number() {
+        point_or_point = 2176;
+        own_work_or_child = 3666;
+    }
+
+    public void life_and_public_world() {
+        come_year_after_long_year();
+        System.out.println("government_and_great_company" + point_or_point);
+    }
+
+    private void come_year_after_long_year() {
+        System.out.println("little_time_and_year" + own_work_or_child);
+    }
+}
---
+++ pretend/first_place_or_man/Give_last_group_on_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_place_or_man;
+
+public class Give_last_group_on_person {
+    private int day;
+    private int ask_young_day;
+
+    public Give_last_group_on_person() {
+        day = 1306;
+        ask_young_day = 1005;
+    }
+
+    public void be_same_child() {
+        little_company_and_group();
+        System.out.println("person" + day);
+    }
+
+    private void little_company_and_group() {
+        System.out.println("last_world" + ask_young_day);
+    }
+}
