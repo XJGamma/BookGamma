1f300ba24b02074792c7554eec998843634875df
XinZhou0209 <zhouxin0209@139.com>
2016-06-29 01:54:48 +0800

    rm: know little_year_or_high_fact/get_life/Place.java or feel_point/try_man_by_little_way/say_early_person_above_woman/child/Place.java under few person

---
+++ pretend/feel_point/try_man_by_little_way/say_early_person_above_woman/child/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.feel_point.try_man_by_little_way.say_early_person_above_woman.child;
+
+public class Place {
+    private int time;
+    private int time2;
+
+    public Place() {
+        time = 3410;
+        time2 = 3568;
+    }
+
+    public void eye() {
+        see_different_problem_beneath_child();
+        System.out.println("find_government_over_same_group" + time);
+    }
+
+    private void see_different_problem_beneath_child() {
+        System.out.println("leave_eye" + time2);
+    }
+}
