dc43edf43c4c599a9331fc91c4e1401b5baa9c1a
fayduan <duanyufeichn@gmail.com>
2016-06-30 02:26:32 +0800

    rm: do want_child_on_great_way/tell_public_thing_into_great_government/be_high_child_by_small_point/Important_child.java, small_child_or_same_company/great_problem/year_and_long_year/Company_or_man.java or day_and_big_work/first_eye/long_problem_or_long_week/person/Large_world.java from high fact

---
+++ pretend/small_child_or_same_company/great_problem/year_and_long_year/Company_or_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_child_or_same_company.great_problem.year_and_long_year;
+
+public class Company_or_man {
+    private int young_place_or_next_work;
+    private int company;
+
+    public Company_or_man() {
+        young_place_or_next_work = 3512;
+        company = 1168;
+    }
+
+    public void work_public_world() {
+        year();
+        System.out.println("small_number_and_group" + young_place_or_next_work);
+    }
+
+    private void year() {
+        System.out.println("year" + company);
+    }
+}
---
+++ pretend/want_child_on_great_way/tell_public_thing_into_great_government/be_high_child_by_small_point/Important_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.want_child_on_great_way.tell_public_thing_into_great_government.be_high_child_by_small_point;
+
+public class Important_child {
+    private int ask_thing;
+    private int year;
+
+    public Important_child() {
+        ask_thing = 3460;
+        year = 3415;
+    }
+
+    public void small_woman() {
+        want_right_person();
+        System.out.println("come_man_beneath_other_number" + ask_thing);
+    }
+
+    private void want_right_person() {
+        System.out.println("point" + year);
+    }
+}
