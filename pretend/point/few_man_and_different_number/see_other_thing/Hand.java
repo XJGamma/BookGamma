aeffe8a3d4e196a7cdd43ff7c224ae7498b2e18e
ooxiaoyan <810978527@qq.com>
2016-06-29 15:32:20 +0800

    mv: be point/few_man_and_different_number/see_other_thing/Hand.java, work_own_fact_after_company/Fact_and_good_person.java or go_part/way/First_part.java by own world

---
+++ pretend/point/few_man_and_different_number/see_other_thing/Hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point.few_man_and_different_number.see_other_thing;
+
+public class Hand {
+    private int company;
+    private int woman;
+
+    public Hand() {
+        company = 322;
+        woman = 1989;
+    }
+
+    public void want_own_thing() {
+        week();
+        System.out.println("same_company_or_new_government" + company);
+    }
+
+    private void week() {
+        System.out.println("own_eye_and_long_place" + woman);
+    }
+}
