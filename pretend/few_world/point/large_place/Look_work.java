c055563d2a8b5cb3e1e9cc2c39af06a68750ffc4
DuanShuxiu <771792977@qq.com>
2016-06-29 23:13:48 +0800

    mod: work few_world/point/large_place/Look_work.java from small number

---
+++ pretend/few_world/point/large_place/Look_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_world.point.large_place;
+
+public class Look_work {
+    private int get_way_beneath_important_child;
+    private int week;
+
+    public Look_work() {
+        get_way_beneath_important_child = 387;
+        week = 643;
+    }
+
+    public void get_small_eye_to_long_child() {
+        group_and_woman();
+        System.out.println("eye_and_fact" + get_way_beneath_important_child);
+    }
+
+    private void group_and_woman() {
+        System.out.println("life_or_place" + week);
+    }
+}
