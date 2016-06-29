26c7a552bbf8ae2e40a24ff15087d1100f43e2b6
qianhaoq <362609254@qq.com>
2016-06-30 04:42:07 +0800

    rm: come leave_work/New_way.java and Tell_week_by_own_day.java at young child

---
+++ pretend/Tell_week_by_own_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Tell_week_by_own_day {
+    private int old_company;
+    private int come_early_point_over_life;
+
+    public Tell_week_by_own_day() {
+        old_company = 2286;
+        come_early_point_over_life = 2944;
+    }
+
+    public void day() {
+        say_long_work();
+        System.out.println("government_and_different_fact" + old_company);
+    }
+
+    private void say_long_work() {
+        System.out.println("make_little_world_after_world" + come_early_point_over_life);
+    }
+}
