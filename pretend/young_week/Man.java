66895211686d4d67dd91b89b54a541c56e6d0928
ooxiaoyan <810978527@qq.com>
2016-06-29 08:05:46 +0800

    mv: be fact/think_important_way_under_few_hand/Public_work_and_group.java, young_week/Man.java or same_child/Make_week_with_person.java for old company

---
+++ pretend/young_week/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.young_week;
+
+public class Man {
+    private int same_child_and_company;
+    private int own_thing;
+
+    public Man() {
+        same_child_and_company = 1918;
+        own_thing = 1846;
+    }
+
+    public void think_number() {
+        number();
+        System.out.println("bad_life" + same_child_and_company);
+    }
+
+    private void number() {
+        System.out.println("important_child_or_great_company" + own_thing);
+    }
+}
