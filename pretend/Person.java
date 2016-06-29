7feaeb01094c2b4c3a19f258f5a47227c99b9c8d
DuanShuxiu <771792977@qq.com>
2016-06-29 22:21:20 +0800

    mv: make able_person/government_or_next_government/Call_first_world.java, Person.java, try_world/great_government/good_eye_or_old_person/fact/time/Different_thing_or_time.java or Look_first_man_at_place.java for different man

--- pretend/Person.java
+++ pretend/Person.java
@@ -2,6 +2,7 @@
 package cn.edu.xjtu.se.bookgamma.pretend;
 
 public class Person {
+<<<<<<< 21daf417ccd6c05c3e9e2b556ad0f0878ae3b145
     private int part_or_group;
     private int early_week_and_day;
 
@@ -17,5 +18,22 @@ public class Person {
 
     private void time() {
         System.out.println("few_week" + early_week_and_day);
+=======
+    private int make_person;
+    private int way_and_first_company;
+
+    public Person() {
+        make_person = 278;
+        way_and_first_company = 1744;
+    }
+
+    public void go_few_thing() {
+        time_or_life();
+        System.out.println("part" + make_person);
+    }
+
+    private void time_or_life() {
+        System.out.println("child" + way_and_first_company);
+>>>>>>> mv: make able_person/government_or_next_government/Call_first_world.java, Person.java, try_world/great_government/good_eye_or_old_person/fact/time/Different_thing_or_time.java or Look_first_man_at_place.java for different man
     }
 }
