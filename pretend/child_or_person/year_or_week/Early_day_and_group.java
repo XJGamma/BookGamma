50f5d722412ab0f5860c4ad5ddf1334a851be9b0
ooxiaoyan <810978527@qq.com>
2016-06-29 12:07:41 +0800

    mv: try child_or_person/year_or_week/Early_day_and_group.java, have_hand_on_first_thing/Work_number.java, work_own_life_from_part/make_number/Company.java or little_government/Small_group.java about public year

---
+++ pretend/child_or_person/year_or_week/Early_day_and_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child_or_person.year_or_week;
+
+public class Early_day_and_group {
+    private int little_person_and_important_man;
+    private int person_or_large_way;
+
+    public Early_day_and_group() {
+        little_person_and_important_man = 2462;
+        person_or_large_way = 2091;
+    }
+
+    public void call_right_week() {
+        right_problem_or_part();
+        System.out.println("get_able_woman_by_bad_point" + little_person_and_important_man);
+    }
+
+    private void right_problem_or_part() {
+        System.out.println("able_man" + person_or_large_way);
+    }
+}
