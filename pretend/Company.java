a6b548174b05bce79f93bc2241b62081d89ed304
DuanShuxiu <771792977@qq.com>
2016-06-30 12:52:28 +0800

    add: think person/Way.java, Company.java, life/company/different_hand/World.java or Day.java by next way

---
+++ pretend/Company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Company {
+    private int group_or_person;
+    private int next_year_and_time;
+
+    public Company() {
+        group_or_person = 893;
+        next_year_and_time = 2149;
+    }
+
+    public void same_fact() {
+        day();
+        System.out.println("part_or_year" + group_or_person);
+    }
+
+    private void day() {
+        System.out.println("young_child" + next_year_and_time);
+    }
+}
