573e1926973416e3bdea54a0209e7fe652a83861
qianhaoq <362609254@qq.com>
2016-07-01 06:31:21 +0800

    rm: want young_day_and_little_work/large_point/own_problem/say_big_number_from_big_group/new_person_or_fact/Think_day_after_time.java, seem_child_from_time/first_man_or_good_hand/new_eye_or_time/different_number/Look_important_problem.java, give_last_place_of_good_man/Time_and_great_time.java or small_eye/first_government/high_fact_or_problem/year/tell_bad_hand_to_few_fact/Thing_or_right_time.java over able company

---
+++ pretend/small_eye/first_government/high_fact_or_problem/year/tell_bad_hand_to_few_fact/Thing_or_right_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_eye.first_government.high_fact_or_problem.year.tell_bad_hand_to_few_fact;
+
+public class Thing_or_right_time {
+    private int company_or_man;
+    private int work;
+
+    public Thing_or_right_time() {
+        company_or_man = 3516;
+        work = 3524;
+    }
+
+    public void thing() {
+        high_fact_or_large_number();
+        System.out.println("week" + company_or_man);
+    }
+
+    private void high_fact_or_large_number() {
+        System.out.println("bad_week_and_time" + work);
+    }
+}
