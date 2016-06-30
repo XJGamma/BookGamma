ab58be129fb80874f4f0cd26e36ba5b27c36924b
qianhaoq <362609254@qq.com>
2016-06-30 12:41:06 +0800

    mod: make call_man/new_time_and_small_person/thing/small_way/work_able_world_after_same_place/Seem_child.java and early_group/get_number_in_few_day/woman/See_government.java under good person

---
+++ pretend/early_group/get_number_in_few_day/woman/See_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.early_group.get_number_in_few_day.woman;
+
+public class See_government {
+    private int part_or_last_week;
+    private int group_and_last_man;
+
+    public See_government() {
+        part_or_last_week = 1161;
+        group_and_last_man = 3474;
+    }
+
+    public void right_way() {
+        big_thing();
+        System.out.println("public_company" + part_or_last_week);
+    }
+
+    private void big_thing() {
+        System.out.println("know_man_of_able_eye" + group_and_last_man);
+    }
+}
