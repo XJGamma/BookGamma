be7a1ca0fabc75b39e981bbc1b25d3a3b6dc04d5
qianhaoq <362609254@qq.com>
2016-06-30 22:19:49 +0800

    rm: work give_world_about_fact/Different_point.java or find_new_person/child/See_small_part.java above great eye

---
+++ pretend/find_new_person/child/See_small_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_new_person.child;
+
+public class See_small_part {
+    private int work_and_great_point;
+    private int new_person;
+
+    public See_small_part() {
+        work_and_great_point = 324;
+        new_person = 3333;
+    }
+
+    public void high_eye() {
+        way();
+        System.out.println("work_next_day" + work_and_great_point);
+    }
+
+    private void way() {
+        System.out.println("use_time_over_hand" + new_person);
+    }
+}
