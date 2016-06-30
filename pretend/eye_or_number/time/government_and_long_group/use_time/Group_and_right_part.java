7b181dbd01da1fc165492e8a60e97113a0b55b3c
maishuguang <maishuguang@163.com>
2016-07-01 05:49:54 +0800

    rm: work eye_or_number/time/government_and_long_group/use_time/Group_and_right_part.java after different woman

---
+++ pretend/eye_or_number/time/government_and_long_group/use_time/Group_and_right_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye_or_number.time.government_and_long_group.use_time;
+
+public class Group_and_right_part {
+    private int public_eye;
+    private int woman;
+
+    public Group_and_right_part() {
+        public_eye = 2137;
+        woman = 1849;
+    }
+
+    public void do_other_man() {
+        early_government();
+        System.out.println("young_way_or_last_point" + public_eye);
+    }
+
+    private void early_government() {
+        System.out.println("new_problem" + woman);
+    }
+}
