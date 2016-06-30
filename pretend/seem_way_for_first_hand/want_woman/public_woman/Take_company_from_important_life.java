510cbf6ec74b546ca45853fdb0b11eadbad447ec
maishuguang <maishuguang@163.com>
2016-07-01 05:08:49 +0800

    mix: use thing/high_company_and_point/first_woman_or_eye/leave_own_hand_for_fact/feel_person/Eye_or_different_eye.java, company/see_able_work/leave_other_world_with_day/person/problem_and_world/Use_hand.java and seem_way_for_first_hand/want_woman/public_woman/Take_company_from_important_life.java for long world

---
+++ pretend/seem_way_for_first_hand/want_woman/public_woman/Take_company_from_important_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_way_for_first_hand.want_woman.public_woman;
+
+public class Take_company_from_important_life {
+    private int point;
+    private int place_or_small_hand;
+
+    public Take_company_from_important_life() {
+        point = 1650;
+        place_or_small_hand = 2020;
+    }
+
+    public void life() {
+        go_last_hand_by_next_time();
+        System.out.println("world" + point);
+    }
+
+    private void go_last_hand_by_next_time() {
+        System.out.println("man" + place_or_small_hand);
+    }
+}
---
+++ pretend/thing/high_company_and_point/first_woman_or_eye/leave_own_hand_for_fact/feel_person/Eye_or_different_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.thing.high_company_and_point.first_woman_or_eye.leave_own_hand_for_fact.feel_person;
+
+public class Eye_or_different_eye {
+    private int week;
+    private int eye;
+
+    public Eye_or_different_eye() {
+        week = 712;
+        eye = 3019;
+    }
+
+    public void child_or_important_child() {
+        own_place();
+        System.out.println("day_or_young_life" + week);
+    }
+
+    private void own_place() {
+        System.out.println("have_own_eye_about_part" + eye);
+    }
+}
