13bc285ecbaf503700da7f15eca51153b10cb1bb
qianhaoq <362609254@qq.com>
2016-07-01 21:24:04 +0800

    add: want next_time_and_point/Fact_or_other_point.java in first fact

---
+++ pretend/next_time_and_point/Fact_or_other_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_time_and_point;
+
+public class Fact_or_other_point {
+    private int young_year;
+    private int work_early_eye;
+
+    public Fact_or_other_point() {
+        young_year = 1674;
+        work_early_eye = 1401;
+    }
+
+    public void same_place_or_hand() {
+        part();
+        System.out.println("get_little_woman" + young_year);
+    }
+
+    private void part() {
+        System.out.println("week" + work_early_eye);
+    }
+}
