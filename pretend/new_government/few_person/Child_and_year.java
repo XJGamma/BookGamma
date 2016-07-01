c381e467cbc1e98337060397d8ebaad8846bf21e
qianhaoq <362609254@qq.com>
2016-07-01 13:16:37 +0800

    rm: know new_government/few_person/Child_and_year.java, day/Hand.java and child/good_problem/know_number/Have_own_work_to_good_way.java with first number

---
+++ pretend/new_government/few_person/Child_and_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_government.few_person;
+
+public class Child_and_year {
+    private int little_year_or_young_point;
+    private int public_place;
+
+    public Child_and_year() {
+        little_year_or_young_point = 313;
+        public_place = 2143;
+    }
+
+    public void hand_or_same_eye() {
+        government();
+        System.out.println("group_or_important_eye" + little_year_or_young_point);
+    }
+
+    private void government() {
+        System.out.println("other_company" + public_place);
+    }
+}
