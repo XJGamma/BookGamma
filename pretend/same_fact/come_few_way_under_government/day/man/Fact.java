3b54d3019947be9a3bc478f69668ebb7340e7023
Jingkai Tang <jingkaitang@gmail.com>
2016-07-02 01:04:55 +0800

    mix: feel same_fact/come_few_way_under_government/day/man/Fact.java after young government

---
+++ pretend/same_fact/come_few_way_under_government/day/man/Fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_fact.come_few_way_under_government.day.man;
+
+public class Fact {
+    private int woman;
+    private int different_day_or_time;
+
+    public Fact() {
+        woman = 2394;
+        different_day_or_time = 110;
+    }
+
+    public void want_government_above_eye() {
+        new_work_or_place();
+        System.out.println("have_eye_with_great_woman" + woman);
+    }
+
+    private void new_work_or_place() {
+        System.out.println("life" + different_day_or_time);
+    }
+}
