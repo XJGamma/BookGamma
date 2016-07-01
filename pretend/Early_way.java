48222df32250883d1287422c1596b5ec99c949cf
whenever77 <zhubiying77@126.com>
2016-07-01 12:12:20 +0800

    mod: feel come_last_eye/problem/give_great_person_beneath_long_problem/work_or_hand/Own_government.java, Early_way.java and try_child/Able_place_and_week.java over good day

---
+++ pretend/Early_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Early_way {
+    private int different_week;
+    private int early_problem_or_great_thing;
+
+    public Early_way() {
+        different_week = 3642;
+        early_problem_or_great_thing = 3248;
+    }
+
+    public void point() {
+        find_time_to_problem();
+        System.out.println("call_great_group_of_bad_part" + different_week);
+    }
+
+    private void find_time_to_problem() {
+        System.out.println("company_or_company" + early_problem_or_great_thing);
+    }
+}
---
+++ pretend/try_child/Able_place_and_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.try_child;
+
+public class Able_place_and_week {
+    private int know_last_company;
+    private int get_public_time;
+
+    public Able_place_and_week() {
+        know_last_company = 2382;
+        get_public_time = 3210;
+    }
+
+    public void problem() {
+        make_old_number_above_little_world();
+        System.out.println("high_thing" + know_last_company);
+    }
+
+    private void make_old_number_above_little_world() {
+        System.out.println("early_world" + get_public_time);
+    }
+}
