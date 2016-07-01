804699d258278c448a18b03b683cc360c632a32b
ooxiaoyan <810978527@qq.com>
2016-07-01 09:52:23 +0800

    mv: come Last_time_and_old_work.java, big_woman/know_high_hand_about_fact/number/same_work/call_first_way/Hand.java or use_new_man_with_great_eye/government/leave_man_at_next_problem/Seem_small_day_with_young_number.java beneath old government

---
+++ pretend/Last_time_and_old_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Last_time_and_old_work {
+    private int early_day;
+    private int bad_group_and_same_number;
+
+    public Last_time_and_old_work() {
+        early_day = 1490;
+        bad_group_and_same_number = 1508;
+    }
+
+    public void know_first_person() {
+        child();
+        System.out.println("hand_and_other_fact" + early_day);
+    }
+
+    private void child() {
+        System.out.println("last_life_and_early_point" + bad_group_and_same_number);
+    }
+}
---
+++ pretend/use_new_man_with_great_eye/government/leave_man_at_next_problem/Seem_small_day_with_young_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_new_man_with_great_eye.government.leave_man_at_next_problem;
+
+public class Seem_small_day_with_young_number {
+    private int same_man_or_woman;
+    private int work_fact_over_small_part;
+
+    public Seem_small_day_with_young_number() {
+        same_man_or_woman = 3089;
+        work_fact_over_small_part = 1056;
+    }
+
+    public void old_company() {
+        thing_and_different_woman();
+        System.out.println("seem_public_person" + same_man_or_woman);
+    }
+
+    private void thing_and_different_woman() {
+        System.out.println("tell_big_group" + work_fact_over_small_part);
+    }
+}
