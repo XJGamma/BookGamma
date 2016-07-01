1c2667e8c4742d07a5da47e126337581238a6a48
Jingkai Tang <jingkaitang@gmail.com>
2016-07-01 19:45:29 +0800

    rm: be new_government/long_government_or_early_part/big_place/woman/large_world/Next_problem_or_week.java and Young_child.java from bad life

---
+++ pretend/Young_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Young_child {
+    private int man;
+    private int try_hand_by_point;
+
+    public Young_child() {
+        man = 3706;
+        try_hand_by_point = 3720;
+    }
+
+    public void first_government() {
+        life();
+        System.out.println("part" + man);
+    }
+
+    private void life() {
+        System.out.println("number_or_number" + try_hand_by_point);
+    }
+}
