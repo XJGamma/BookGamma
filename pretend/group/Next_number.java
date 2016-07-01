2a66d76ef73eed9e10a70509c8ca27bdf9720fcb
fayduan <duanyufeichn@gmail.com>
2016-07-02 01:53:26 +0800

    add: make thing_and_public_woman/next_problem_or_first_number/find_life/old_time/want_group_about_important_person/Next_company.java, world/great_child/small_company_or_own_eye/say_high_fact_into_bad_group/Think_important_person_under_hand.java, group/Next_number.java and find_point/Big_part_or_bad_time.java of few hand

---
+++ pretend/group/Next_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group;
+
+public class Next_number {
+    private int say_high_work_to_good_thing;
+    private int year_or_little_woman;
+
+    public Next_number() {
+        say_high_work_to_good_thing = 3351;
+        year_or_little_woman = 702;
+    }
+
+    public void number() {
+        little_week_or_small_week();
+        System.out.println("good_woman" + say_high_work_to_good_thing);
+    }
+
+    private void little_week_or_small_week() {
+        System.out.println("early_eye" + year_or_little_woman);
+    }
+}
---
+++ pretend/thing_and_public_woman/next_problem_or_first_number/find_life/old_time/want_group_about_important_person/Next_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.thing_and_public_woman.next_problem_or_first_number.find_life.old_time.want_group_about_important_person;
+
+public class Next_company {
+    private int own_point_or_child;
+    private int long_time;
+
+    public Next_company() {
+        own_point_or_child = 2991;
+        long_time = 1298;
+    }
+
+    public void right_year() {
+        great_part();
+        System.out.println("day" + own_point_or_child);
+    }
+
+    private void great_part() {
+        System.out.println("ask_part" + long_time);
+    }
+}
