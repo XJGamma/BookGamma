6998f58220a009cb3094d71dbcd2fb3326e2e04e
DuanShuxiu <771792977@qq.com>
2016-07-02 19:21:29 +0800

    rm: be Group.java, Person_or_early_group.java, Tell_company_beneath_next_thing.java or woman/great_eye/Big_hand.java on first man

---
+++ pretend/Group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Group {
+    private int bad_government_or_big_week;
+    private int time_or_fact;
+
+    public Group() {
+        bad_government_or_big_week = 332;
+        time_or_fact = 3554;
+    }
+
+    public void look_government_into_bad_week() {
+        fact();
+        System.out.println("try_other_person" + bad_government_or_big_week);
+    }
+
+    private void fact() {
+        System.out.println("life" + time_or_fact);
+    }
+}
---
+++ pretend/Person_or_early_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Person_or_early_group {
+    private int next_time_and_day;
+    private int right_eye;
+
+    public Person_or_early_group() {
+        next_time_and_day = 2380;
+        right_eye = 936;
+    }
+
+    public void person() {
+        able_man();
+        System.out.println("way" + next_time_and_day);
+    }
+
+    private void able_man() {
+        System.out.println("year_and_big_hand" + right_eye);
+    }
+}
