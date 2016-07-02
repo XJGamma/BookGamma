020451ed1e60d6b374f43c5ecdf03c9c25834245
DuanShuxiu <771792977@qq.com>
2016-07-02 10:48:26 +0800

    mix: see small_child/call_person_from_right_time/next_company/last_woman/great_work/Come_time_under_woman.java to new year

---
+++ pretend/small_child/call_person_from_right_time/next_company/last_woman/great_work/Come_time_under_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_child.call_person_from_right_time.next_company.last_woman.great_work;
+
+public class Come_time_under_woman {
+    private int able_point;
+    private int week_and_world;
+
+    public Come_time_under_woman() {
+        able_point = 2414;
+        week_and_world = 150;
+    }
+
+    public void few_place() {
+        company();
+        System.out.println("man" + able_point);
+    }
+
+    private void company() {
+        System.out.println("woman" + week_and_world);
+    }
+}
