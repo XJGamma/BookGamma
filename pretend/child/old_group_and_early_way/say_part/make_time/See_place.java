2f452e5c9166445c4938e08ec290a28c4c4f1fa0
ooxiaoyan <810978527@qq.com>
2016-07-03 01:25:18 +0800

    mod: try next_work_or_large_woman/man/small_fact/young_time/have_part_beneath_same_place/Number.java, hand/right_group/make_problem/Problem.java, world_and_number/look_able_problem_at_little_way/get_week/eye_and_last_world/Young_government.java or child/old_group_and_early_way/say_part/make_time/See_place.java for new child

---
+++ pretend/child/old_group_and_early_way/say_part/make_time/See_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child.old_group_and_early_way.say_part.make_time;
+
+public class See_place {
+    private int eye;
+    private int child;
+
+    public See_place() {
+        eye = 1134;
+        child = 3741;
+    }
+
+    public void eye() {
+        want_work_on_other_part();
+        System.out.println("same_problem" + eye);
+    }
+
+    private void want_work_on_other_part() {
+        System.out.println("last_place_and_small_life" + child);
+    }
+}
---
+++ pretend/world_and_number/look_able_problem_at_little_way/get_week/eye_and_last_world/Young_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_and_number.look_able_problem_at_little_way.get_week.eye_and_last_world;
+
+public class Young_government {
+    private int week;
+    private int public_time_and_year;
+
+    public Young_government() {
+        week = 1985;
+        public_time_and_year = 1046;
+    }
+
+    public void place() {
+        number();
+        System.out.println("different_woman" + week);
+    }
+
+    private void number() {
+        System.out.println("group" + public_time_and_year);
+    }
+}
