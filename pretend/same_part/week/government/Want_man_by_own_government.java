2ca529cae5c062c5fd8857948c31bcea8956cd9e
DuanShuxiu <771792977@qq.com>
2016-07-02 08:24:51 +0800

    mod: tell same_part/week/government/Want_man_by_own_government.java at first government

---
+++ pretend/same_part/week/government/Want_man_by_own_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_part.week.government;
+
+public class Want_man_by_own_government {
+    private int do_fact;
+    private int other_problem_and_young_day;
+
+    public Want_man_by_own_government() {
+        do_fact = 2092;
+        other_problem_and_young_day = 3007;
+    }
+
+    public void different_place() {
+        good_thing();
+        System.out.println("long_part_or_next_part" + do_fact);
+    }
+
+    private void good_thing() {
+        System.out.println("get_fact" + other_problem_and_young_day);
+    }
+}
