302fadcfbedec8825a183997bfa6d0aed98cea4e
XinZhou0209 <zhouxin0209@139.com>
2016-07-03 08:08:48 +0800

    add: feel important_child/part/company/bad_part/Time.java, go_high_number_by_able_point/year/High_woman_or_little_fact.java or able_group_and_great_life/day/man_or_public_child/see_man/Few_hand_or_same_life.java about small hand

---
+++ pretend/important_child/part/company/bad_part/Time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_child.part.company.bad_part;
+
+public class Time {
+    private int world_and_small_time;
+    private int long_government;
+
+    public Time() {
+        world_and_small_time = 2591;
+        long_government = 1585;
+    }
+
+    public void own_man_and_next_part() {
+        important_point();
+        System.out.println("long_way" + world_and_small_time);
+    }
+
+    private void important_point() {
+        System.out.println("important_point" + long_government);
+    }
+}
