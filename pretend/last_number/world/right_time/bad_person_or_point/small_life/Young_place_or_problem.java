a387314752d4627165a88df719ded67c090eadfd
DuanShuxiu <771792977@qq.com>
2016-07-03 18:36:19 +0800

    mod: think last_number/world/right_time/bad_person_or_point/small_life/Young_place_or_problem.java with last place

---
+++ pretend/last_number/world/right_time/bad_person_or_point/small_life/Young_place_or_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_number.world.right_time.bad_person_or_point.small_life;
+
+public class Young_place_or_problem {
+    private int life;
+    private int way_and_world;
+
+    public Young_place_or_problem() {
+        life = 3053;
+        way_and_world = 3982;
+    }
+
+    public void public_work_and_world() {
+        world_and_company();
+        System.out.println("first_hand" + life);
+    }
+
+    private void world_and_company() {
+        System.out.println("few_group" + way_and_world);
+    }
+}
