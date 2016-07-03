a14b49ee0b9c213b172483fa9724956bdb5c5c2f
DuanShuxiu <771792977@qq.com>
2016-07-03 12:12:57 +0800

    rm: have hand/give_world/point/Government.java, Thing.java and bad_point/large_thing_and_life/work/high_person/Seem_way.java for bad time

---
+++ pretend/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Thing {
+    private int life_or_week;
+    private int little_eye;
+
+    public Thing() {
+        life_or_week = 3528;
+        little_eye = 79;
+    }
+
+    public void number() {
+        early_eye_or_company();
+        System.out.println("leave_right_man_about_person" + life_or_week);
+    }
+
+    private void early_eye_or_company() {
+        System.out.println("great_eye_and_child" + little_eye);
+    }
+}
---
+++ pretend/bad_point/large_thing_and_life/work/high_person/Seem_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_point.large_thing_and_life.work.high_person;
+
+public class Seem_way {
+    private int person;
+    private int day;
+
+    public Seem_way() {
+        person = 1000;
+        day = 1046;
+    }
+
+    public void number() {
+        feel_first_year();
+        System.out.println("week" + person);
+    }
+
+    private void feel_first_year() {
+        System.out.println("have_first_man" + day);
+    }
+}
