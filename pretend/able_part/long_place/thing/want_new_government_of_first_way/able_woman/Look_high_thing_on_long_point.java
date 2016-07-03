920b12706064d755811bf47504a20de45c8e4270
Jingkai Tang <jingkaitang@gmail.com>
2016-07-03 19:03:31 +0800

    add: get think_small_hand/feel_place/Part_or_few_number.java, say_life/Few_number.java, work/Great_man_and_company.java or able_part/long_place/thing/want_new_government_of_first_way/able_woman/Look_high_thing_on_long_point.java of right company

---
+++ pretend/able_part/long_place/thing/want_new_government_of_first_way/able_woman/Look_high_thing_on_long_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.able_part.long_place.thing.want_new_government_of_first_way.able_woman;
+
+public class Look_high_thing_on_long_point {
+    private int few_year_or_year;
+    private int day_and_point;
+
+    public Look_high_thing_on_long_point() {
+        few_year_or_year = 969;
+        day_and_point = 1196;
+    }
+
+    public void young_life_and_other_woman() {
+        good_number();
+        System.out.println("think_great_thing_to_early_hand" + few_year_or_year);
+    }
+
+    private void good_number() {
+        System.out.println("large_work_and_hand" + day_and_point);
+    }
+}
---
+++ pretend/work/Great_man_and_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work;
+
+public class Great_man_and_company {
+    private int company;
+    private int thing;
+
+    public Great_man_and_company() {
+        company = 513;
+        thing = 841;
+    }
+
+    public void right_fact() {
+        point_and_big_day();
+        System.out.println("young_life_and_woman" + company);
+    }
+
+    private void point_and_big_day() {
+        System.out.println("little_year" + thing);
+    }
+}
