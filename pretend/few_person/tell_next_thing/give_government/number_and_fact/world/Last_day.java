2e63fbb351a7af32ab6452da3fbdcf7a463ee5bd
DuanShuxiu <771792977@qq.com>
2016-07-04 14:30:59 +0800

    mix: say few_person/tell_next_thing/give_government/number_and_fact/world/Last_day.java or Time.java with large child

---
+++ pretend/few_person/tell_next_thing/give_government/number_and_fact/world/Last_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_person.tell_next_thing.give_government.number_and_fact.world;
+
+public class Last_day {
+    private int able_person;
+    private int give_next_thing_into_part;
+
+    public Last_day() {
+        able_person = 467;
+        give_next_thing_into_part = 363;
+    }
+
+    public void next_fact() {
+        be_way();
+        System.out.println("have_thing" + able_person);
+    }
+
+    private void be_way() {
+        System.out.println("old_thing_or_place" + give_next_thing_into_part);
+    }
+}
