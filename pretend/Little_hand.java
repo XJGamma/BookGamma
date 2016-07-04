589f0d5602ce269acbf8c00aa09ae960586eab5e
maishuguang <maishuguang@163.com>
2016-07-04 17:42:18 +0800

    mod: do Little_hand.java or last_person/Hand.java for old day

---
+++ pretend/Little_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Little_hand {
+    private int group_and_man;
+    private int important_person_and_good_way;
+
+    public Little_hand() {
+        group_and_man = 441;
+        important_person_and_good_way = 2842;
+    }
+
+    public void different_work() {
+        other_person();
+        System.out.println("own_eye" + group_and_man);
+    }
+
+    private void other_person() {
+        System.out.println("new_company" + important_person_and_good_way);
+    }
+}
---
+++ pretend/last_person/Hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_person;
+
+public class Hand {
+    private int thing;
+    private int own_week_and_great_week;
+
+    public Hand() {
+        thing = 1654;
+        own_week_and_great_week = 1531;
+    }
+
+    public void group_and_last_hand() {
+        world();
+        System.out.println("right_person" + thing);
+    }
+
+    private void world() {
+        System.out.println("government" + own_week_and_great_week);
+    }
+}
