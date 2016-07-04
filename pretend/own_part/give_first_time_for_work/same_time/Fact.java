f4bc562a9abce9cbc1edc27a0ec36f490581e659
ooxiaoyan <810978527@qq.com>
2016-07-04 22:04:56 +0800

    add: give next_week/see_first_woman/feel_company_beneath_world/go_thing_from_child/new_child/Group.java and own_part/give_first_time_for_work/same_time/Fact.java over few person

---
+++ pretend/own_part/give_first_time_for_work/same_time/Fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.own_part.give_first_time_for_work.same_time;
+
+public class Fact {
+    private int important_eye;
+    private int way;
+
+    public Fact() {
+        important_eye = 2613;
+        way = 552;
+    }
+
+    public void come_thing() {
+        early_point_or_way();
+        System.out.println("few_eye_or_company" + important_eye);
+    }
+
+    private void early_point_or_way() {
+        System.out.println("small_number_or_hand" + way);
+    }
+}
