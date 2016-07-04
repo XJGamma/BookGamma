257c291d130ccc4b28780b5f5b6cc7d8ef9fe2c1
ooxiaoyan <810978527@qq.com>
2016-07-04 21:36:12 +0800

    add: feel part_and_child/person/take_day/Way_and_few_number.java, go_child_to_right_hand/Big_company.java or good_work/public_company/Look_last_eye.java over different number

---
+++ pretend/go_child_to_right_hand/Big_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_child_to_right_hand;
+
+public class Big_company {
+    private int young_woman;
+    private int small_part;
+
+    public Big_company() {
+        young_woman = 2010;
+        small_part = 3733;
+    }
+
+    public void small_place() {
+        get_high_problem_by_high_company();
+        System.out.println("woman_and_great_year" + young_woman);
+    }
+
+    private void get_high_problem_by_high_company() {
+        System.out.println("go_next_day" + small_part);
+    }
+}
---
+++ pretend/good_work/public_company/Look_last_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_work.public_company;
+
+public class Look_last_eye {
+    private int week_and_able_person;
+    private int fact_and_group;
+
+    public Look_last_eye() {
+        week_and_able_person = 731;
+        fact_and_group = 1259;
+    }
+
+    public void call_large_eye_at_early_world() {
+        long_time();
+        System.out.println("way_or_long_week" + week_and_able_person);
+    }
+
+    private void long_time() {
+        System.out.println("big_company" + fact_and_group);
+    }
+}
