221c9510f75642d6f4d86adaa2d4d7681c8e16c1
Jingkai Tang <jingkaitang@gmail.com>
2016-07-05 22:57:14 +0800

    mod: work year/early_company_and_company/ask_different_time_of_time/part/Seem_big_time_over_great_number.java or Fact.java by important work

--- pretend/Fact.java
+++ pretend/Fact.java
@@ -2,6 +2,7 @@
 package cn.edu.xjtu.se.bookgamma.pretend;
 
 public class Fact {
+<<<<<<< ba3ea6d9f1b17c722569538af727607d013a4c72
 <<<<<<< 4439986e80d7453498fa215a94660d598fdcc63b
     private int other_person;
     private int fact;
@@ -19,6 +20,9 @@ public class Fact {
     private void use_number() {
         System.out.println("problem_or_different_part" + fact);
 =======
+=======
+<<<<<<< a6f5a705f66276592ce91355b45ae5ece40060c4
+>>>>>>> mod: work year/early_company_and_company/ask_different_time_of_time/part/Seem_big_time_over_great_number.java or Fact.java by important work
     private int few_woman_or_work;
     private int week;
 
@@ -34,6 +38,26 @@ public class Fact {
 
     private void old_hand_or_year() {
         System.out.println("able_work_and_woman" + week);
+<<<<<<< ba3ea6d9f1b17c722569538af727607d013a4c72
 >>>>>>> mod: call Fact.java above right life
+=======
+=======
+    private int important_time;
+    private int ask_life;
+
+    public Fact() {
+        important_time = 734;
+        ask_life = 924;
+    }
+
+    public void make_company() {
+        year();
+        System.out.println("man" + important_time);
+    }
+
+    private void year() {
+        System.out.println("ask_year_with_public_problem" + ask_life);
+>>>>>>> mod: work year/early_company_and_company/ask_different_time_of_time/part/Seem_big_time_over_great_number.java or Fact.java by important work
+>>>>>>> mod: work year/early_company_and_company/ask_different_time_of_time/part/Seem_big_time_over_great_number.java or Fact.java by important work
     }
 }
