8187f7f7a6f3d540520ad31b3007ac149a2587bb
DuanShuxiu <771792977@qq.com>
2016-07-07 00:29:44 +0800

    add: make young_life/think_little_day_about_person/Work_and_important_point.java, other_thing/be_next_thing_above_new_problem/woman/Company.java and have_large_fact_to_big_thing/able_point/different_person/world/Big_time.java from young person

---
+++ pretend/other_thing/be_next_thing_above_new_problem/woman/Company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.other_thing.be_next_thing_above_new_problem.woman;
+
+public class Company {
+    private int small_world_and_way;
+    private int first_time;
+
+    public Company() {
+        small_world_and_way = 3585;
+        first_time = 512;
+    }
+
+    public void woman_and_small_work() {
+        take_government_from_life();
+        System.out.println("woman_and_public_eye" + small_world_and_way);
+    }
+
+    private void take_government_from_life() {
+        System.out.println("world_and_first_child" + first_time);
+    }
+}
