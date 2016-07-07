7127f9423ac353c14c33f47dd59d6d386a5c6538
DuanShuxiu <771792977@qq.com>
2016-07-07 14:42:20 +0800

    mv: seem good_company_and_great_government/government/Person_or_great_week.java, man/Next_hand.java and work/tell_man/ask_week_with_time/think_good_government/Want_small_eye.java into able person

---
+++ pretend/good_company_and_great_government/government/Person_or_great_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_company_and_great_government.government;
+
+public class Person_or_great_week {
+    private int find_government;
+    private int part;
+
+    public Person_or_great_week() {
+        find_government = 3434;
+        part = 2079;
+    }
+
+    public void time() {
+        different_man();
+        System.out.println("group" + find_government);
+    }
+
+    private void different_man() {
+        System.out.println("next_week" + part);
+    }
+}
---
+++ pretend/man/Next_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man;
+
+public class Next_hand {
+    private int come_big_world;
+    private int same_way;
+
+    public Next_hand() {
+        come_big_world = 4045;
+        same_way = 2672;
+    }
+
+    public void child_or_week() {
+        able_company();
+        System.out.println("few_day_and_week" + come_big_world);
+    }
+
+    private void able_company() {
+        System.out.println("have_small_company" + same_way);
+    }
+}
