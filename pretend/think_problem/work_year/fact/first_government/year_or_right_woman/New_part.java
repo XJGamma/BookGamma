fa8cdb84e4af391ddb7cdd0996ed24a05472e850
ooxiaoyan <810978527@qq.com>
2016-07-08 03:54:22 +0800

    rm: get think_problem/work_year/fact/first_government/year_or_right_woman/New_part.java and person/hand/week_and_fact/Great_government.java on old thing

---
+++ pretend/think_problem/work_year/fact/first_government/year_or_right_woman/New_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.think_problem.work_year.fact.first_government.year_or_right_woman;
+
+public class New_part {
+    private int work_work;
+    private int person_or_large_week;
+
+    public New_part() {
+        work_work = 2319;
+        person_or_large_week = 418;
+    }
+
+    public void important_place_or_way() {
+        point_and_government();
+        System.out.println("eye" + work_work);
+    }
+
+    private void point_and_government() {
+        System.out.println("want_government" + person_or_large_week);
+    }
+}
