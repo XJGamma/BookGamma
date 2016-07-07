3168c5cb94527558a88212c17966ff8fcba43919
DuanShuxiu <771792977@qq.com>
2016-07-07 09:14:12 +0800

    rm: know group_and_little_number/different_work/problem/number/own_child_or_person/See_public_government.java or Important_way.java up important woman

---
+++ pretend/Important_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Important_way {
+    private int number;
+    private int new_day;
+
+    public Important_way() {
+        number = 2478;
+        new_day = 4078;
+    }
+
+    public void work_day_into_life() {
+        young_world_or_world();
+        System.out.println("person_and_public_problem" + number);
+    }
+
+    private void young_world_or_world() {
+        System.out.println("company" + new_day);
+    }
+}
---
+++ pretend/group_and_little_number/different_work/problem/number/own_child_or_person/See_public_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group_and_little_number.different_work.problem.number.own_child_or_person;
+
+public class See_public_government {
+    private int think_early_week_on_time;
+    private int want_own_problem;
+
+    public See_public_government() {
+        think_early_week_on_time = 412;
+        want_own_problem = 122;
+    }
+
+    public void child() {
+        first_thing_and_day();
+        System.out.println("go_small_time_after_able_point" + think_early_week_on_time);
+    }
+
+    private void first_thing_and_day() {
+        System.out.println("child" + want_own_problem);
+    }
+}
