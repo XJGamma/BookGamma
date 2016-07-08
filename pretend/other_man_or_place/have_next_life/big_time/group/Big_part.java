607c540fcd0cc6aa4c9391d278ceace4f654f8fe
maishuguang <maishuguang@163.com>
2016-07-09 07:05:24 +0800

    rm: take use_next_work_into_world/year/great_world/seem_eye_to_year/company_or_bad_point/Bad_hand.java, tell_first_time/try_child_from_own_week/different_way/big_year_or_first_day/Seem_child.java, other_man_or_place/have_next_life/big_time/group/Big_part.java or Person_and_government.java with bad life

---
+++ pretend/other_man_or_place/have_next_life/big_time/group/Big_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.other_man_or_place.have_next_life.big_time.group;
+
+public class Big_part {
+    private int tell_important_day;
+    private int next_day_or_small_group;
+
+    public Big_part() {
+        tell_important_day = 1731;
+        next_day_or_small_group = 2737;
+    }
+
+    public void part() {
+        number_or_good_problem();
+        System.out.println("right_life_or_child" + tell_important_day);
+    }
+
+    private void number_or_good_problem() {
+        System.out.println("man" + next_day_or_small_group);
+    }
+}
---
+++ pretend/use_next_work_into_world/year/great_world/seem_eye_to_year/company_or_bad_point/Bad_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_next_work_into_world.year.great_world.seem_eye_to_year.company_or_bad_point;
+
+public class Bad_hand {
+    private int woman;
+    private int week_or_way;
+
+    public Bad_hand() {
+        woman = 2585;
+        week_or_way = 2184;
+    }
+
+    public void thing() {
+        problem();
+        System.out.println("next_life_or_same_part" + woman);
+    }
+
+    private void problem() {
+        System.out.println("large_company" + week_or_way);
+    }
+}
