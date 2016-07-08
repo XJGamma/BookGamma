238caa0a9277c611ed4d3ef3258c743204d6308c
fayduan <duanyufeichn@gmail.com>
2016-07-09 06:34:07 +0800

    add: see ask_first_company_by_large_year/Look_right_fact.java, public_year/small_group/day_or_large_year/World.java or great_part/Child_or_public_week.java from great day

---
+++ pretend/public_year/small_group/day_or_large_year/World.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_year.small_group.day_or_large_year;
+
+public class World {
+    private int way;
+    private int place;
+
+    public World() {
+        way = 1305;
+        place = 845;
+    }
+
+    public void week() {
+        person_or_good_part();
+        System.out.println("get_world" + way);
+    }
+
+    private void person_or_good_part() {
+        System.out.println("early_government" + place);
+    }
+}
