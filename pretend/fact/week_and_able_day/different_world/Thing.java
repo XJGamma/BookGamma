2df8b4781d28a85310c1456773f4fffda8ab82e0
XinZhou0209 <zhouxin0209@139.com>
2016-07-08 08:32:21 +0800

    rm: make important_work_or_able_thing/want_important_part_above_great_way/Government_and_little_problem.java or fact/week_and_able_day/different_world/Thing.java about different problem

---
+++ pretend/fact/week_and_able_day/different_world/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact.week_and_able_day.different_world;
+
+public class Thing {
+    private int last_fact;
+    private int right_world;
+
+    public Thing() {
+        last_fact = 2902;
+        right_world = 1171;
+    }
+
+    public void way_and_large_day() {
+        have_right_place_of_way();
+        System.out.println("eye" + last_fact);
+    }
+
+    private void have_right_place_of_way() {
+        System.out.println("think_small_woman" + right_world);
+    }
+}
