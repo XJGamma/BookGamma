4039399133ca4eb02d300937fcb7b27d21f74775
ooxiaoyan <810978527@qq.com>
2016-07-09 02:26:10 +0800

    rm: make Own_hand.java and world_or_large_government/week/Little_year_or_different_hand.java into own thing

---
+++ pretend/world_or_large_government/week/Little_year_or_different_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_or_large_government.week;
+
+public class Little_year_or_different_hand {
+    private int own_part;
+    private int man;
+
+    public Little_year_or_different_hand() {
+        own_part = 703;
+        man = 135;
+    }
+
+    public void work_place() {
+        woman();
+        System.out.println("first_point" + own_part);
+    }
+
+    private void woman() {
+        System.out.println("man" + man);
+    }
+}
