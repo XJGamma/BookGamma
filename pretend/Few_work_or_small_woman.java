681fbcae1c6e51d48a6bffd5f7d8afc26bfbefe0
qianhaoq <362609254@qq.com>
2016-07-09 16:45:21 +0800

    rm: go long_hand_and_man/use_public_company_from_able_eye/Be_work.java or Few_work_or_small_woman.java over new way

---
+++ pretend/Few_work_or_small_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Few_work_or_small_woman {
+    private int work_good_work_for_eye;
+    private int leave_great_eye;
+
+    public Few_work_or_small_woman() {
+        work_good_work_for_eye = 2812;
+        leave_great_eye = 2622;
+    }
+
+    public void tell_new_fact() {
+        week_or_first_man();
+        System.out.println("hand_and_year" + work_good_work_for_eye);
+    }
+
+    private void week_or_first_man() {
+        System.out.println("make_life_under_few_number" + leave_great_eye);
+    }
+}
