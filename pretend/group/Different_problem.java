48dfd4ca64be10b9261a9740e6f1bba5dbf9a385
DuanShuxiu <771792977@qq.com>
2016-07-09 16:12:09 +0800

    mix: feel right_point/new_life/Problem.java and group/Different_problem.java about new day

---
+++ pretend/group/Different_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group;
+
+public class Different_problem {
+    private int time;
+    private int person;
+
+    public Different_problem() {
+        time = 1279;
+        person = 881;
+    }
+
+    public void long_child() {
+        work();
+        System.out.println("person_and_first_person" + time);
+    }
+
+    private void work() {
+        System.out.println("man" + person);
+    }
+}
---
+++ pretend/right_point/new_life/Problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_point.new_life;
+
+public class Problem {
+    private int own_day;
+    private int able_company_and_right_part;
+
+    public Problem() {
+        own_day = 1128;
+        able_company_and_right_part = 2980;
+    }
+
+    public void way() {
+        long_work();
+        System.out.println("place_and_week" + own_day);
+    }
+
+    private void long_work() {
+        System.out.println("week" + able_company_and_right_part);
+    }
+}
