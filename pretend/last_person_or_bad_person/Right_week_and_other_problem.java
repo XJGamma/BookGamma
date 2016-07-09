77232c9d1820c3bd6e6d20cc05a135cf018c3562
ooxiaoyan <810978527@qq.com>
2016-07-09 08:11:35 +0800

    rm: use big_company/group/company_or_day/other_man/long_time_and_few_fact/Give_place_for_own_world.java, child_or_hand/world_and_next_life/old_eye/try_number/hand/Ask_hand_after_woman.java, Public_fact_and_own_point.java or last_person_or_bad_person/Right_week_and_other_problem.java beneath new eye

---
+++ pretend/last_person_or_bad_person/Right_week_and_other_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_person_or_bad_person;
+
+public class Right_week_and_other_problem {
+    private int fact;
+    private int come_early_point;
+
+    public Right_week_and_other_problem() {
+        fact = 3276;
+        come_early_point = 291;
+    }
+
+    public void government() {
+        different_company();
+        System.out.println("small_place_and_week" + fact);
+    }
+
+    private void different_company() {
+        System.out.println("say_early_group" + come_early_point);
+    }
+}
