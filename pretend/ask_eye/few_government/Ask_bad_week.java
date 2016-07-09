7135a6d8982167b570d626bcca67e919c2520cc0
DuanShuxiu <771792977@qq.com>
2016-07-09 17:11:02 +0800

    mix: think ask_eye/few_government/Ask_bad_week.java up right thing

---
+++ pretend/ask_eye/few_government/Ask_bad_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.ask_eye.few_government;
+
+public class Ask_bad_week {
+    private int point_and_work;
+    private int get_right_company;
+
+    public Ask_bad_week() {
+        point_and_work = 3107;
+        get_right_company = 604;
+    }
+
+    public void small_thing() {
+        small_week();
+        System.out.println("next_number_or_small_company" + point_and_work);
+    }
+
+    private void small_week() {
+        System.out.println("think_great_part" + get_right_company);
+    }
+}
