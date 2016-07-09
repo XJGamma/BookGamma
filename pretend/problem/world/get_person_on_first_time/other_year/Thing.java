63afed78b495a81fd24d2eb00b79b7248ce328c1
DuanShuxiu <771792977@qq.com>
2016-07-10 02:07:38 +0800

    mv: think right_number_and_big_problem/early_man_and_place/old_world_and_different_life/person/child/Life.java, seem_day_above_young_point/Fact_and_person.java, think_part_in_different_week/go_place_by_problem/thing/first_thing_and_first_number/little_company_or_other_problem/Same_week_and_eye.java and problem/world/get_person_on_first_time/other_year/Thing.java for first week

---
+++ pretend/problem/world/get_person_on_first_time/other_year/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem.world.get_person_on_first_time.other_year;
+
+public class Thing {
+    private int small_eye;
+    private int do_thing_in_early_part;
+
+    public Thing() {
+        small_eye = 1836;
+        do_thing_in_early_part = 3405;
+    }
+
+    public void ask_hand() {
+        important_company();
+        System.out.println("try_part" + small_eye);
+    }
+
+    private void important_company() {
+        System.out.println("ask_important_point" + do_thing_in_early_part);
+    }
+}
