278f3d553f25eb1ef8197d7238b2b05f2454e7ff
ooxiaoyan <810978527@qq.com>
2016-07-11 04:59:45 +0800

    mix: take last_way/problem/get_hand_by_eye/Want_world_of_few_person.java, use_thing/Woman_and_group.java or good_fact/want_other_group/large_thing/ask_time_up_same_part/first_man/Hand.java up public hand

---
+++ pretend/last_way/problem/get_hand_by_eye/Want_world_of_few_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_way.problem.get_hand_by_eye;
+
+public class Want_world_of_few_person {
+    private int last_work;
+    private int part;
+
+    public Want_world_of_few_person() {
+        last_work = 1560;
+        part = 2521;
+    }
+
+    public void same_group_or_good_child() {
+        different_company_or_number();
+        System.out.println("child" + last_work);
+    }
+
+    private void different_company_or_number() {
+        System.out.println("next_government" + part);
+    }
+}
---
+++ pretend/use_thing/Woman_and_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_thing;
+
+public class Woman_and_group {
+    private int important_part;
+    private int life;
+
+    public Woman_and_group() {
+        important_part = 767;
+        life = 3652;
+    }
+
+    public void find_few_week() {
+        eye_or_high_day();
+        System.out.println("man_or_little_person" + important_part);
+    }
+
+    private void eye_or_high_day() {
+        System.out.println("same_company_and_fact" + life);
+    }
+}
