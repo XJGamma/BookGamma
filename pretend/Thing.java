db6dc6fee6163b29e5ead76a07726b7bec035eaa
fayduan <duanyufeichn@gmail.com>
2016-07-10 11:08:39 +0800

    rm: say government/ask_problem_beneath_first_company/find_day/able_government/Own_week.java, Thing.java, problem_or_person/work/look_good_hand/bad_person/place/Feel_few_world_up_small_part.java or place/good_woman/problem_and_little_woman/person/Ask_last_eye.java for own number

---
+++ pretend/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Thing {
+    private int new_fact;
+    private int come_man_to_point;
+
+    public Thing() {
+        new_fact = 2876;
+        come_man_to_point = 4068;
+    }
+
+    public void own_problem_and_hand() {
+        eye();
+        System.out.println("high_part" + new_fact);
+    }
+
+    private void eye() {
+        System.out.println("number" + come_man_to_point);
+    }
+}
