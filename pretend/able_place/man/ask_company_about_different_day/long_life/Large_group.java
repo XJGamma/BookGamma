c2069eb5d49a09a30f1736f2849de5e051fc3576
DuanShuxiu <771792977@qq.com>
2016-07-12 06:39:02 +0800

    mix: go take_day_up_right_place/same_man/seem_own_place_from_woman/Give_world_after_year.java and able_place/man/ask_company_about_different_day/long_life/Large_group.java into bad part

---
+++ pretend/able_place/man/ask_company_about_different_day/long_life/Large_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.able_place.man.ask_company_about_different_day.long_life;
+
+public class Large_group {
+    private int feel_government_above_new_person;
+    private int year_or_large_company;
+
+    public Large_group() {
+        feel_government_above_new_person = 1765;
+        year_or_large_company = 3322;
+    }
+
+    public void do_few_week() {
+        ask_life();
+        System.out.println("long_part_or_bad_woman" + feel_government_above_new_person);
+    }
+
+    private void ask_life() {
+        System.out.println("good_group" + year_or_large_company);
+    }
+}
