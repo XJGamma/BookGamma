a8ff94466f0fb9677587cea8a6a9f17be2f2339d
XinZhou0209 <zhouxin0209@139.com>
2016-07-12 07:17:17 +0800

    mv: want part/good_point/Group.java into large point

---
+++ pretend/part/good_point/Group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.part.good_point;
+
+public class Group {
+    private int government;
+    private int last_point;
+
+    public Group() {
+        government = 100;
+        last_point = 2634;
+    }
+
+    public void number() {
+        big_point();
+        System.out.println("next_point" + government);
+    }
+
+    private void big_point() {
+        System.out.println("time_or_public_work" + last_point);
+    }
+}
