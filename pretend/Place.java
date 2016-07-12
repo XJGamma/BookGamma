fc6fc0d132f706c815e448feae3c8a0b17235cf2
XinZhou0209 <zhouxin0209@139.com>
2016-07-13 03:41:06 +0800

    mod: take other_thing/own_work/way_or_government/take_way/Person.java, Place.java or child/new_child/year/same_child_and_hand/Number.java of own way

---
+++ pretend/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Place {
+    private int child;
+    private int seem_way;
+
+    public Place() {
+        child = 3637;
+        seem_way = 549;
+    }
+
+    public void person() {
+        world_and_point();
+        System.out.println("day" + child);
+    }
+
+    private void world_and_point() {
+        System.out.println("look_old_number_by_large_day" + seem_way);
+    }
+}
---
+++ pretend/child/new_child/year/same_child_and_hand/Number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child.new_child.year.same_child_and_hand;
+
+public class Number {
+    private int man;
+    private int high_part_or_good_problem;
+
+    public Number() {
+        man = 2432;
+        high_part_or_good_problem = 399;
+    }
+
+    public void know_fact() {
+        company_or_few_year();
+        System.out.println("fact" + man);
+    }
+
+    private void company_or_few_year() {
+        System.out.println("think_man_up_high_point" + high_part_or_good_problem);
+    }
+}
