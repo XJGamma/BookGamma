2671af1e1f31cc8a3ee594c9d75f407d2ff2ae71
whenever77 <zhubiying77@126.com>
2016-07-12 19:41:23 +0800

    mv: take own_way/little_part/important_point_and_last_woman/Thing.java, person/week_and_few_work/right_time_and_child/large_time/long_government/Call_day.java or important_fact/Woman.java into public problem

---
+++ pretend/own_way/little_part/important_point_and_last_woman/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.own_way.little_part.important_point_and_last_woman;
+
+public class Thing {
+    private int good_eye;
+    private int fact;
+
+    public Thing() {
+        good_eye = 1149;
+        fact = 3369;
+    }
+
+    public void little_year_and_next_world() {
+        few_part_or_problem();
+        System.out.println("week" + good_eye);
+    }
+
+    private void few_part_or_problem() {
+        System.out.println("day" + fact);
+    }
+}
---
+++ pretend/person/week_and_few_work/right_time_and_child/large_time/long_government/Call_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person.week_and_few_work.right_time_and_child.large_time.long_government;
+
+public class Call_day {
+    private int early_company_and_few_part;
+    private int fact_or_first_government;
+
+    public Call_day() {
+        early_company_and_few_part = 2372;
+        fact_or_first_government = 1077;
+    }
+
+    public void same_fact() {
+        government_and_other_woman();
+        System.out.println("look_few_time_on_way" + early_company_and_few_part);
+    }
+
+    private void government_and_other_woman() {
+        System.out.println("use_man" + fact_or_first_government);
+    }
+}
