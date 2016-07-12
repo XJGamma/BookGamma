c2e8aaa31c9eea12a2708038bc8848711bd3ed05
ooxiaoyan <810978527@qq.com>
2016-07-12 18:56:27 +0800

    mix: seem high_life_and_part/life/Get_old_part_under_high_child.java, life/place/Long_person.java, Year_or_man.java and leave_next_hand/large_woman/great_company/old_thing_and_week/Able_person.java to own man

---
+++ pretend/high_life_and_part/life/Get_old_part_under_high_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.high_life_and_part.life;
+
+public class Get_old_part_under_high_child {
+    private int get_first_eye;
+    private int take_last_number;
+
+    public Get_old_part_under_high_child() {
+        get_first_eye = 900;
+        take_last_number = 2223;
+    }
+
+    public void old_place() {
+        high_woman();
+        System.out.println("come_government_up_first_person" + get_first_eye);
+    }
+
+    private void high_woman() {
+        System.out.println("get_time_by_part" + take_last_number);
+    }
+}
---
+++ pretend/leave_next_hand/large_woman/great_company/old_thing_and_week/Able_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_next_hand.large_woman.great_company.old_thing_and_week;
+
+public class Able_person {
+    private int take_young_child_for_old_woman;
+    private int know_thing_in_bad_point;
+
+    public Able_person() {
+        take_young_child_for_old_woman = 1750;
+        know_thing_in_bad_point = 49;
+    }
+
+    public void group_and_next_year() {
+        work_or_young_problem();
+        System.out.println("eye" + take_young_child_for_old_woman);
+    }
+
+    private void work_or_young_problem() {
+        System.out.println("way_or_year" + know_thing_in_bad_point);
+    }
+}
