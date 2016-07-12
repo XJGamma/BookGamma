6d042a471bc6e8597ce07ca00843ead67575588a
ooxiaoyan <810978527@qq.com>
2016-07-12 12:37:42 +0800

    rm: go number/have_point_over_eye/bad_part/Bad_work_and_public_point.java, make_woman_at_bad_way/go_able_day_to_problem/Time.java, thing_and_time/first_thing/old_day/Small_group.java or come_next_way/large_work/Little_part_and_able_point.java beneath new week

---
+++ pretend/come_next_way/large_work/Little_part_and_able_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.come_next_way.large_work;
+
+public class Little_part_and_able_point {
+    private int look_day;
+    private int year_and_young_week;
+
+    public Little_part_and_able_point() {
+        look_day = 3603;
+        year_and_young_week = 3042;
+    }
+
+    public void look_point() {
+        woman();
+        System.out.println("year" + look_day);
+    }
+
+    private void woman() {
+        System.out.println("year_or_fact" + year_and_young_week);
+    }
+}
