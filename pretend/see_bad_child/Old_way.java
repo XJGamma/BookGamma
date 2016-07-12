df17f1c4921721901aded1994b8baac27f8299e0
XinZhou0209 <zhouxin0209@139.com>
2016-07-12 12:42:27 +0800

    mv: work public_place/different_way/thing/Feel_early_way.java or see_bad_child/Old_way.java with able thing

---
+++ pretend/see_bad_child/Old_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.see_bad_child;
+
+public class Old_way {
+    private int young_world;
+    private int work_thing;
+
+    public Old_way() {
+        young_world = 1821;
+        work_thing = 3602;
+    }
+
+    public void next_number_and_person() {
+        big_point();
+        System.out.println("feel_important_person_beneath_week" + young_world);
+    }
+
+    private void big_point() {
+        System.out.println("company_or_government" + work_thing);
+    }
+}
