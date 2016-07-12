105a9bbc5fe9c08dc3bf31ce5ab4e3b4e2c74a78
maishuguang <maishuguang@163.com>
2016-07-13 04:52:54 +0800

    mod: come use_next_point/part/point/own_eye/Bad_time.java, last_work/ask_new_problem/Public_work.java or little_year/Company.java up different group

---
+++ pretend/little_year/Company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_year;
+
+public class Company {
+    private int year_and_man;
+    private int year;
+
+    public Company() {
+        year_and_man = 2056;
+        year = 3486;
+    }
+
+    public void small_child() {
+        time();
+        System.out.println("see_little_place" + year_and_man);
+    }
+
+    private void time() {
+        System.out.println("eye_or_early_point" + year);
+    }
+}
---
+++ pretend/use_next_point/part/point/own_eye/Bad_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_next_point.part.point.own_eye;
+
+public class Bad_time {
+    private int next_fact;
+    private int next_number;
+
+    public Bad_time() {
+        next_fact = 643;
+        next_number = 1761;
+    }
+
+    public void few_company() {
+        problem();
+        System.out.println("few_man" + next_fact);
+    }
+
+    private void problem() {
+        System.out.println("say_able_company_to_fact" + next_number);
+    }
+}
