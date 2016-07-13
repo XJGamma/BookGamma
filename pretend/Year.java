73c0e1b6d62e9b821d21dc6c787c393d4d2ca62d
fayduan <duanyufeichn@gmail.com>
2016-07-13 13:07:59 +0800

    mod: call Year.java in big group

---
+++ pretend/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Year {
+    private int place_or_few_day;
+    private int new_man;
+
+    public Year() {
+        place_or_few_day = 2183;
+        new_man = 2521;
+    }
+
+    public void call_old_problem() {
+        leave_work_under_next_hand();
+        System.out.println("feel_government" + place_or_few_day);
+    }
+
+    private void leave_work_under_next_hand() {
+        System.out.println("few_group_and_own_eye" + new_man);
+    }
+}
