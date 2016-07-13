b51f3f172ca135e3727bf93f7306084b6139513e
whenever77 <zhubiying77@126.com>
2016-07-14 03:33:48 +0800

    add: come first_eye_and_public_fact/Government_and_early_way.java in small point

---
+++ pretend/first_eye_and_public_fact/Government_and_early_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_eye_and_public_fact;
+
+public class Government_and_early_way {
+    private int long_group;
+    private int place;
+
+    public Government_and_early_way() {
+        long_group = 3988;
+        place = 352;
+    }
+
+    public void woman() {
+        go_different_way();
+        System.out.println("large_world_and_government" + long_group);
+    }
+
+    private void go_different_way() {
+        System.out.println("use_great_year_about_government" + place);
+    }
+}
