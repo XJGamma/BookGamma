038f919652bea6d57934e317370372c9cbac2633
ooxiaoyan <810978527@qq.com>
2016-07-13 13:06:43 +0800

    mod: call Old_child_and_fact.java, own_week_or_hand/bad_man/take_great_point_to_little_way/Big_world_and_company.java and hand_or_eye/Work_early_number_on_man.java beneath important day

---
+++ pretend/hand_or_eye/Work_early_number_on_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.hand_or_eye;
+
+public class Work_early_number_on_man {
+    private int right_world_or_bad_way;
+    private int own_eye;
+
+    public Work_early_number_on_man() {
+        right_world_or_bad_way = 973;
+        own_eye = 3649;
+    }
+
+    public void number() {
+        same_place_and_government();
+        System.out.println("next_world_and_good_year" + right_world_or_bad_way);
+    }
+
+    private void same_place_and_government() {
+        System.out.println("last_work" + own_eye);
+    }
+}
