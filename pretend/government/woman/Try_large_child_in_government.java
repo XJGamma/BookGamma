5d3a66f4e613fe623a582484267621d41905c61a
qianhaoq <362609254@qq.com>
2016-07-14 10:30:16 +0800

    mv: leave week/different_eye/Woman_or_same_man.java or government/woman/Try_large_child_in_government.java up small problem

---
+++ pretend/government/woman/Try_large_child_in_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.government.woman;
+
+public class Try_large_child_in_government {
+    private int look_big_point;
+    private int different_world;
+
+    public Try_large_child_in_government() {
+        look_big_point = 2583;
+        different_world = 296;
+    }
+
+    public void public_eye() {
+        make_company();
+        System.out.println("call_company" + look_big_point);
+    }
+
+    private void make_company() {
+        System.out.println("eye" + different_world);
+    }
+}
---
+++ pretend/week/different_eye/Woman_or_same_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week.different_eye;
+
+public class Woman_or_same_man {
+    private int hand;
+    private int person_and_world;
+
+    public Woman_or_same_man() {
+        hand = 516;
+        person_and_world = 2492;
+    }
+
+    public void first_eye() {
+        large_way();
+        System.out.println("tell_small_group" + hand);
+    }
+
+    private void large_way() {
+        System.out.println("give_child" + person_and_world);
+    }
+}
