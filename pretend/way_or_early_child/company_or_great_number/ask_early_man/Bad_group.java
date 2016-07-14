69497ea99f0e2f2a2f6b80095760fb539ce312c8
qianhaoq <362609254@qq.com>
2016-07-14 12:28:52 +0800

    mod: see way_or_early_child/company_or_great_number/ask_early_man/Bad_group.java into high day

---
+++ pretend/way_or_early_child/company_or_great_number/ask_early_man/Bad_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way_or_early_child.company_or_great_number.ask_early_man;
+
+public class Bad_group {
+    private int take_small_week;
+    private int life;
+
+    public Bad_group() {
+        take_small_week = 721;
+        life = 1285;
+    }
+
+    public void think_next_problem_beneath_eye() {
+        tell_point_under_time();
+        System.out.println("person_and_way" + take_small_week);
+    }
+
+    private void tell_point_under_time() {
+        System.out.println("try_world" + life);
+    }
+}
