07bd82082a4fc0ecb91dbc7e35d4c3516657f4a2
ooxiaoyan <810978527@qq.com>
2016-07-14 21:01:36 +0800

    mv: see Right_day_and_government.java, small_life/Old_part.java, fact/First_life.java or great_problem_and_number/way/week/Get_woman.java over public fact

---
+++ pretend/small_life/Old_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_life;
+
+public class Old_part {
+    private int work_life_about_big_life;
+    private int see_government;
+
+    public Old_part() {
+        work_life_about_big_life = 3594;
+        see_government = 1471;
+    }
+
+    public void little_world() {
+        try_little_world_at_thing();
+        System.out.println("good_time" + work_life_about_big_life);
+    }
+
+    private void try_little_world_at_thing() {
+        System.out.println("week" + see_government);
+    }
+}
