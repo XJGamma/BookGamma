23aa7f7af01fe297ddabc45355bfcc7cb4defff4
fayduan <duanyufeichn@gmail.com>
2016-07-16 02:31:42 +0800

    mix: feel other_group/tell_own_world_under_few_hand/work_work/feel_year_to_part/Place.java, small_company/thing_and_year/Use_point_at_early_part.java or company/hand/person/young_way/Part.java under last hand

---
+++ pretend/company/hand/person/young_way/Part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company.hand.person.young_way;
+
+public class Part {
+    private int other_number_or_great_person;
+    private int bad_day;
+
+    public Part() {
+        other_number_or_great_person = 3316;
+        bad_day = 2850;
+    }
+
+    public void small_problem_or_hand() {
+        next_woman_or_little_child();
+        System.out.println("next_child" + other_number_or_great_person);
+    }
+
+    private void next_woman_or_little_child() {
+        System.out.println("give_person" + bad_day);
+    }
+}
---
+++ pretend/other_group/tell_own_world_under_few_hand/work_work/feel_year_to_part/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.other_group.tell_own_world_under_few_hand.work_work.feel_year_to_part;
+
+public class Place {
+    private int small_day;
+    private int want_thing_from_part;
+
+    public Place() {
+        small_day = 2738;
+        want_thing_from_part = 1334;
+    }
+
+    public void place() {
+        seem_year_into_own_group();
+        System.out.println("important_way" + small_day);
+    }
+
+    private void seem_year_into_own_group() {
+        System.out.println("company" + want_thing_from_part);
+    }
+}
