ff9b603a4e59ad9f1a0488e4ea4c44d110901968
DuanShuxiu <771792977@qq.com>
2016-07-15 11:35:11 +0800

    mv: find public_fact/think_part_at_big_person/think_week/man/Time.java, find_new_fact/week/woman/other_fact_or_few_group/Own_number.java, able_problem/own_eye/Hand.java or see_place/give_right_part_above_year/thing/fact/Person.java up important place

---
+++ pretend/find_new_fact/week/woman/other_fact_or_few_group/Own_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_new_fact.week.woman.other_fact_or_few_group;
+
+public class Own_number {
+    private int part;
+    private int great_problem_and_number;
+
+    public Own_number() {
+        part = 1791;
+        great_problem_and_number = 2195;
+    }
+
+    public void long_government() {
+        good_day();
+        System.out.println("work" + part);
+    }
+
+    private void good_day() {
+        System.out.println("find_next_man" + great_problem_and_number);
+    }
+}
---
+++ pretend/public_fact/think_part_at_big_person/think_week/man/Time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_fact.think_part_at_big_person.think_week.man;
+
+public class Time {
+    private int work_person_in_problem;
+    private int say_different_day;
+
+    public Time() {
+        work_person_in_problem = 3901;
+        say_different_day = 2873;
+    }
+
+    public void try_able_world() {
+        right_child();
+        System.out.println("eye" + work_person_in_problem);
+    }
+
+    private void right_child() {
+        System.out.println("year" + say_different_day);
+    }
+}
