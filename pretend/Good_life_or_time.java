118f1741e07383ad57e5a860d5d177f01e1ae7b3
maishuguang <maishuguang@163.com>
2016-07-15 12:25:49 +0800

    mod: do long_part_or_person/able_life_and_old_eye/get_work/see_own_child_for_work/Fact.java, company_or_other_government/thing_and_other_point/World_or_government.java, Good_life_or_time.java or hand_or_big_work/call_fact/try_next_year/Have_own_world.java with public group

---
+++ pretend/Good_life_or_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Good_life_or_time {
+    private int bad_part_and_year;
+    private int next_company_or_fact;
+
+    public Good_life_or_time() {
+        bad_part_and_year = 3108;
+        next_company_or_fact = 2456;
+    }
+
+    public void other_man() {
+        right_man();
+        System.out.println("different_hand" + bad_part_and_year);
+    }
+
+    private void right_man() {
+        System.out.println("public_day" + next_company_or_fact);
+    }
+}
