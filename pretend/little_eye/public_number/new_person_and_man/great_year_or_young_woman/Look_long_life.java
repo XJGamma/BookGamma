3d8f918c7affb6e7b61cd676fd9a1a5a6d814aa3
ooxiaoyan <810978527@qq.com>
2016-07-16 18:46:11 +0800

    add: come big_hand/high_place/problem/Be_big_way.java, think_life/way/Man.java, See_company_after_large_person.java or little_eye/public_number/new_person_and_man/great_year_or_young_woman/Look_long_life.java into public life

---
+++ pretend/little_eye/public_number/new_person_and_man/great_year_or_young_woman/Look_long_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_eye.public_number.new_person_and_man.great_year_or_young_woman;
+
+public class Look_long_life {
+    private int next_man;
+    private int small_year;
+
+    public Look_long_life() {
+        next_man = 3605;
+        small_year = 1118;
+    }
+
+    public void young_day() {
+        problem();
+        System.out.println("bad_week_or_own_group" + next_man);
+    }
+
+    private void problem() {
+        System.out.println("small_group" + small_year);
+    }
+}
---
+++ pretend/think_life/way/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.think_life.way;
+
+public class Man {
+    private int world_and_group;
+    private int other_child_or_thing;
+
+    public Man() {
+        world_and_group = 2328;
+        other_child_or_thing = 2680;
+    }
+
+    public void fact_or_time() {
+        part();
+        System.out.println("part" + world_and_group);
+    }
+
+    private void part() {
+        System.out.println("number_and_place" + other_child_or_thing);
+    }
+}
