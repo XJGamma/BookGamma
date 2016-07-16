501cdd0c9746fe0c0cb35ed5002bf6719f123320
maishuguang <maishuguang@163.com>
2016-07-16 13:53:07 +0800

    rm: do work_same_eye/have_thing/problem_or_time/Hand.java, eye/tell_point/few_group_or_high_day/right_child/Person.java, eye/Thing.java or have_problem_after_old_problem/long_day/long_week/Eye.java above first world

---
+++ pretend/eye/tell_point/few_group_or_high_day/right_child/Person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye.tell_point.few_group_or_high_day.right_child;
+
+public class Person {
+    private int see_long_group_of_person;
+    private int point;
+
+    public Person() {
+        see_long_group_of_person = 4055;
+        point = 1379;
+    }
+
+    public void leave_place() {
+        work_and_government();
+        System.out.println("small_place" + see_long_group_of_person);
+    }
+
+    private void work_and_government() {
+        System.out.println("week_and_person" + point);
+    }
+}
