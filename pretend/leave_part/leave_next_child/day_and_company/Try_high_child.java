cd85f88d2fa2f84c7ee9be719d3d6f9fba22ce47
Jingkai Tang <jingkaitang@gmail.com>
2016-07-17 04:10:01 +0800

    add: use next_place_or_person/eye_or_few_fact/woman/way_or_early_place/Find_old_work_on_different_government.java, leave_part/leave_next_child/day_and_company/Try_high_child.java and world_and_point/able_place_or_man/Know_important_time.java above next thing

---
+++ pretend/leave_part/leave_next_child/day_and_company/Try_high_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_part.leave_next_child.day_and_company;
+
+public class Try_high_child {
+    private int be_right_point_on_fact;
+    private int right_world;
+
+    public Try_high_child() {
+        be_right_point_on_fact = 2654;
+        right_world = 362;
+    }
+
+    public void give_point_by_important_fact() {
+        early_point();
+        System.out.println("ask_able_work" + be_right_point_on_fact);
+    }
+
+    private void early_point() {
+        System.out.println("man_and_company" + right_world);
+    }
+}
---
+++ pretend/world_and_point/able_place_or_man/Know_important_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_and_point.able_place_or_man;
+
+public class Know_important_time {
+    private int man;
+    private int woman;
+
+    public Know_important_time() {
+        man = 666;
+        woman = 2903;
+    }
+
+    public void good_man() {
+        thing();
+        System.out.println("place_and_fact" + man);
+    }
+
+    private void thing() {
+        System.out.println("great_eye" + woman);
+    }
+}
