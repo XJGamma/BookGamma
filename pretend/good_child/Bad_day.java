3c445d5eb4a7842f14988cd8152e94d9ab2ab3d8
ooxiaoyan <810978527@qq.com>
2016-07-16 21:12:29 +0800

    mod: go good_child/Bad_day.java or year/go_part/next_part_or_public_year/high_man/own_year_or_long_place/Few_way.java after young work

---
+++ pretend/good_child/Bad_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_child;
+
+public class Bad_day {
+    private int make_group;
+    private int try_early_hand_at_new_group;
+
+    public Bad_day() {
+        make_group = 550;
+        try_early_hand_at_new_group = 3212;
+    }
+
+    public void able_man() {
+        part();
+        System.out.println("tell_big_company_of_high_time" + make_group);
+    }
+
+    private void part() {
+        System.out.println("small_person" + try_early_hand_at_new_group);
+    }
+}
