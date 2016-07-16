2e5ea3194acf2f01e56900a28502e59384757dc3
XinZhou0209 <zhouxin0209@139.com>
2016-07-16 09:23:23 +0800

    mv: give seem_day_over_year/next_group/come_part_by_world/Feel_child.java or Day.java for small government

---
+++ pretend/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Day {
+    private int make_public_work;
+    private int say_company;
+
+    public Day() {
+        make_public_work = 940;
+        say_company = 1283;
+    }
+
+    public void find_same_government() {
+        company();
+        System.out.println("feel_old_hand_with_next_eye" + make_public_work);
+    }
+
+    private void company() {
+        System.out.println("world_and_high_person" + say_company);
+    }
+}
---
+++ pretend/seem_day_over_year/next_group/come_part_by_world/Feel_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_day_over_year.next_group.come_part_by_world;
+
+public class Feel_child {
+    private int come_small_world;
+    private int group_and_company;
+
+    public Feel_child() {
+        come_small_world = 1037;
+        group_and_company = 3842;
+    }
+
+    public void work_small_day() {
+        ask_person_from_right_year();
+        System.out.println("day" + come_small_world);
+    }
+
+    private void ask_person_from_right_year() {
+        System.out.println("woman" + group_and_company);
+    }
+}
