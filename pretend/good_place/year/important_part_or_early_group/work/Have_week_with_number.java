c6309967b9db75c53d87c3ba4e6a87518fcba88a
DuanShuxiu <771792977@qq.com>
2016-07-16 21:26:50 +0800

    mix: make good_place/year/important_part_or_early_group/work/Have_week_with_number.java into early number

---
+++ pretend/good_place/year/important_part_or_early_group/work/Have_week_with_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_place.year.important_part_or_early_group.work;
+
+public class Have_week_with_number {
+    private int place;
+    private int great_place;
+
+    public Have_week_with_number() {
+        place = 2677;
+        great_place = 197;
+    }
+
+    public void place_or_woman() {
+        high_group();
+        System.out.println("hand" + place);
+    }
+
+    private void high_group() {
+        System.out.println("use_good_week_for_woman" + great_place);
+    }
+}
