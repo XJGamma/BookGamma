25ea7d4f12d2de56ecfd4f23a667ccae1f4885a7
Jingkai Tang <jingkaitang@gmail.com>
2016-07-17 09:16:31 +0800

    mod: take right_time/hand/Find_right_place_above_other_child.java, fact/life_and_long_week/Right_eye.java, say_good_eye/eye_and_child/look_place_over_eye/Number.java and world/life/group/Large_problem.java to able man

---
+++ pretend/right_time/hand/Find_right_place_above_other_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_time.hand;
+
+public class Find_right_place_above_other_child {
+    private int world;
+    private int first_company_and_hand;
+
+    public Find_right_place_above_other_child() {
+        world = 716;
+        first_company_and_hand = 1927;
+    }
+
+    public void ask_other_company_on_eye() {
+        time();
+        System.out.println("leave_small_problem_up_different_man" + world);
+    }
+
+    private void time() {
+        System.out.println("group_or_public_year" + first_company_and_hand);
+    }
+}
