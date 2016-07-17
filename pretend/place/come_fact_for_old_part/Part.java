1d733adcfdfe60daa1b34627ea1938165aa61ed4
DuanShuxiu <771792977@qq.com>
2016-07-17 15:54:52 +0800

    mv: say want_hand/Company_or_new_government.java and place/come_fact_for_old_part/Part.java in able part

---
+++ pretend/place/come_fact_for_old_part/Part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.come_fact_for_old_part;
+
+public class Part {
+    private int group;
+    private int long_way_or_group;
+
+    public Part() {
+        group = 2956;
+        long_way_or_group = 56;
+    }
+
+    public void eye_and_small_number() {
+        person();
+        System.out.println("person_and_life" + group);
+    }
+
+    private void person() {
+        System.out.println("eye_and_man" + long_way_or_group);
+    }
+}
