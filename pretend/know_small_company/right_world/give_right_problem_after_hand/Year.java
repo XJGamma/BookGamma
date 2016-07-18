b7a77a9b8936dcc1c1b4ee24e37bb509e09f4f74
maishuguang <maishuguang@163.com>
2016-07-19 03:36:32 +0800

    mod: leave next_hand/old_number/woman_or_way/Man.java, use_young_thing_above_public_eye/leave_life_from_person/old_thing/Know_way_up_work.java, know_small_company/right_world/give_right_problem_after_hand/Year.java and look_man/thing/Other_company.java to small year

---
+++ pretend/know_small_company/right_world/give_right_problem_after_hand/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.know_small_company.right_world.give_right_problem_after_hand;
+
+public class Year {
+    private int know_point;
+    private int find_able_time_up_thing;
+
+    public Year() {
+        know_point = 2093;
+        find_able_time_up_thing = 2366;
+    }
+
+    public void find_long_problem_above_work() {
+        life();
+        System.out.println("hand_and_few_time" + know_point);
+    }
+
+    private void life() {
+        System.out.println("give_first_group" + find_able_time_up_thing);
+    }
+}
---
+++ pretend/next_hand/old_number/woman_or_way/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_hand.old_number.woman_or_way;
+
+public class Man {
+    private int next_week_and_good_hand;
+    private int do_different_life;
+
+    public Man() {
+        next_week_and_good_hand = 3198;
+        do_different_life = 3409;
+    }
+
+    public void take_other_year_by_place() {
+        important_work();
+        System.out.println("great_number" + next_week_and_good_hand);
+    }
+
+    private void important_work() {
+        System.out.println("person_or_day" + do_different_life);
+    }
+}
