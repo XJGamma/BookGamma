2382fd7bdaaa25ac3217a728c840740a6c9bc452
DuanShuxiu <771792977@qq.com>
2016-07-19 01:21:56 +0800

    mix: be little_thing_and_different_thing/work_little_woman/right_fact/work_little_company_in_high_number/man/Important_child.java, fact/say_life_of_first_world/try_last_hand/Life_or_next_company.java or Call_place.java to great day

---
+++ pretend/Call_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Call_place {
+    private int new_day;
+    private int public_government_or_bad_man;
+
+    public Call_place() {
+        new_day = 785;
+        public_government_or_bad_man = 3275;
+    }
+
+    public void week() {
+        year();
+        System.out.println("be_new_child_at_way" + new_day);
+    }
+
+    private void year() {
+        System.out.println("early_life_or_early_week" + public_government_or_bad_man);
+    }
+}
---
+++ pretend/little_thing_and_different_thing/work_little_woman/right_fact/work_little_company_in_high_number/man/Important_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_thing_and_different_thing.work_little_woman.right_fact.work_little_company_in_high_number.man;
+
+public class Important_child {
+    private int part;
+    private int hand;
+
+    public Important_child() {
+        part = 2083;
+        hand = 3936;
+    }
+
+    public void world() {
+        government();
+        System.out.println("tell_few_number" + part);
+    }
+
+    private void government() {
+        System.out.println("high_fact" + hand);
+    }
+}
