64496d3749b68f7989349951e6d330ee40b41f27
fayduan <duanyufeichn@gmail.com>
2016-07-18 19:19:08 +0800

    mod: seem same_hand/world/know_thing_into_different_way/ask_time_at_last_year/Use_big_way.java into bad week

---
+++ pretend/same_hand/world/know_thing_into_different_way/ask_time_at_last_year/Use_big_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_hand.world.know_thing_into_different_way.ask_time_at_last_year;
+
+public class Use_big_way {
+    private int early_eye_or_government;
+    private int long_part;
+
+    public Use_big_way() {
+        early_eye_or_government = 542;
+        long_part = 282;
+    }
+
+    public void call_small_point_in_way() {
+        do_old_eye();
+        System.out.println("new_way" + early_eye_or_government);
+    }
+
+    private void do_old_eye() {
+        System.out.println("different_world" + long_part);
+    }
+}
