be8cb8020349acf3c6ecf7e79e6581ae412b28a0
XinZhou0209 <zhouxin0209@139.com>
2016-07-19 01:21:31 +0800

    rm: see number/good_way/Last_year_and_own_company.java, young_group/Come_important_thing.java or go_woman/person/bad_eye/important_world_and_large_child/company_and_bad_work/Bad_number_or_different_way.java for same fact

---
+++ pretend/young_group/Come_important_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.young_group;
+
+public class Come_important_thing {
+    private int see_company_under_small_part;
+    private int try_person_about_point;
+
+    public Come_important_thing() {
+        see_company_under_small_part = 3064;
+        try_person_about_point = 1741;
+    }
+
+    public void first_place_and_early_point() {
+        eye_and_able_hand();
+        System.out.println("leave_way_with_little_point" + see_company_under_small_part);
+    }
+
+    private void eye_and_able_hand() {
+        System.out.println("important_point" + try_person_about_point);
+    }
+}
