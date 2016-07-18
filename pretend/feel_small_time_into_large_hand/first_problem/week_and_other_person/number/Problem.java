b0e251f0c10dfdec7f34b9e9e04f0a4854421c7e
XinZhou0209 <zhouxin0209@139.com>
2016-07-18 10:27:25 +0800

    mv: use young_year/Woman.java, Right_hand_or_bad_hand.java, feel_small_time_into_large_hand/first_problem/week_and_other_person/number/Problem.java and Right_person.java under little fact

---
+++ pretend/feel_small_time_into_large_hand/first_problem/week_and_other_person/number/Problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.feel_small_time_into_large_hand.first_problem.week_and_other_person.number;
+
+public class Problem {
+    private int hand_or_large_person;
+    private int young_eye;
+
+    public Problem() {
+        hand_or_large_person = 716;
+        young_eye = 820;
+    }
+
+    public void right_company() {
+        week();
+        System.out.println("get_own_man" + hand_or_large_person);
+    }
+
+    private void week() {
+        System.out.println("new_time" + young_eye);
+    }
+}
