2ce2f6ac1357b0284433612324238eec9896331a
qianhaoq <362609254@qq.com>
2016-07-19 16:25:20 +0800

    mv: look seem_young_part/point_or_right_eye/early_point/day_or_great_time/come_person/Person.java, little_work_or_number/right_woman_and_year/young_year/big_man/person/Person.java or Few_day.java in first point

---
+++ pretend/Few_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Few_day {
+    private int work_and_other_work;
+    private int early_life_or_important_year;
+
+    public Few_day() {
+        work_and_other_work = 991;
+        early_life_or_important_year = 10;
+    }
+
+    public void early_thing_and_bad_life() {
+        ask_part_of_fact();
+        System.out.println("be_life" + work_and_other_work);
+    }
+
+    private void ask_part_of_fact() {
+        System.out.println("bad_group" + early_life_or_important_year);
+    }
+}
---
+++ pretend/seem_young_part/point_or_right_eye/early_point/day_or_great_time/come_person/Person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_young_part.point_or_right_eye.early_point.day_or_great_time.come_person;
+
+public class Person {
+    private int right_way_and_different_problem;
+    private int day;
+
+    public Person() {
+        right_way_and_different_problem = 3222;
+        day = 581;
+    }
+
+    public void go_high_eye_under_eye() {
+        see_thing_under_old_place();
+        System.out.println("say_great_week_by_right_eye" + right_way_and_different_problem);
+    }
+
+    private void see_thing_under_old_place() {
+        System.out.println("number" + day);
+    }
+}
