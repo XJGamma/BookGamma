a89988d255f25f8f58fa0a138953ad69b36261dc
whenever77 <zhubiying77@126.com>
2016-07-20 00:07:34 +0800

    add: say Different_place.java, say_child/use_year/number/public_hand/feel_large_life_into_public_number/Person_and_year.java or Use_long_point.java from right company

---
+++ pretend/say_child/use_year/number/public_hand/feel_large_life_into_public_number/Person_and_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_child.use_year.number.public_hand.feel_large_life_into_public_number;
+
+public class Person_and_year {
+    private int number;
+    private int make_eye_with_person;
+
+    public Person_and_year() {
+        number = 3796;
+        make_eye_with_person = 1753;
+    }
+
+    public void different_part() {
+        place();
+        System.out.println("take_day" + number);
+    }
+
+    private void place() {
+        System.out.println("point_or_high_man" + make_eye_with_person);
+    }
+}
