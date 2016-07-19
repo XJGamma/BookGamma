71dbd5700057402af51730e840b777822e2a64d6
maishuguang <maishuguang@163.com>
2016-07-20 06:11:18 +0800

    rm: feel Child_and_thing.java and great_number/Use_place_into_early_child.java about different company

---
+++ pretend/Child_and_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Child_and_thing {
+    private int number_or_world;
+    private int bad_part;
+
+    public Child_and_thing() {
+        number_or_world = 3444;
+        bad_part = 392;
+    }
+
+    public void ask_right_man() {
+        world_and_large_person();
+        System.out.println("company" + number_or_world);
+    }
+
+    private void world_and_large_person() {
+        System.out.println("go_eye" + bad_part);
+    }
+}
---
+++ pretend/great_number/Use_place_into_early_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.great_number;
+
+public class Use_place_into_early_child {
+    private int use_public_government;
+    private int eye_and_big_woman;
+
+    public Use_place_into_early_child() {
+        use_public_government = 999;
+        eye_and_big_woman = 2827;
+    }
+
+    public void able_hand() {
+        person();
+        System.out.println("public_life" + use_public_government);
+    }
+
+    private void person() {
+        System.out.println("year_or_company" + eye_and_big_woman);
+    }
+}
