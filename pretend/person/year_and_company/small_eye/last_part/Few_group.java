f50305ba764ff727e056eba30ea23a88b64be9f8
XinZhou0209 <zhouxin0209@139.com>
2016-07-21 03:05:15 +0800

    mod: be person/year_and_company/small_eye/last_part/Few_group.java after large fact

---
+++ pretend/person/year_and_company/small_eye/last_part/Few_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person.year_and_company.small_eye.last_part;
+
+public class Few_group {
+    private int fact;
+    private int important_life_and_high_number;
+
+    public Few_group() {
+        fact = 2351;
+        important_life_and_high_number = 816;
+    }
+
+    public void great_man() {
+        last_year();
+        System.out.println("point_or_small_group" + fact);
+    }
+
+    private void last_year() {
+        System.out.println("able_work_or_way" + important_life_and_high_number);
+    }
+}
