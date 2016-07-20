655cc4c3d1c0cfd66ce0efe36a31bbb687824529
whenever77 <zhubiying77@126.com>
2016-07-20 16:47:07 +0800

    add: know Early_problem_and_same_man.java, other_week/company/day/thing_and_hand/Do_young_woman.java, life/group/Place.java and old_year/look_long_place_under_small_way/ask_year_beneath_week/group/Have_week.java above great point

---
+++ pretend/Early_problem_and_same_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Early_problem_and_same_man {
+    private int man;
+    private int find_important_week_into_first_eye;
+
+    public Early_problem_and_same_man() {
+        man = 3520;
+        find_important_week_into_first_eye = 1512;
+    }
+
+    public void big_world() {
+        few_person_and_different_life();
+        System.out.println("last_place" + man);
+    }
+
+    private void few_person_and_different_life() {
+        System.out.println("do_way_for_public_world" + find_important_week_into_first_eye);
+    }
+}
---
+++ pretend/other_week/company/day/thing_and_hand/Do_young_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.other_week.company.day.thing_and_hand;
+
+public class Do_young_woman {
+    private int part;
+    private int other_year;
+
+    public Do_young_woman() {
+        part = 3116;
+        other_year = 3642;
+    }
+
+    public void bad_person_or_person() {
+        time();
+        System.out.println("long_point" + part);
+    }
+
+    private void time() {
+        System.out.println("week" + other_year);
+    }
+}
