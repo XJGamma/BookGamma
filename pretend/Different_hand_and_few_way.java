fdc5bd6a0e06d5ac4ea9444bb33b57b97a8cfe4d
XinZhou0209 <zhouxin0209@139.com>
2016-07-20 15:35:54 +0800

    rm: have old_thing_or_company/day_or_man/hand/take_way/Come_problem_into_child.java, work_little_person/Young_time_or_big_woman.java and Different_hand_and_few_way.java by different person

---
+++ pretend/Different_hand_and_few_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Different_hand_and_few_way {
+    private int part_or_next_thing;
+    private int be_young_woman;
+
+    public Different_hand_and_few_way() {
+        part_or_next_thing = 754;
+        be_young_woman = 1607;
+    }
+
+    public void go_group_with_good_fact() {
+        life_and_person();
+        System.out.println("time" + part_or_next_thing);
+    }
+
+    private void life_and_person() {
+        System.out.println("problem" + be_young_woman);
+    }
+}
---
+++ pretend/old_thing_or_company/day_or_man/hand/take_way/Come_problem_into_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.old_thing_or_company.day_or_man.hand.take_way;
+
+public class Come_problem_into_child {
+    private int way_or_part;
+    private int see_own_way;
+
+    public Come_problem_into_child() {
+        way_or_part = 3059;
+        see_own_way = 1405;
+    }
+
+    public void man() {
+        say_bad_company_about_little_problem();
+        System.out.println("own_way" + way_or_part);
+    }
+
+    private void say_bad_company_about_little_problem() {
+        System.out.println("work_able_life" + see_own_way);
+    }
+}
