0354a55075a9ecb0c29d9d37085e4e74a83cc8e8
XinZhou0209 <zhouxin0209@139.com>
2016-07-21 06:34:38 +0800

    rm: want public_person/Big_life.java, eye/person_or_large_hand/place_and_week/be_little_fact/Large_life.java or man_or_eye/long_year_or_long_time/few_way/woman/own_problem/Work_time_to_time.java from good person

---
+++ pretend/man_or_eye/long_year_or_long_time/few_way/woman/own_problem/Work_time_to_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man_or_eye.long_year_or_long_time.few_way.woman.own_problem;
+
+public class Work_time_to_time {
+    private int week;
+    private int first_time;
+
+    public Work_time_to_time() {
+        week = 2395;
+        first_time = 682;
+    }
+
+    public void big_year() {
+        seem_big_woman_with_long_week();
+        System.out.println("same_thing_and_few_way" + week);
+    }
+
+    private void seem_big_woman_with_long_week() {
+        System.out.println("new_life" + first_time);
+    }
+}
---
+++ pretend/public_person/Big_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_person;
+
+public class Big_life {
+    private int woman;
+    private int seem_hand_after_same_week;
+
+    public Big_life() {
+        woman = 3424;
+        seem_hand_after_same_week = 3544;
+    }
+
+    public void work_same_year() {
+        small_man();
+        System.out.println("important_problem" + woman);
+    }
+
+    private void small_man() {
+        System.out.println("government" + seem_hand_after_same_week);
+    }
+}
