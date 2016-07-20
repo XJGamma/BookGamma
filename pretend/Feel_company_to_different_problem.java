58d9807c8babde13413bcf7f967699e842cd6919
Jingkai Tang <jingkaitang@gmail.com>
2016-07-21 02:15:44 +0800

    rm: make be_old_person_to_important_group/place_and_old_fact/early_time_or_early_day/Number.java, Feel_company_to_different_problem.java, work_week_about_large_fact/Thing.java or child_or_same_world/call_different_number/own_hand/other_point/world/Next_life_and_problem.java into next government

---
+++ pretend/Feel_company_to_different_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Feel_company_to_different_problem {
+    private int woman_or_young_man;
+    private int part;
+
+    public Feel_company_to_different_problem() {
+        woman_or_young_man = 2096;
+        part = 712;
+    }
+
+    public void few_week_and_bad_place() {
+        way_and_woman();
+        System.out.println("able_man" + woman_or_young_man);
+    }
+
+    private void way_and_woman() {
+        System.out.println("do_work" + part);
+    }
+}
---
+++ pretend/be_old_person_to_important_group/place_and_old_fact/early_time_or_early_day/Number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.be_old_person_to_important_group.place_and_old_fact.early_time_or_early_day;
+
+public class Number {
+    private int world_and_few_year;
+    private int long_woman_or_high_person;
+
+    public Number() {
+        world_and_few_year = 3116;
+        long_woman_or_high_person = 1434;
+    }
+
+    public void able_part_or_long_world() {
+        child_and_little_eye();
+        System.out.println("new_thing_or_time" + world_and_few_year);
+    }
+
+    private void child_and_little_eye() {
+        System.out.println("take_same_week_at_week" + long_woman_or_high_person);
+    }
+}
