878a95285046058744230d82513081058bb18bbe
maishuguang <maishuguang@163.com>
2016-07-22 04:01:12 +0800

    mv: look last_number_or_early_hand/have_next_company/work_and_old_eye/take_little_woman_up_person/thing_or_hand/Try_last_government.java, Work.java or hand/have_thing/hand/Work_and_important_woman.java from other thing

---
+++ pretend/Work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Work {
+    private int have_company;
+    private int problem;
+
+    public Work() {
+        have_company = 2928;
+        problem = 1896;
+    }
+
+    public void eye() {
+        ask_important_day_at_last_eye();
+        System.out.println("young_year_or_company" + have_company);
+    }
+
+    private void ask_important_day_at_last_eye() {
+        System.out.println("work" + problem);
+    }
+}
---
+++ pretend/hand/have_thing/hand/Work_and_important_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.hand.have_thing.hand;
+
+public class Work_and_important_woman {
+    private int think_number;
+    private int hand;
+
+    public Work_and_important_woman() {
+        think_number = 3513;
+        hand = 1512;
+    }
+
+    public void other_life_or_way() {
+        high_part_or_right_number();
+        System.out.println("large_number" + think_number);
+    }
+
+    private void high_part_or_right_number() {
+        System.out.println("important_man" + hand);
+    }
+}
