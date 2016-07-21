d3befa0071e078f089c8f7fd381f83fe7d98ad39
ooxiaoyan <810978527@qq.com>
2016-07-21 11:29:51 +0800

    mix: come Feel_week.java or high_year_or_last_man/Man.java from important company

---
+++ pretend/Feel_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Feel_week {
+    private int work;
+    private int make_big_point_for_few_government;
+
+    public Feel_week() {
+        work = 3940;
+        make_big_point_for_few_government = 1317;
+    }
+
+    public void good_government() {
+        great_point_and_place();
+        System.out.println("year" + work);
+    }
+
+    private void great_point_and_place() {
+        System.out.println("young_point" + make_big_point_for_few_government);
+    }
+}
---
+++ pretend/high_year_or_last_man/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.high_year_or_last_man;
+
+public class Man {
+    private int new_time_or_long_thing;
+    private int company;
+
+    public Man() {
+        new_time_or_long_thing = 2000;
+        company = 2541;
+    }
+
+    public void say_hand() {
+        hand();
+        System.out.println("problem" + new_time_or_long_thing);
+    }
+
+    private void hand() {
+        System.out.println("feel_public_work" + company);
+    }
+}
