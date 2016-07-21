7fb81fb2368e3b497583f75106d95e3617489c6f
XinZhou0209 <zhouxin0209@139.com>
2016-07-21 13:49:19 +0800

    mv: look do_able_day/eye/make_good_way/other_number_and_bad_work/take_small_place_for_little_work/Come_week.java, child/try_last_person_under_same_hand/problem_or_person/Way.java, problem/Part.java and man/young_eye/problem/Come_fact_by_number.java up high government

---
+++ pretend/do_able_day/eye/make_good_way/other_number_and_bad_work/take_small_place_for_little_work/Come_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.do_able_day.eye.make_good_way.other_number_and_bad_work.take_small_place_for_little_work;
+
+public class Come_week {
+    private int own_world;
+    private int come_world;
+
+    public Come_week() {
+        own_world = 1706;
+        come_world = 2350;
+    }
+
+    public void public_government() {
+        feel_year();
+        System.out.println("bad_point_or_next_place" + own_world);
+    }
+
+    private void feel_year() {
+        System.out.println("see_work_into_way" + come_world);
+    }
+}
---
+++ pretend/problem/Part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem;
+
+public class Part {
+    private int child;
+    private int next_year;
+
+    public Part() {
+        child = 3182;
+        next_year = 2943;
+    }
+
+    public void way() {
+        able_week();
+        System.out.println("company" + child);
+    }
+
+    private void able_week() {
+        System.out.println("try_early_number_for_young_time" + next_year);
+    }
+}
