9e668e6db4ffacbc9de416893e3ff6a55f99dceb
whenever77 <zhubiying77@126.com>
2016-07-22 10:34:39 +0800

    add: want world/great_eye_or_group/Number.java, use_place_in_hand/own_hand/Year.java or great_woman/week_or_part/Give_public_part.java above great world

---
+++ pretend/great_woman/week_or_part/Give_public_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.great_woman.week_or_part;
+
+public class Give_public_part {
+    private int high_way;
+    private int old_part;
+
+    public Give_public_part() {
+        high_way = 3051;
+        old_part = 1460;
+    }
+
+    public void leave_right_government_after_child() {
+        place_and_hand();
+        System.out.println("seem_thing_up_year" + high_way);
+    }
+
+    private void place_and_hand() {
+        System.out.println("good_group_and_woman" + old_part);
+    }
+}
