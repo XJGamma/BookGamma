984d8da551b86a6e6a6a77c30917fdedac7fbeba
qianhaoq <362609254@qq.com>
2016-07-22 13:16:20 +0800

    mod: ask problem/high_person_and_large_life/part/company/world_or_group/Long_way_and_man.java from own group

---
+++ pretend/problem/high_person_and_large_life/part/company/world_or_group/Long_way_and_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem.high_person_and_large_life.part.company.world_or_group;
+
+public class Long_way_and_man {
+    private int long_woman_and_year;
+    private int woman;
+
+    public Long_way_and_man() {
+        long_woman_and_year = 3365;
+        woman = 3469;
+    }
+
+    public void big_work() {
+        use_same_eye_beneath_own_life();
+        System.out.println("big_eye" + long_woman_and_year);
+    }
+
+    private void use_same_eye_beneath_own_life() {
+        System.out.println("important_eye" + woman);
+    }
+}
