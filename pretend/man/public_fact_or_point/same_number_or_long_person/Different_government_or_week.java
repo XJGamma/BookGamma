a0bfe3a827229c414562d3e5269c6e68f54101df
DuanShuxiu <771792977@qq.com>
2016-07-23 00:59:02 +0800

    rm: use man/public_fact_or_point/same_number_or_long_person/Different_government_or_week.java or high_company/group/man/big_part/Seem_child.java on high child

---
+++ pretend/man/public_fact_or_point/same_number_or_long_person/Different_government_or_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man.public_fact_or_point.same_number_or_long_person;
+
+public class Different_government_or_week {
+    private int good_hand;
+    private int great_day;
+
+    public Different_government_or_week() {
+        good_hand = 1287;
+        great_day = 3006;
+    }
+
+    public void want_good_part() {
+        make_life_into_bad_point();
+        System.out.println("have_great_thing" + good_hand);
+    }
+
+    private void make_life_into_bad_point() {
+        System.out.println("man_or_work" + great_day);
+    }
+}
