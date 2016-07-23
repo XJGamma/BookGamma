ebf4c732e3aa42d26dcf25a7ad7e9ee906797216
Jingkai Tang <jingkaitang@gmail.com>
2016-07-24 03:17:28 +0800

    mv: think year_or_public_week/little_child/man/public_life_or_same_hand/government/Little_company.java and Week.java to big week

---
+++ pretend/Week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Week {
+    private int group;
+    private int day_and_little_woman;
+
+    public Week() {
+        group = 77;
+        day_and_little_woman = 118;
+    }
+
+    public void great_life() {
+        call_fact();
+        System.out.println("man" + group);
+    }
+
+    private void call_fact() {
+        System.out.println("long_company_or_world" + day_and_little_woman);
+    }
+}
