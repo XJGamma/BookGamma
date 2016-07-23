6a0b788ca1ccb16b8fea7b3e51b9ee9f6dc0ebb7
XinZhou0209 <zhouxin0209@139.com>
2016-07-24 03:19:53 +0800

    mv: see good_world_and_good_fact/way/old_number_or_important_number/hand_or_world/think_big_week_in_government/Next_woman.java and Good_work.java up important day

---
+++ pretend/good_world_and_good_fact/way/old_number_or_important_number/hand_or_world/think_big_week_in_government/Next_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_world_and_good_fact.way.old_number_or_important_number.hand_or_world.think_big_week_in_government;
+
+public class Next_woman {
+    private int high_point_or_next_number;
+    private int take_number;
+
+    public Next_woman() {
+        high_point_or_next_number = 622;
+        take_number = 2488;
+    }
+
+    public void fact() {
+        child_and_part();
+        System.out.println("life_or_few_man" + high_point_or_next_number);
+    }
+
+    private void child_and_part() {
+        System.out.println("time" + take_number);
+    }
+}
