394851bffaf5e9a7e816d3ef5fd7266e5f6c45a7
fayduan <duanyufeichn@gmail.com>
2016-07-23 11:41:01 +0800

    mv: look young_place/important_part/think_week_in_problem/World.java, tell_big_child/first_way_or_place/Old_point_or_able_point.java, number/great_hand/other_hand/Next_point.java or next_company_and_large_year/look_small_world_about_point/child_or_person/group/Time_and_other_place.java with long company

---
+++ pretend/next_company_and_large_year/look_small_world_about_point/child_or_person/group/Time_and_other_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_company_and_large_year.look_small_world_about_point.child_or_person.group;
+
+public class Time_and_other_place {
+    private int call_year_under_large_way;
+    private int take_child_up_year;
+
+    public Time_and_other_place() {
+        call_year_under_large_way = 2358;
+        take_child_up_year = 3894;
+    }
+
+    public void come_big_place_above_large_number() {
+        good_government();
+        System.out.println("want_woman" + call_year_under_large_way);
+    }
+
+    private void good_government() {
+        System.out.println("way_or_person" + take_child_up_year);
+    }
+}
---
+++ pretend/number/great_hand/other_hand/Next_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.number.great_hand.other_hand;
+
+public class Next_point {
+    private int tell_problem_by_woman;
+    private int do_other_point;
+
+    public Next_point() {
+        tell_problem_by_woman = 183;
+        do_other_point = 2325;
+    }
+
+    public void feel_big_man_beneath_long_problem() {
+        long_place();
+        System.out.println("seem_long_thing" + tell_problem_by_woman);
+    }
+
+    private void long_place() {
+        System.out.println("point_or_company" + do_other_point);
+    }
+}
