631df6a655e77353d078761067f7c07efd7816dc
ooxiaoyan <810978527@qq.com>
2016-07-23 10:42:07 +0800

    mix: know tell_government_by_problem/know_life_about_thing/public_work_or_new_child/Eye_or_child.java, long_government/own_group_and_able_part/government/say_right_week/See_world.java, company_and_part/seem_big_man/first_number/Ask_last_number_of_year.java and eye/work/small_week/year_or_next_hand/Life_and_new_man.java over other thing

---
+++ pretend/company_and_part/seem_big_man/first_number/Ask_last_number_of_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company_and_part.seem_big_man.first_number;
+
+public class Ask_last_number_of_year {
+    private int feel_child_in_number;
+    private int early_thing;
+
+    public Ask_last_number_of_year() {
+        feel_child_in_number = 2093;
+        early_thing = 3728;
+    }
+
+    public void time() {
+        few_company();
+        System.out.println("group_or_big_problem" + feel_child_in_number);
+    }
+
+    private void few_company() {
+        System.out.println("use_good_point" + early_thing);
+    }
+}
---
+++ pretend/eye/work/small_week/year_or_next_hand/Life_and_new_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye.work.small_week.year_or_next_hand;
+
+public class Life_and_new_man {
+    private int number;
+    private int new_time;
+
+    public Life_and_new_man() {
+        number = 3278;
+        new_time = 3869;
+    }
+
+    public void different_hand() {
+        early_thing();
+        System.out.println("see_right_woman_up_thing" + number);
+    }
+
+    private void early_thing() {
+        System.out.println("large_day_and_own_point" + new_time);
+    }
+}
