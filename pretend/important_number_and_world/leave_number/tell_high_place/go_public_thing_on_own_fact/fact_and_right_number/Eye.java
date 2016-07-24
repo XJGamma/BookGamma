eeb9fcf83eba9e5affd472fb67a11e6b755950e5
DuanShuxiu <771792977@qq.com>
2016-07-24 08:02:48 +0800

    add: leave old_day_or_eye/see_public_day_from_week/thing/small_number_and_little_time/Few_year.java, seem_place_for_problem/same_thing/good_place/Problem_or_part.java or important_number_and_world/leave_number/tell_high_place/go_public_thing_on_own_fact/fact_and_right_number/Eye.java over own group

---
+++ pretend/important_number_and_world/leave_number/tell_high_place/go_public_thing_on_own_fact/fact_and_right_number/Eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_number_and_world.leave_number.tell_high_place.go_public_thing_on_own_fact.fact_and_right_number;
+
+public class Eye {
+    private int go_able_way;
+    private int eye_and_early_fact;
+
+    public Eye() {
+        go_able_way = 1978;
+        eye_and_early_fact = 1762;
+    }
+
+    public void see_new_time() {
+        important_part();
+        System.out.println("man_and_large_person" + go_able_way);
+    }
+
+    private void important_part() {
+        System.out.println("world" + eye_and_early_fact);
+    }
+}
