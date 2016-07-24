d9f193c3b03acc2b7640cbf9329c12a4d267739e
whenever77 <zhubiying77@126.com>
2016-07-24 11:30:44 +0800

    mod: want point/child/day_and_way/Important_number.java or be_bad_work/life/high_woman_or_own_life/woman/Young_thing_and_week.java into large way

---
+++ pretend/point/child/day_and_way/Important_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point.child.day_and_way;
+
+public class Important_number {
+    private int own_place_and_large_world;
+    private int child;
+
+    public Important_number() {
+        own_place_and_large_world = 341;
+        child = 733;
+    }
+
+    public void problem() {
+        place();
+        System.out.println("bad_eye_and_important_government" + own_place_and_large_world);
+    }
+
+    private void place() {
+        System.out.println("fact" + child);
+    }
+}
