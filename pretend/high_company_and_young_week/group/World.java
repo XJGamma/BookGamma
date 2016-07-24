cbf51c9182ee0a1046d55795c33d905bff65d9c4
ooxiaoyan <810978527@qq.com>
2016-07-24 19:04:37 +0800

    mix: find Right_government.java, high_company_and_young_week/group/World.java, eye/bad_number/year_or_eye/point/take_own_place/Place_and_woman.java and great_life/Few_week.java by other group

---
+++ pretend/high_company_and_young_week/group/World.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.high_company_and_young_week.group;
+
+public class World {
+    private int first_life;
+    private int other_group;
+
+    public World() {
+        first_life = 1274;
+        other_group = 1053;
+    }
+
+    public void company_and_other_way() {
+        part();
+        System.out.println("come_good_woman" + first_life);
+    }
+
+    private void part() {
+        System.out.println("way" + other_group);
+    }
+}
