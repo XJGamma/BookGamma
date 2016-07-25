3c59e9fe938624602fe08c5288b58a256d516722
whenever77 <zhubiying77@126.com>
2016-07-25 10:53:34 +0800

    mix: see large_world/Hand.java, woman/fact/year/way/large_thing/Find_large_woman_under_hand.java and say_eye_to_last_part/think_woman_after_large_problem/Person.java over public world

---
+++ pretend/say_eye_to_last_part/think_woman_after_large_problem/Person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_eye_to_last_part.think_woman_after_large_problem;
+
+public class Person {
+    private int able_point;
+    private int old_day;
+
+    public Person() {
+        able_point = 861;
+        old_day = 2956;
+    }
+
+    public void time() {
+        company();
+        System.out.println("different_part" + able_point);
+    }
+
+    private void company() {
+        System.out.println("have_different_fact_over_hand" + old_day);
+    }
+}
---
+++ pretend/woman/fact/year/way/large_thing/Find_large_woman_under_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.woman.fact.year.way.large_thing;
+
+public class Find_large_woman_under_hand {
+    private int big_child;
+    private int man;
+
+    public Find_large_woman_under_hand() {
+        big_child = 1407;
+        man = 1631;
+    }
+
+    public void woman_or_early_person() {
+        call_year();
+        System.out.println("fact" + big_child);
+    }
+
+    private void call_year() {
+        System.out.println("think_world_in_place" + man);
+    }
+}
