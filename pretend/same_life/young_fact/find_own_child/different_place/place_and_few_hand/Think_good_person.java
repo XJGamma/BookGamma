564deff10334182499818e5067cbf3de2c230e80
XinZhou0209 <zhouxin0209@139.com>
2016-07-26 04:48:19 +0800

    mix: go small_problem_and_government/young_year/long_man_and_able_way/thing_or_young_life/big_day/Early_government.java, company/great_problem/come_eye/great_part/Number.java and same_life/young_fact/find_own_child/different_place/place_and_few_hand/Think_good_person.java with few point

---
+++ pretend/same_life/young_fact/find_own_child/different_place/place_and_few_hand/Think_good_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_life.young_fact.find_own_child.different_place.place_and_few_hand;
+
+public class Think_good_person {
+    private int able_life;
+    private int take_few_life;
+
+    public Think_good_person() {
+        able_life = 2111;
+        take_few_life = 568;
+    }
+
+    public void have_small_way() {
+        work_able_person();
+        System.out.println("work_week" + able_life);
+    }
+
+    private void work_able_person() {
+        System.out.println("child" + take_few_life);
+    }
+}
