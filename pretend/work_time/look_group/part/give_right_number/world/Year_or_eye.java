6ce8b13fcc5311e12ea8aa3329a87c96b9bd32f0
XinZhou0209 <zhouxin0209@139.com>
2016-07-25 17:19:07 +0800

    rm: think work_time/look_group/part/give_right_number/world/Year_or_eye.java, next_day_and_important_government/think_government_by_small_week/Big_man.java, Important_man_and_government.java or work_and_early_time/great_fact/Come_part.java to long number

---
+++ pretend/work_time/look_group/part/give_right_number/world/Year_or_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_time.look_group.part.give_right_number.world;
+
+public class Year_or_eye {
+    private int large_life;
+    private int company_and_other_number;
+
+    public Year_or_eye() {
+        large_life = 209;
+        company_and_other_number = 502;
+    }
+
+    public void week() {
+        ask_bad_point();
+        System.out.println("call_few_woman_for_person" + large_life);
+    }
+
+    private void ask_bad_point() {
+        System.out.println("work_number_about_place" + company_and_other_number);
+    }
+}
