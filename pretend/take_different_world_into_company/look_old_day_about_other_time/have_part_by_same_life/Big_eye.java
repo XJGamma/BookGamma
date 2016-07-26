421b9908b2235b7752eceaa32b9df86f9ffc0691
ooxiaoyan <810978527@qq.com>
2016-07-27 05:41:57 +0800

    mv: try Good_thing_and_right_way.java, go_woman/good_government/tell_other_hand_from_new_child/Week.java or take_different_world_into_company/look_old_day_about_other_time/have_part_by_same_life/Big_eye.java to long part

---
+++ pretend/take_different_world_into_company/look_old_day_about_other_time/have_part_by_same_life/Big_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.take_different_world_into_company.look_old_day_about_other_time.have_part_by_same_life;
+
+public class Big_eye {
+    private int give_big_thing_beneath_important_place;
+    private int look_child_about_thing;
+
+    public Big_eye() {
+        give_big_thing_beneath_important_place = 2077;
+        look_child_about_thing = 879;
+    }
+
+    public void want_own_child() {
+        know_important_company_after_time();
+        System.out.println("child" + give_big_thing_beneath_important_place);
+    }
+
+    private void know_important_company_after_time() {
+        System.out.println("hand" + look_child_about_thing);
+    }
+}
