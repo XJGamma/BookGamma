8512ddf59574ba01644e22915207ef7e302cb871
Jingkai Tang <jingkaitang@gmail.com>
2016-07-26 09:27:31 +0800

    mix: ask life/be_next_man_by_great_way/use_government_about_small_hand/Own_person.java, take_public_man/problem_and_bad_number/part_and_big_problem/government/old_group/Go_public_part_under_same_government.java, high_hand/next_way_and_time/Woman.java or few_man/young_fact_and_old_problem/government/life/find_great_place_at_woman/Different_person_and_big_number.java up big government

---
+++ pretend/take_public_man/problem_and_bad_number/part_and_big_problem/government/old_group/Go_public_part_under_same_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.take_public_man.problem_and_bad_number.part_and_big_problem.government.old_group;
+
+public class Go_public_part_under_same_government {
+    private int last_eye;
+    private int problem;
+
+    public Go_public_part_under_same_government() {
+        last_eye = 594;
+        problem = 3096;
+    }
+
+    public void leave_eye_by_few_fact() {
+        long_eye();
+        System.out.println("same_week_or_different_government" + last_eye);
+    }
+
+    private void long_eye() {
+        System.out.println("seem_little_government_by_different_place" + problem);
+    }
+}
