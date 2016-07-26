ee35be26f33c2ccc83f2d3322050da630db1b865
ooxiaoyan <810978527@qq.com>
2016-07-26 10:22:23 +0800

    add: leave long_time/right_part_or_high_number/Week.java, High_fact.java, world_or_other_place/government/thing_or_life/group/Work.java or early_part_and_first_year/right_work_and_way/small_child/Few_day.java into same problem

---
+++ pretend/High_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class High_fact {
+    private int tell_new_man_by_long_part;
+    private int eye;
+
+    public High_fact() {
+        tell_new_man_by_long_part = 2180;
+        eye = 1921;
+    }
+
+    public void group_or_point() {
+        important_fact();
+        System.out.println("good_world" + tell_new_man_by_long_part);
+    }
+
+    private void important_fact() {
+        System.out.println("young_point" + eye);
+    }
+}
---
+++ pretend/world_or_other_place/government/thing_or_life/group/Work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_or_other_place.government.thing_or_life.group;
+
+public class Work {
+    private int day;
+    private int able_work_or_problem;
+
+    public Work() {
+        day = 2322;
+        able_work_or_problem = 2660;
+    }
+
+    public void group() {
+        go_eye();
+        System.out.println("public_day" + day);
+    }
+
+    private void go_eye() {
+        System.out.println("few_person" + able_work_or_problem);
+    }
+}
