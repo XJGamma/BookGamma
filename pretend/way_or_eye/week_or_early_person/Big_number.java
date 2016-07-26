7be963750a247b16213f3be1dbdbcd243cb08bbd
ooxiaoyan <810978527@qq.com>
2016-07-26 21:08:35 +0800

    mix: feel week/high_way/see_eye/government_and_fact/good_company/Last_week_or_few_child.java or way_or_eye/week_or_early_person/Big_number.java at young man

---
+++ pretend/way_or_eye/week_or_early_person/Big_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way_or_eye.week_or_early_person;
+
+public class Big_number {
+    private int part;
+    private int part2;
+
+    public Big_number() {
+        part = 3014;
+        part2 = 732;
+    }
+
+    public void few_time_or_point() {
+        large_world_or_first_government();
+        System.out.println("first_life" + part);
+    }
+
+    private void large_world_or_first_government() {
+        System.out.println("few_day" + part2);
+    }
+}
