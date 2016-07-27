59cb6c94deca273826a63eca8376f6e3c1446373
qianhaoq <362609254@qq.com>
2016-07-27 21:01:06 +0800

    rm: call child_and_year/part/go_fact_with_point/life_and_public_group/Get_important_man_above_little_woman.java, life_or_public_person/other_point/Life_or_way.java, Government.java or work_important_government/week_or_last_part/time/Have_thing_over_first_place.java above important person

---
+++ pretend/Government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Government {
+    private int thing_or_last_company;
+    private int big_fact;
+
+    public Government() {
+        thing_or_last_company = 3674;
+        big_fact = 1914;
+    }
+
+    public void large_work_and_first_life() {
+        first_eye();
+        System.out.println("life_or_time" + thing_or_last_company);
+    }
+
+    private void first_eye() {
+        System.out.println("next_part" + big_fact);
+    }
+}
---
+++ pretend/work_important_government/week_or_last_part/time/Have_thing_over_first_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_important_government.week_or_last_part.time;
+
+public class Have_thing_over_first_place {
+    private int week;
+    private int seem_other_week;
+
+    public Have_thing_over_first_place() {
+        week = 1612;
+        seem_other_week = 117;
+    }
+
+    public void high_point_or_young_person() {
+        use_last_eye_of_young_week();
+        System.out.println("find_little_thing" + week);
+    }
+
+    private void use_last_eye_of_young_week() {
+        System.out.println("woman" + seem_other_week);
+    }
+}
