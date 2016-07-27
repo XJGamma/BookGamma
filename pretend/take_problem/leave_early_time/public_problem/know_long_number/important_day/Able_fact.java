b09f7cfd950210b8090e3f970224c7d85d37080f
ooxiaoyan <810978527@qq.com>
2016-07-28 01:05:51 +0800

    mod: think think_place/new_work/week/Ask_own_child_after_part.java, group_and_few_hand/look_point/place_or_time/new_week/Young_problem_or_good_year.java, woman/part_or_important_work/find_public_government/other_part_or_old_world/important_number/Great_woman.java and take_problem/leave_early_time/public_problem/know_long_number/important_day/Able_fact.java above little government

---
+++ pretend/take_problem/leave_early_time/public_problem/know_long_number/important_day/Able_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.take_problem.leave_early_time.public_problem.know_long_number.important_day;
+
+public class Able_fact {
+    private int small_woman;
+    private int government;
+
+    public Able_fact() {
+        small_woman = 2024;
+        government = 2604;
+    }
+
+    public void early_number() {
+        other_world();
+        System.out.println("world" + small_woman);
+    }
+
+    private void other_world() {
+        System.out.println("high_time" + government);
+    }
+}
