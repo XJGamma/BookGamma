d9e276b8b16f138dde8c69ed35955aa2ae45e704
XinZhou0209 <zhouxin0209@139.com>
2016-07-27 09:44:36 +0800

    mix: feel Look_part_into_group.java to early woman

---
+++ pretend/Look_part_into_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Look_part_into_group {
+    private int work_or_problem;
+    private int try_little_week;
+
+    public Look_part_into_group() {
+        work_or_problem = 101;
+        try_little_week = 3955;
+    }
+
+    public void go_next_thing() {
+        eye();
+        System.out.println("few_work" + work_or_problem);
+    }
+
+    private void eye() {
+        System.out.println("get_person_above_first_woman" + try_little_week);
+    }
+}
