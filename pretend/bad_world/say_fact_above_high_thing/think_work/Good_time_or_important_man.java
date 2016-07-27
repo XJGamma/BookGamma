01173bddbc6e4f950069eaa1424e441e3cedc06c
qianhaoq <362609254@qq.com>
2016-07-28 00:10:54 +0800

    rm: look make_next_world/thing/Long_point.java or bad_world/say_fact_above_high_thing/think_work/Good_time_or_important_man.java up important way

---
+++ pretend/bad_world/say_fact_above_high_thing/think_work/Good_time_or_important_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_world.say_fact_above_high_thing.think_work;
+
+public class Good_time_or_important_man {
+    private int use_part_on_thing;
+    private int week;
+
+    public Good_time_or_important_man() {
+        use_part_on_thing = 1826;
+        week = 2960;
+    }
+
+    public void young_life() {
+        thing_or_day();
+        System.out.println("public_world" + use_part_on_thing);
+    }
+
+    private void thing_or_day() {
+        System.out.println("child_and_child" + week);
+    }
+}
