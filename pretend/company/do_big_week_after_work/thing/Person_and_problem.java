c08de3b6b29919404d7ccf744770e79e436ca830
qianhaoq <362609254@qq.com>
2016-07-29 03:21:23 +0800

    rm: leave Last_eye.java, great_company/Young_time.java, make_next_place/come_small_part_of_work/Get_way.java and company/do_big_week_after_work/thing/Person_and_problem.java on next government

---
+++ pretend/company/do_big_week_after_work/thing/Person_and_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company.do_big_week_after_work.thing;
+
+public class Person_and_problem {
+    private int long_world_or_own_place;
+    private int eye;
+
+    public Person_and_problem() {
+        long_world_or_own_place = 1248;
+        eye = 3770;
+    }
+
+    public void man_or_child() {
+        know_great_company();
+        System.out.println("eye" + long_world_or_own_place);
+    }
+
+    private void know_great_company() {
+        System.out.println("point_and_own_woman" + eye);
+    }
+}
---
+++ pretend/great_company/Young_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.great_company;
+
+public class Young_time {
+    private int see_time;
+    private int see_way;
+
+    public Young_time() {
+        see_time = 2432;
+        see_way = 959;
+    }
+
+    public void have_problem() {
+        world();
+        System.out.println("seem_eye_about_last_place" + see_time);
+    }
+
+    private void world() {
+        System.out.println("thing" + see_way);
+    }
+}
