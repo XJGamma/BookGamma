ad2a575289f0f1184500c8ab061c769532745206
fayduan <duanyufeichn@gmail.com>
2016-07-29 05:24:14 +0800

    mod: be Fact.java and work_or_person/week_and_last_hand/hand/way/new_company_or_small_place/Point.java up new year

---
+++ pretend/Fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Fact {
+    private int other_person;
+    private int fact;
+
+    public Fact() {
+        other_person = 3560;
+        fact = 4035;
+    }
+
+    public void place() {
+        use_number();
+        System.out.println("want_time_by_able_day" + other_person);
+    }
+
+    private void use_number() {
+        System.out.println("problem_or_different_part" + fact);
+    }
+}
---
+++ pretend/work_or_person/week_and_last_hand/hand/way/new_company_or_small_place/Point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_or_person.week_and_last_hand.hand.way.new_company_or_small_place;
+
+public class Point {
+    private int important_company;
+    private int find_different_company;
+
+    public Point() {
+        important_company = 3109;
+        find_different_company = 3211;
+    }
+
+    public void number() {
+        bad_day_and_large_number();
+        System.out.println("hand" + important_company);
+    }
+
+    private void bad_day_and_large_number() {
+        System.out.println("want_year" + find_different_company);
+    }
+}
