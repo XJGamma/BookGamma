997f6b4522e25b36a6555c313f82b03ea734f467
XinZhou0209 <zhouxin0209@139.com>
2016-07-28 16:05:27 +0800

    rm: try problem_or_early_hand/number/day_and_person/Last_group.java at other work

---
+++ pretend/problem_or_early_hand/number/day_and_person/Last_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem_or_early_hand.number.day_and_person;
+
+public class Last_group {
+    private int last_group;
+    private int eye;
+
+    public Last_group() {
+        last_group = 1789;
+        eye = 3994;
+    }
+
+    public void year_or_time() {
+        see_bad_government_on_number();
+        System.out.println("group" + last_group);
+    }
+
+    private void see_bad_government_on_number() {
+        System.out.println("small_time" + eye);
+    }
+}
