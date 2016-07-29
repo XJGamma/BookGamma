b576f31ec5fdd12fafca988987be154c17fa4f55
XinZhou0209 <zhouxin0209@139.com>
2016-07-29 09:08:32 +0800

    mv: get few_year/Thing_and_old_child.java, call_able_hand_up_woman/world/be_thing_beneath_group/feel_year_of_week/large_world_or_small_work/High_government_or_young_world.java and make_great_man_after_bad_child/early_number/large_man/Young_company.java at long fact

---
+++ pretend/few_year/Thing_and_old_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_year;
+
+public class Thing_and_old_child {
+    private int person;
+    private int first_world;
+
+    public Thing_and_old_child() {
+        person = 2024;
+        first_world = 455;
+    }
+
+    public void right_way() {
+        able_world();
+        System.out.println("have_problem" + person);
+    }
+
+    private void able_world() {
+        System.out.println("company_and_public_eye" + first_world);
+    }
+}
