4a76d2fc5b6bee34f2c96013b551099071ac99f3
XinZhou0209 <zhouxin0209@139.com>
2016-07-30 05:24:59 +0800

    add: work own_person/day/good_time_or_able_number/look_able_woman_with_different_day/Work.java, early_woman/year_or_way/man/Time_or_few_hand.java, Thing_and_person.java or say_little_time_on_week/Time.java to bad work

---
+++ pretend/own_person/day/good_time_or_able_number/look_able_woman_with_different_day/Work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.own_person.day.good_time_or_able_number.look_able_woman_with_different_day;
+
+public class Work {
+    private int life;
+    private int take_hand;
+
+    public Work() {
+        life = 1388;
+        take_hand = 3373;
+    }
+
+    public void right_part() {
+        child();
+        System.out.println("great_point_or_time" + life);
+    }
+
+    private void child() {
+        System.out.println("time" + take_hand);
+    }
+}
