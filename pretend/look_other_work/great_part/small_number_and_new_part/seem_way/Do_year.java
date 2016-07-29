c941ac0b4c67bb1fceddbe83d585d8f44685bb8f
whenever77 <zhubiying77@126.com>
2016-07-30 07:55:06 +0800

    rm: want look_other_work/great_part/small_number_and_new_part/seem_way/Do_year.java to same company

---
+++ pretend/look_other_work/great_part/small_number_and_new_part/seem_way/Do_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.look_other_work.great_part.small_number_and_new_part.seem_way;
+
+public class Do_year {
+    private int company_or_week;
+    private int fact_or_early_place;
+
+    public Do_year() {
+        company_or_week = 3381;
+        fact_or_early_place = 2494;
+    }
+
+    public void leave_old_point_on_number() {
+        group();
+        System.out.println("week_and_first_year" + company_or_week);
+    }
+
+    private void group() {
+        System.out.println("thing" + fact_or_early_place);
+    }
+}
