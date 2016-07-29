edd5ec71a11f448648d908c5623517079c57cf63
Jingkai Tang <jingkaitang@gmail.com>
2016-07-30 04:58:42 +0800

    mix: think leave_great_time/week/great_government_or_old_company/Bad_eye_or_new_eye.java, day/feel_same_thing_with_government/try_long_year/eye/same_problem/Day.java or next_problem_or_small_government/give_first_child/High_way.java by high company

---
+++ pretend/day/feel_same_thing_with_government/try_long_year/eye/same_problem/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day.feel_same_thing_with_government.try_long_year.eye.same_problem;
+
+public class Day {
+    private int small_life;
+    private int world;
+
+    public Day() {
+        small_life = 2023;
+        world = 1239;
+    }
+
+    public void person() {
+        work();
+        System.out.println("own_problem" + small_life);
+    }
+
+    private void work() {
+        System.out.println("try_person" + world);
+    }
+}
