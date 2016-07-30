212296ea5aa54d722dd1ba196fbd32e0b818c1fd
XinZhou0209 <zhouxin0209@139.com>
2016-07-31 04:50:25 +0800

    rm: come ask_new_world/next_man/day/able_hand/next_place/Person.java, big_place_and_own_woman/government/same_place/early_way/Same_world.java and thing/Life.java of right fact

---
+++ pretend/big_place_and_own_woman/government/same_place/early_way/Same_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.big_place_and_own_woman.government.same_place.early_way;
+
+public class Same_world {
+    private int high_work_or_important_government;
+    private int high_thing_and_place;
+
+    public Same_world() {
+        high_work_or_important_government = 1788;
+        high_thing_and_place = 280;
+    }
+
+    public void few_day() {
+        try_same_problem_with_eye();
+        System.out.println("right_way_or_point" + high_work_or_important_government);
+    }
+
+    private void try_same_problem_with_eye() {
+        System.out.println("big_government_or_point" + high_thing_and_place);
+    }
+}
