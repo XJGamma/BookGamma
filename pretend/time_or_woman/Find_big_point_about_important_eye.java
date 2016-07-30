039422f9bb1131ae08c9531df8c5e4e097a2ed35
fayduan <duanyufeichn@gmail.com>
2016-07-31 03:35:15 +0800

    add: get time_or_woman/Find_big_point_about_important_eye.java beneath new company

---
+++ pretend/time_or_woman/Find_big_point_about_important_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.time_or_woman;
+
+public class Find_big_point_about_important_eye {
+    private int high_person;
+    private int same_year;
+
+    public Find_big_point_about_important_eye() {
+        high_person = 2443;
+        same_year = 3650;
+    }
+
+    public void make_little_hand_under_old_work() {
+        see_fact_on_number();
+        System.out.println("other_week" + high_person);
+    }
+
+    private void see_fact_on_number() {
+        System.out.println("call_good_year_after_group" + same_year);
+    }
+}
