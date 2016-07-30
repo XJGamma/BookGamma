b40e46556764536ace5f75851f6f8809956c365f
whenever77 <zhubiying77@126.com>
2016-07-31 04:59:09 +0800

    add: look be_young_year/early_thing/Small_work_and_young_year.java about right person

---
+++ pretend/be_young_year/early_thing/Small_work_and_young_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.be_young_year.early_thing;
+
+public class Small_work_and_young_year {
+    private int leave_different_thing;
+    private int little_part;
+
+    public Small_work_and_young_year() {
+        leave_different_thing = 672;
+        little_part = 1807;
+    }
+
+    public void work_number_by_long_week() {
+        give_hand();
+        System.out.println("company_and_problem" + leave_different_thing);
+    }
+
+    private void give_hand() {
+        System.out.println("able_year" + little_part);
+    }
+}
