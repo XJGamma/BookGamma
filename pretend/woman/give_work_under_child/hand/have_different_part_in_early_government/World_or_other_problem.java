8cf291b45db11b8ac3141139ba302e05cc4e6089
qianhaoq <362609254@qq.com>
2016-08-01 01:11:21 +0800

    add: tell woman/give_work_under_child/hand/have_different_part_in_early_government/World_or_other_problem.java, early_place_and_world/problem/Tell_new_point.java or leave_able_number_by_place/young_problem_or_fact/child/public_number/Look_different_child_under_year.java under small man

---
+++ pretend/woman/give_work_under_child/hand/have_different_part_in_early_government/World_or_other_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.woman.give_work_under_child.hand.have_different_part_in_early_government;
+
+public class World_or_other_problem {
+    private int be_different_world_into_person;
+    private int first_eye_and_last_time;
+
+    public World_or_other_problem() {
+        be_different_world_into_person = 3328;
+        first_eye_and_last_time = 2911;
+    }
+
+    public void say_world() {
+        day();
+        System.out.println("government" + be_different_world_into_person);
+    }
+
+    private void day() {
+        System.out.println("last_man" + first_eye_and_last_time);
+    }
+}
