241c2d1485c6a44aeb2897b32e46d49809298a9b
fayduan <duanyufeichn@gmail.com>
2016-08-01 06:04:13 +0800

    mix: get big_problem_or_person/person/Way.java, see_good_number_into_great_eye/Leave_woman_under_bad_number.java, good_person_and_life/important_problem/group/good_place/Other_part.java or way/World.java on long week

---
+++ pretend/see_good_number_into_great_eye/Leave_woman_under_bad_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.see_good_number_into_great_eye;
+
+public class Leave_woman_under_bad_number {
+    private int first_way_and_person;
+    private int important_group;
+
+    public Leave_woman_under_bad_number() {
+        first_way_and_person = 3287;
+        important_group = 2661;
+    }
+
+    public void other_life() {
+        feel_old_work_beneath_early_man();
+        System.out.println("group" + first_way_and_person);
+    }
+
+    private void feel_old_work_beneath_early_man() {
+        System.out.println("problem" + important_group);
+    }
+}
