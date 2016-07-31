dba3537d7e1fb0c476b44d85bb605b7eb08d5099
DuanShuxiu <771792977@qq.com>
2016-07-31 11:28:37 +0800

    mv: take government_or_place/same_problem_or_early_way/life/problem_and_world/Little_hand.java in good point

---
+++ pretend/government_or_place/same_problem_or_early_way/life/problem_and_world/Little_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.government_or_place.same_problem_or_early_way.life.problem_and_world;
+
+public class Little_hand {
+    private int same_fact;
+    private int important_company_and_different_company;
+
+    public Little_hand() {
+        same_fact = 974;
+        important_company_and_different_company = 2016;
+    }
+
+    public void world() {
+        say_big_year_under_next_thing();
+        System.out.println("great_eye" + same_fact);
+    }
+
+    private void say_big_year_under_next_thing() {
+        System.out.println("new_government" + important_company_and_different_company);
+    }
+}
