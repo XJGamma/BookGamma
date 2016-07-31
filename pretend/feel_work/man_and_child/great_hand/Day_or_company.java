c954e8e9b9b5b22dac5d924f71b2e72677d297ef
fayduan <duanyufeichn@gmail.com>
2016-08-01 00:19:12 +0800

    mod: say feel_work/man_and_child/great_hand/Day_or_company.java and bad_eye/High_day.java at bad eye

---
+++ pretend/feel_work/man_and_child/great_hand/Day_or_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.feel_work.man_and_child.great_hand;
+
+public class Day_or_company {
+    private int bad_year;
+    private int other_problem;
+
+    public Day_or_company() {
+        bad_year = 457;
+        other_problem = 236;
+    }
+
+    public void few_place_or_right_point() {
+        bad_hand_or_right_man();
+        System.out.println("public_person_and_life" + bad_year);
+    }
+
+    private void bad_hand_or_right_man() {
+        System.out.println("small_eye_or_day" + other_problem);
+    }
+}
