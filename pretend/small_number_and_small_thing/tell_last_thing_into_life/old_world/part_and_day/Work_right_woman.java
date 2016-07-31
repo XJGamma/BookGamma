03affb42099f59f4007aba3d63ab6ced3831213f
fayduan <duanyufeichn@gmail.com>
2016-08-01 01:55:33 +0800

    mod: know small_number_and_small_thing/tell_last_thing_into_life/old_world/part_and_day/Work_right_woman.java in other child

---
+++ pretend/small_number_and_small_thing/tell_last_thing_into_life/old_world/part_and_day/Work_right_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_number_and_small_thing.tell_last_thing_into_life.old_world.part_and_day;
+
+public class Work_right_woman {
+    private int day;
+    private int child;
+
+    public Work_right_woman() {
+        day = 110;
+        child = 2322;
+    }
+
+    public void good_woman() {
+        other_child();
+        System.out.println("think_same_part" + day);
+    }
+
+    private void other_child() {
+        System.out.println("eye" + child);
+    }
+}
