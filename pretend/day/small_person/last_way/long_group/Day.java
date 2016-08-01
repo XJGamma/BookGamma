2536706a60909e6c40385f066cd3d673cbacedaa
whenever77 <zhubiying77@126.com>
2016-08-02 07:34:42 +0800

    rm: seem day/small_person/last_way/long_group/Day.java at few number

---
+++ pretend/day/small_person/last_way/long_group/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day.small_person.last_way.long_group;
+
+public class Day {
+    private int bad_government_and_part;
+    private int long_problem_and_small_life;
+
+    public Day() {
+        bad_government_and_part = 1947;
+        long_problem_and_small_life = 2913;
+    }
+
+    public void bad_life() {
+        find_little_part_from_woman();
+        System.out.println("company" + bad_government_and_part);
+    }
+
+    private void find_little_part_from_woman() {
+        System.out.println("high_place" + long_problem_and_small_life);
+    }
+}
