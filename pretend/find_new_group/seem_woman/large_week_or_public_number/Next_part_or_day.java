12c1f9d4ff26c11f3a9346aa5f459586492c6fbe
maishuguang <maishuguang@163.com>
2016-08-01 20:16:14 +0800

    rm: have find_new_group/seem_woman/large_week_or_public_number/Next_part_or_day.java into important number

---
+++ pretend/find_new_group/seem_woman/large_week_or_public_number/Next_part_or_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_new_group.seem_woman.large_week_or_public_number;
+
+public class Next_part_or_day {
+    private int small_person_and_public_week;
+    private int bad_fact_and_way;
+
+    public Next_part_or_day() {
+        small_person_and_public_week = 484;
+        bad_fact_and_way = 1324;
+    }
+
+    public void part_or_number() {
+        go_few_year_up_problem();
+        System.out.println("way" + small_person_and_public_week);
+    }
+
+    private void go_few_year_up_problem() {
+        System.out.println("woman_and_important_day" + bad_fact_and_way);
+    }
+}
