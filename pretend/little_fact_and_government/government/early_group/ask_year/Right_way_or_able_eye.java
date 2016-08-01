5dc610763f9a005e6a84fb13e2ea88798c37f66d
XinZhou0209 <zhouxin0209@139.com>
2016-08-01 10:52:44 +0800

    rm: try leave_same_child_after_big_part/group/thing/make_new_year_at_next_thing/child_or_own_day/Person_or_company.java, little_fact_and_government/government/early_group/ask_year/Right_way_or_able_eye.java, get_child_to_woman/work/eye/different_number/Feel_day_with_little_person.java or life/different_week/child/big_point_and_few_number/hand/Great_fact_or_thing.java in different government

---
+++ pretend/little_fact_and_government/government/early_group/ask_year/Right_way_or_able_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_fact_and_government.government.early_group.ask_year;
+
+public class Right_way_or_able_eye {
+    private int time;
+    private int give_company_of_thing;
+
+    public Right_way_or_able_eye() {
+        time = 2019;
+        give_company_of_thing = 3017;
+    }
+
+    public void long_child_or_bad_man() {
+        say_work();
+        System.out.println("few_day_and_same_world" + time);
+    }
+
+    private void say_work() {
+        System.out.println("number" + give_company_of_thing);
+    }
+}
