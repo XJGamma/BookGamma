b335fb9cef539c8c874430a8dac984340b6125be
ooxiaoyan <810978527@qq.com>
2016-08-01 22:51:40 +0800

    mod: take last_work_or_own_woman/get_small_day/person/part/year_or_other_fact/Point.java, Place_and_other_government.java, try_long_week/leave_few_point_into_man/own_week/week/high_day/Point.java and woman/large_world/World.java to able time

---
+++ pretend/Place_and_other_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Place_and_other_government {
+    private int point;
+    private int first_child;
+
+    public Place_and_other_government() {
+        point = 1660;
+        first_child = 3204;
+    }
+
+    public void problem() {
+        thing_or_woman();
+        System.out.println("important_government" + point);
+    }
+
+    private void thing_or_woman() {
+        System.out.println("eye_or_next_hand" + first_child);
+    }
+}
