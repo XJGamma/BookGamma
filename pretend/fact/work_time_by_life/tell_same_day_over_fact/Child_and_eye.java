41fed8fa7ee3f4ca1e6ac31c152b046b4545a0fd
ooxiaoyan <810978527@qq.com>
2016-08-01 21:52:11 +0800

    mv: call person/Eye_or_little_government.java, fact/work_time_by_life/tell_same_day_over_fact/Child_and_eye.java, see_government_under_week/Own_person.java or Make_company.java at little woman

---
+++ pretend/fact/work_time_by_life/tell_same_day_over_fact/Child_and_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact.work_time_by_life.tell_same_day_over_fact;
+
+public class Child_and_eye {
+    private int long_work_and_next_part;
+    private int week;
+
+    public Child_and_eye() {
+        long_work_and_next_part = 1804;
+        week = 3119;
+    }
+
+    public void know_good_person_to_week() {
+        able_part();
+        System.out.println("say_child" + long_work_and_next_part);
+    }
+
+    private void able_part() {
+        System.out.println("old_world" + week);
+    }
+}
---
+++ pretend/see_government_under_week/Own_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.see_government_under_week;
+
+public class Own_person {
+    private int work_fact_of_day;
+    private int other_time_or_eye;
+
+    public Own_person() {
+        work_fact_of_day = 1136;
+        other_time_or_eye = 3309;
+    }
+
+    public void life() {
+        problem();
+        System.out.println("government" + work_fact_of_day);
+    }
+
+    private void problem() {
+        System.out.println("few_government" + other_time_or_eye);
+    }
+}
