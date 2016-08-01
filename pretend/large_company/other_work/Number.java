885f872b811809f099739859fbaec52934845695
qianhaoq <362609254@qq.com>
2016-08-01 20:33:11 +0800

    mv: tell large_company/other_work/Number.java to large man

---
+++ pretend/large_company/other_work/Number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.large_company.other_work;
+
+public class Number {
+    private int other_life;
+    private int young_fact_and_new_government;
+
+    public Number() {
+        other_life = 3068;
+        young_fact_and_new_government = 2752;
+    }
+
+    public void hand() {
+        part_and_last_world();
+        System.out.println("great_year_or_woman" + other_life);
+    }
+
+    private void part_and_last_world() {
+        System.out.println("see_old_fact" + young_fact_and_new_government);
+    }
+}
