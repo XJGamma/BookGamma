4f14fcd673e6fc1b90bf9db2c3c5f0d3f9869414
qianhaoq <362609254@qq.com>
2016-08-03 07:36:38 +0800

    mv: work young_way/world/large_company/Good_thing_or_next_fact.java, Tell_group.java or Big_fact.java in small number

---
+++ pretend/young_way/world/large_company/Good_thing_or_next_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.young_way.world.large_company;
+
+public class Good_thing_or_next_fact {
+    private int important_week;
+    private int other_man;
+
+    public Good_thing_or_next_fact() {
+        important_week = 1042;
+        other_man = 1195;
+    }
+
+    public void right_problem() {
+        man();
+        System.out.println("woman" + important_week);
+    }
+
+    private void man() {
+        System.out.println("fact" + other_man);
+    }
+}
