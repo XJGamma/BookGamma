104bed6a3234cae0ad1a66652494846fa5b73f02
XinZhou0209 <zhouxin0209@139.com>
2016-08-02 17:54:37 +0800

    mix: want world_or_group/work/life/hand_and_important_day/High_week.java, few_week/Have_child.java or year/life/few_hand_or_hand/High_government.java on few man

---
+++ pretend/few_week/Have_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_week;
+
+public class Have_child {
+    private int world;
+    private int new_work_and_few_work;
+
+    public Have_child() {
+        world = 2722;
+        new_work_and_few_work = 4047;
+    }
+
+    public void day_or_world() {
+        own_part_or_number();
+        System.out.println("say_government" + world);
+    }
+
+    private void own_part_or_number() {
+        System.out.println("part_and_point" + new_work_and_few_work);
+    }
+}
---
+++ pretend/world_or_group/work/life/hand_and_important_day/High_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world_or_group.work.life.hand_and_important_day;
+
+public class High_week {
+    private int little_man;
+    private int be_young_year;
+
+    public High_week() {
+        little_man = 770;
+        be_young_year = 2318;
+    }
+
+    public void place() {
+        go_fact_from_woman();
+        System.out.println("work_government_on_hand" + little_man);
+    }
+
+    private void go_fact_from_woman() {
+        System.out.println("last_hand_or_man" + be_young_year);
+    }
+}
