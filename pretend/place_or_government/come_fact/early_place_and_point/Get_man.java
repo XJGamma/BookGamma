e653b173234bf71d407334441c5d7a76bc9c828f
XinZhou0209 <zhouxin0209@139.com>
2016-08-02 15:37:20 +0800

    add: come great_world/man/Life.java, different_year_and_number/tell_time_with_few_world/Week.java, world/last_life_or_week/Same_number_and_different_work.java or place_or_government/come_fact/early_place_and_point/Get_man.java in long government

---
+++ pretend/place_or_government/come_fact/early_place_and_point/Get_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place_or_government.come_fact.early_place_and_point;
+
+public class Get_man {
+    private int point;
+    private int place;
+
+    public Get_man() {
+        point = 3444;
+        place = 2660;
+    }
+
+    public void problem() {
+        world();
+        System.out.println("bad_person_and_public_person" + point);
+    }
+
+    private void world() {
+        System.out.println("different_fact" + place);
+    }
+}
