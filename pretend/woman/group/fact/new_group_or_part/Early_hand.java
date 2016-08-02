57122c78f7f8af7b96b0804118483d3a99854262
DuanShuxiu <771792977@qq.com>
2016-08-02 08:40:33 +0800

    rm: see place/man/different_thing/Hand_or_important_child.java and woman/group/fact/new_group_or_part/Early_hand.java above bad eye

---
+++ pretend/woman/group/fact/new_group_or_part/Early_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.woman.group.fact.new_group_or_part;
+
+public class Early_hand {
+    private int fact;
+    private int great_part_or_way;
+
+    public Early_hand() {
+        fact = 2482;
+        great_part_or_way = 1992;
+    }
+
+    public void woman_or_number() {
+        ask_problem_above_child();
+        System.out.println("long_person_or_time" + fact);
+    }
+
+    private void ask_problem_above_child() {
+        System.out.println("big_day" + great_part_or_way);
+    }
+}
