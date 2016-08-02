ef174c1233c1320fc3a8d4f7fa97ef45e3c451fd
whenever77 <zhubiying77@126.com>
2016-08-02 17:02:50 +0800

    rm: look Different_hand.java or man_or_company/point_and_fact/Way.java on young thing

---
+++ pretend/man_or_company/point_and_fact/Way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man_or_company.point_and_fact;
+
+public class Way {
+    private int good_week;
+    private int eye_or_place;
+
+    public Way() {
+        good_week = 1935;
+        eye_or_place = 3705;
+    }
+
+    public void part() {
+        try_woman();
+        System.out.println("old_work_or_first_woman" + good_week);
+    }
+
+    private void try_woman() {
+        System.out.println("time" + eye_or_place);
+    }
+}
