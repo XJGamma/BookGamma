8b4a0df6b5090b1e06da4a903afce9aa0dbaa245
XinZhou0209 <zhouxin0209@139.com>
2016-08-04 01:58:26 +0800

    add: tell Small_world_and_time.java, public_point/seem_year/try_long_group/want_long_time_to_different_thing/year/Try_first_problem.java or important_government_and_different_fact/make_large_part_up_early_child/little_life_or_large_day/right_life_or_small_part/number/Young_problem.java from next part

---
+++ pretend/important_government_and_different_fact/make_large_part_up_early_child/little_life_or_large_day/right_life_or_small_part/number/Young_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_government_and_different_fact.make_large_part_up_early_child.little_life_or_large_day.right_life_or_small_part.number;
+
+public class Young_problem {
+    private int great_part_or_high_thing;
+    private int different_week_and_eye;
+
+    public Young_problem() {
+        great_part_or_high_thing = 1480;
+        different_week_and_eye = 3534;
+    }
+
+    public void life_and_public_child() {
+        week_and_world();
+        System.out.println("long_child" + great_part_or_high_thing);
+    }
+
+    private void week_and_world() {
+        System.out.println("thing" + different_week_and_eye);
+    }
+}
