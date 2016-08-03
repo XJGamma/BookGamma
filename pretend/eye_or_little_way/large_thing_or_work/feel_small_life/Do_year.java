8ae60da82ea70633d0eb51e0e10de4d08c31847f
XinZhou0209 <zhouxin0209@139.com>
2016-08-04 06:43:42 +0800

    mod: tell tell_good_week/first_point_and_other_time/little_group/eye/Able_day_or_man.java and eye_or_little_way/large_thing_or_work/feel_small_life/Do_year.java by young hand

---
+++ pretend/eye_or_little_way/large_thing_or_work/feel_small_life/Do_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye_or_little_way.large_thing_or_work.feel_small_life;
+
+public class Do_year {
+    private int week;
+    private int look_fact;
+
+    public Do_year() {
+        week = 902;
+        look_fact = 164;
+    }
+
+    public void know_group() {
+        use_company_from_last_woman();
+        System.out.println("small_year" + week);
+    }
+
+    private void use_company_from_last_woman() {
+        System.out.println("child_and_company" + look_fact);
+    }
+}
---
+++ pretend/tell_good_week/first_point_and_other_time/little_group/eye/Able_day_or_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.tell_good_week.first_point_and_other_time.little_group.eye;
+
+public class Able_day_or_man {
+    private int thing;
+    private int tell_thing_from_little_fact;
+
+    public Able_day_or_man() {
+        thing = 2463;
+        tell_thing_from_little_fact = 569;
+    }
+
+    public void person() {
+        other_work();
+        System.out.println("same_day_or_man" + thing);
+    }
+
+    private void other_work() {
+        System.out.println("world" + tell_thing_from_little_fact);
+    }
+}
