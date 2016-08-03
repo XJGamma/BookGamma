7fbe9afc2b7932614e300f04bc5f73ddb72ebf20
ooxiaoyan <810978527@qq.com>
2016-08-04 00:19:59 +0800

    mix: go next_week/man/different_world_or_good_part/different_group/other_fact/Thing.java, number/eye/think_little_problem_to_problem/Point.java or ask_week/problem_and_first_number/world/Person_and_part.java after good government

---
+++ pretend/next_week/man/different_world_or_good_part/different_group/other_fact/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_week.man.different_world_or_good_part.different_group.other_fact;
+
+public class Thing {
+    private int world_or_person;
+    private int life;
+
+    public Thing() {
+        world_or_person = 3881;
+        life = 2205;
+    }
+
+    public void person() {
+        have_public_day();
+        System.out.println("leave_long_person_over_young_point" + world_or_person);
+    }
+
+    private void have_public_day() {
+        System.out.println("work" + life);
+    }
+}
