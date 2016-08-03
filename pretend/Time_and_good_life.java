2adefdd029d2072880be7fc6a45bd62db374cff9
fayduan <duanyufeichn@gmail.com>
2016-08-03 16:06:23 +0800

    mod: know Time_and_good_life.java on old company

---
+++ pretend/Time_and_good_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Time_and_good_life {
+    private int small_eye;
+    private int last_day;
+
+    public Time_and_good_life() {
+        small_eye = 2951;
+        last_day = 713;
+    }
+
+    public void different_life_and_problem() {
+        work_person();
+        System.out.println("great_man" + small_eye);
+    }
+
+    private void work_person() {
+        System.out.println("take_big_woman_above_government" + last_day);
+    }
+}
