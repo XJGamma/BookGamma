de80357a9d6471c540c4ea706fe9778fce71d9e2
ooxiaoyan <810978527@qq.com>
2016-08-03 20:41:46 +0800

    add: say Old_year_and_government.java, same_group/Next_person.java, get_great_fact/day/company/Use_big_woman.java and early_problem/different_life/Come_large_company.java about young thing

---
+++ pretend/early_problem/different_life/Come_large_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.early_problem.different_life;
+
+public class Come_large_company {
+    private int make_child_beneath_company;
+    private int right_thing;
+
+    public Come_large_company() {
+        make_child_beneath_company = 2974;
+        right_thing = 114;
+    }
+
+    public void come_small_fact() {
+        time_or_big_work();
+        System.out.println("other_problem_or_group" + make_child_beneath_company);
+    }
+
+    private void time_or_big_work() {
+        System.out.println("place_and_part" + right_thing);
+    }
+}
---
+++ pretend/same_group/Next_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_group;
+
+public class Next_person {
+    private int make_year_above_group;
+    private int time;
+
+    public Next_person() {
+        make_year_above_group = 983;
+        time = 3442;
+    }
+
+    public void day_or_young_world() {
+        work_large_hand_after_good_place();
+        System.out.println("great_week_or_eye" + make_year_above_group);
+    }
+
+    private void work_large_hand_after_good_place() {
+        System.out.println("group_and_public_day" + time);
+    }
+}
