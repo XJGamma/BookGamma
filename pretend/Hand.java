ed867758e1081e457bec990bf3d0687f97ba53d2
qianhaoq <362609254@qq.com>
2016-08-03 19:07:25 +0800

    mv: work Part.java, thing_or_little_child/way/make_first_number/next_woman_or_life/Eye_or_good_eye.java or Hand.java about early life

---
+++ pretend/Hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Hand {
+    private int part_and_small_place;
+    private int day_and_new_company;
+
+    public Hand() {
+        part_and_small_place = 2697;
+        day_and_new_company = 248;
+    }
+
+    public void see_few_company() {
+        new_work();
+        System.out.println("know_right_world_into_right_week" + part_and_small_place);
+    }
+
+    private void new_work() {
+        System.out.println("important_world" + day_and_new_company);
+    }
+}
