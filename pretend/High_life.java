1fac7dae150e4e42a2bcf094e93e5cd4e6aff418
XinZhou0209 <zhouxin0209@139.com>
2016-08-04 17:45:26 +0800

    mod: get High_life.java, Use_company_at_place.java and great_fact/New_problem_or_life.java for other place

---
+++ pretend/High_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class High_life {
+    private int come_life;
+    private int man;
+
+    public High_life() {
+        come_life = 2854;
+        man = 525;
+    }
+
+    public void feel_time() {
+        fact_and_place();
+        System.out.println("number" + come_life);
+    }
+
+    private void fact_and_place() {
+        System.out.println("woman_and_long_place" + man);
+    }
+}
