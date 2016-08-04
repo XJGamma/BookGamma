916a01a2407314f607bc1f2290c86e7a30088b18
ooxiaoyan <810978527@qq.com>
2016-08-04 09:20:14 +0800

    rm: make find_large_way/try_time/First_week_and_problem.java, number/way/thing_or_work/next_place/feel_person_about_part/Hand.java or early_company/world/own_problem_or_world/next_day/thing_or_right_eye/Fact_or_week.java for first week

---
+++ pretend/find_large_way/try_time/First_week_and_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_large_way.try_time;
+
+public class First_week_and_problem {
+    private int go_different_day;
+    private int take_small_time_beneath_time;
+
+    public First_week_and_problem() {
+        go_different_day = 3030;
+        take_small_time_beneath_time = 1381;
+    }
+
+    public void bad_woman() {
+        man();
+        System.out.println("use_man_over_small_year" + go_different_day);
+    }
+
+    private void man() {
+        System.out.println("different_way_and_group" + take_small_time_beneath_time);
+    }
+}
