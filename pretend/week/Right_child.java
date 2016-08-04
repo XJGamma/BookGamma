918375857012addb25597f3e8f42968e64264ecd
whenever77 <zhubiying77@126.com>
2016-08-04 16:50:49 +0800

    mv: take seem_week_about_government/find_point_for_thing/great_woman/man/want_woman_above_next_company/Use_small_place.java or week/Right_child.java by last fact

---
+++ pretend/week/Right_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week;
+
+public class Right_child {
+    private int time;
+    private int life_and_part;
+
+    public Right_child() {
+        time = 2890;
+        life_and_part = 1576;
+    }
+
+    public void eye_or_long_government() {
+        work();
+        System.out.println("do_bad_work_under_important_year" + time);
+    }
+
+    private void work() {
+        System.out.println("leave_few_hand" + life_and_part);
+    }
+}
