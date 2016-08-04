b10b9399279b783ee063fc1cebaecc23988a1b1d
DuanShuxiu <771792977@qq.com>
2016-08-04 20:14:30 +0800

    rm: leave good_thing/big_government/high_part/part/week_and_life/Way_or_place.java, government/Tell_good_time_on_year.java, old_government_or_small_eye/Do_hand.java and try_week/world/bad_day/Right_time.java from good child

---
+++ pretend/good_thing/big_government/high_part/part/week_and_life/Way_or_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_thing.big_government.high_part.part.week_and_life;
+
+public class Way_or_place {
+    private int place_or_week;
+    private int own_problem;
+
+    public Way_or_place() {
+        place_or_week = 2569;
+        own_problem = 3875;
+    }
+
+    public void large_week_or_government() {
+        other_week_and_thing();
+        System.out.println("right_part" + place_or_week);
+    }
+
+    private void other_week_and_thing() {
+        System.out.println("group" + own_problem);
+    }
+}
