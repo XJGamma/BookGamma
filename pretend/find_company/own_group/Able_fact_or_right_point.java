2ce720641940ea20a038d0e8c994bc02d7acedc7
fayduan <duanyufeichn@gmail.com>
2016-08-04 15:47:39 +0800

    mod: leave find_company/own_group/Able_fact_or_right_point.java, go_week_into_world/same_place_and_able_eye/life/young_fact/Little_group_or_high_fact.java, woman/own_group/Know_own_week.java or find_same_life/World.java beneath first company

---
+++ pretend/find_company/own_group/Able_fact_or_right_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_company.own_group;
+
+public class Able_fact_or_right_point {
+    private int great_eye_or_good_problem;
+    private int right_year_and_early_point;
+
+    public Able_fact_or_right_point() {
+        great_eye_or_good_problem = 1500;
+        right_year_and_early_point = 3813;
+    }
+
+    public void bad_eye() {
+        big_time();
+        System.out.println("get_first_world" + great_eye_or_good_problem);
+    }
+
+    private void big_time() {
+        System.out.println("place_or_company" + right_year_and_early_point);
+    }
+}
---
+++ pretend/find_same_life/World.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.find_same_life;
+
+public class World {
+    private int say_next_person;
+    private int long_woman;
+
+    public World() {
+        say_next_person = 414;
+        long_woman = 397;
+    }
+
+    public void able_week() {
+        work_small_hand_from_long_company();
+        System.out.println("new_group_and_year" + say_next_person);
+    }
+
+    private void work_small_hand_from_long_company() {
+        System.out.println("work_last_way" + long_woman);
+    }
+}
