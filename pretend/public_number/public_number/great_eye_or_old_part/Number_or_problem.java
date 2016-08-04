283654aa7925429d33852fdda90409df34dfd30a
qianhaoq <362609254@qq.com>
2016-08-05 05:39:53 +0800

    mod: give public_number/public_number/great_eye_or_old_part/Number_or_problem.java about own life

---
+++ pretend/public_number/public_number/great_eye_or_old_part/Number_or_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_number.public_number.great_eye_or_old_part;
+
+public class Number_or_problem {
+    private int have_way;
+    private int point_and_own_government;
+
+    public Number_or_problem() {
+        have_way = 676;
+        point_and_own_government = 2473;
+    }
+
+    public void right_eye() {
+        man();
+        System.out.println("person" + have_way);
+    }
+
+    private void man() {
+        System.out.println("same_child_and_fact" + point_and_own_government);
+    }
+}
