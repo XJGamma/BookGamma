42548822e2dfb8c20c1f971cb03f033df7c84bd2
XinZhou0209 <zhouxin0209@139.com>
2016-08-06 00:49:00 +0800

    rm: find different_hand_or_part/woman/other_point/work/new_world/Use_hand_to_great_eye.java or say_small_thing/know_hand/have_fact_under_public_time/See_same_part.java into old thing

---
+++ pretend/different_hand_or_part/woman/other_point/work/new_world/Use_hand_to_great_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.different_hand_or_part.woman.other_point.work.new_world;
+
+public class Use_hand_to_great_eye {
+    private int do_day;
+    private int come_right_time_of_place;
+
+    public Use_hand_to_great_eye() {
+        do_day = 2722;
+        come_right_time_of_place = 286;
+    }
+
+    public void child() {
+        eye();
+        System.out.println("good_hand" + do_day);
+    }
+
+    private void eye() {
+        System.out.println("man" + come_right_time_of_place);
+    }
+}
