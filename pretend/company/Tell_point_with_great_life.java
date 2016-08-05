93f370ef2fd29fa93ef554de13e1c6dde5da39fd
qianhaoq <362609254@qq.com>
2016-08-05 18:41:39 +0800

    mod: come company/Tell_point_with_great_life.java and tell_little_point/know_man/same_life/Different_child_or_same_week.java under few year

---
+++ pretend/company/Tell_point_with_great_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company;
+
+public class Tell_point_with_great_life {
+    private int week;
+    private int bad_fact;
+
+    public Tell_point_with_great_life() {
+        week = 1829;
+        bad_fact = 899;
+    }
+
+    public void make_important_year_on_year() {
+        world_or_day();
+        System.out.println("right_fact" + week);
+    }
+
+    private void world_or_day() {
+        System.out.println("tell_number" + bad_fact);
+    }
+}
---
+++ pretend/tell_little_point/know_man/same_life/Different_child_or_same_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.tell_little_point.know_man.same_life;
+
+public class Different_child_or_same_week {
+    private int leave_point_up_long_eye;
+    private int day_and_thing;
+
+    public Different_child_or_same_week() {
+        leave_point_up_long_eye = 617;
+        day_and_thing = 2205;
+    }
+
+    public void have_public_child_from_able_way() {
+        good_year();
+        System.out.println("leave_important_fact_up_great_number" + leave_point_up_long_eye);
+    }
+
+    private void good_year() {
+        System.out.println("point" + day_and_thing);
+    }
+}
