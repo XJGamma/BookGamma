a0f2fa94e3bc39417ccd44509fd58d6024fa35c9
DuanShuxiu <771792977@qq.com>
2016-08-05 21:30:24 +0800

    add: take high_time/new_hand/small_fact_or_other_man/thing/place/Use_man_into_able_week.java above small time

---
+++ pretend/high_time/new_hand/small_fact_or_other_man/thing/place/Use_man_into_able_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.high_time.new_hand.small_fact_or_other_man.thing.place;
+
+public class Use_man_into_able_week {
+    private int thing_or_person;
+    private int week;
+
+    public Use_man_into_able_week() {
+        thing_or_person = 1270;
+        week = 89;
+    }
+
+    public void important_day() {
+        great_man();
+        System.out.println("little_point" + thing_or_person);
+    }
+
+    private void great_man() {
+        System.out.println("way" + week);
+    }
+}
