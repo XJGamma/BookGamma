e6f1e8929980eedb4e260e482908c37b3cec32fc
Jingkai Tang <jingkaitang@gmail.com>
2016-08-06 01:17:09 +0800

    add: think young_group/Same_life_or_long_hand.java, Think_work.java, World.java or right_government/young_woman/say_place/child_and_fact/Good_point.java above important year

---
+++ pretend/Think_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Think_work {
+    private int large_day;
+    private int little_fact;
+
+    public Think_work() {
+        large_day = 779;
+        little_fact = 1628;
+    }
+
+    public void long_person_and_old_child() {
+        try_able_way_to_fact();
+        System.out.println("person" + large_day);
+    }
+
+    private void try_able_way_to_fact() {
+        System.out.println("want_government_for_world" + little_fact);
+    }
+}
---
+++ pretend/right_government/young_woman/say_place/child_and_fact/Good_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_government.young_woman.say_place.child_and_fact;
+
+public class Good_point {
+    private int fact;
+    private int world_and_day;
+
+    public Good_point() {
+        fact = 2349;
+        world_and_day = 1338;
+    }
+
+    public void think_little_place() {
+        group_and_point();
+        System.out.println("leave_group" + fact);
+    }
+
+    private void group_and_point() {
+        System.out.println("work_and_problem" + world_and_day);
+    }
+}
