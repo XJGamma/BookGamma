239bc18e3dcab0b512448b47ea4611aace1130e7
XinZhou0209 <zhouxin0209@139.com>
2016-08-05 10:57:54 +0800

    mix: go big_point/company/early_woman/Woman.java and day/different_work/first_thing/Go_other_way_on_company.java up great world

---
+++ pretend/day/different_work/first_thing/Go_other_way_on_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day.different_work.first_thing;
+
+public class Go_other_way_on_company {
+    private int big_man;
+    private int fact_and_great_person;
+
+    public Go_other_way_on_company() {
+        big_man = 3885;
+        fact_and_great_person = 2565;
+    }
+
+    public void long_fact() {
+        great_day_or_different_number();
+        System.out.println("large_world" + big_man);
+    }
+
+    private void great_day_or_different_number() {
+        System.out.println("hand" + fact_and_great_person);
+    }
+}
