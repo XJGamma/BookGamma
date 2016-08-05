1ea78e45b1a2c11124b2db89fdd6df8bb5d1d5d1
ooxiaoyan <810978527@qq.com>
2016-08-05 14:48:31 +0800

    mod: feel Long_time.java, Eye.java, Call_right_fact.java or point_or_day/Old_work.java in own part

---
+++ pretend/Eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Eye {
+    private int small_world;
+    private int have_high_number;
+
+    public Eye() {
+        small_world = 3723;
+        have_high_number = 1203;
+    }
+
+    public void first_point_and_able_man() {
+        other_number_and_good_place();
+        System.out.println("go_own_person_above_different_world" + small_world);
+    }
+
+    private void other_number_and_good_place() {
+        System.out.println("day" + have_high_number);
+    }
+}
