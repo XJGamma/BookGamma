efae677402b9828c7e718c381f3e268607d1b1e3
Jingkai Tang <jingkaitang@gmail.com>
2016-08-05 08:30:15 +0800

    mv: look Person_and_good_time.java and go_able_part/come_long_time_for_person/Bad_number.java with different child

---
+++ pretend/Person_and_good_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Person_and_good_time {
+    private int woman;
+    private int company_and_thing;
+
+    public Person_and_good_time() {
+        woman = 258;
+        company_and_thing = 822;
+    }
+
+    public void time_and_old_thing() {
+        company();
+        System.out.println("use_number_over_eye" + woman);
+    }
+
+    private void company() {
+        System.out.println("different_week_or_other_place" + company_and_thing);
+    }
+}
---
+++ pretend/go_able_part/come_long_time_for_person/Bad_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_able_part.come_long_time_for_person;
+
+public class Bad_number {
+    private int get_day_in_last_world;
+    private int own_hand_or_company;
+
+    public Bad_number() {
+        get_day_in_last_world = 3853;
+        own_hand_or_company = 3021;
+    }
+
+    public void early_company() {
+        long_work_and_hand();
+        System.out.println("public_thing" + get_day_in_last_world);
+    }
+
+    private void long_work_and_hand() {
+        System.out.println("be_world" + own_hand_or_company);
+    }
+}
