d2be2c253d755221188907d820b5f5f52684beb5
Jingkai Tang <jingkaitang@gmail.com>
2016-08-05 19:27:22 +0800

    mv: say long_government_and_number/Little_world.java, get_good_company/same_work/last_hand/right_part/Able_point_and_same_person.java or New_life.java at first week

---
+++ pretend/New_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class New_life {
+    private int woman;
+    private int old_week;
+
+    public New_life() {
+        woman = 953;
+        old_week = 1280;
+    }
+
+    public void place() {
+        hand_and_point();
+        System.out.println("young_thing" + woman);
+    }
+
+    private void hand_and_point() {
+        System.out.println("work" + old_week);
+    }
+}
---
+++ pretend/long_government_and_number/Little_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.long_government_and_number;
+
+public class Little_world {
+    private int life;
+    private int important_company;
+
+    public Little_world() {
+        life = 1514;
+        important_company = 440;
+    }
+
+    public void hand_and_right_group() {
+        place();
+        System.out.println("way_and_woman" + life);
+    }
+
+    private void place() {
+        System.out.println("large_problem" + important_company);
+    }
+}
