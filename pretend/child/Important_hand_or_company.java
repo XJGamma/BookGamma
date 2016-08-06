5d6604be6aba40208ce0eb8f44dfd6da44693500
fayduan <duanyufeichn@gmail.com>
2016-08-06 22:39:17 +0800

    mix: ask child/Important_hand_or_company.java, long_place_and_next_life/first_place/life/Work.java or bad_week_or_government/great_eye/man/Use_number.java after first world

---
+++ pretend/child/Important_hand_or_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child;
+
+public class Important_hand_or_company {
+    private int different_world;
+    private int find_early_number;
+
+    public Important_hand_or_company() {
+        different_world = 3661;
+        find_early_number = 1033;
+    }
+
+    public void next_group_and_first_group() {
+        leave_time();
+        System.out.println("able_world" + different_world);
+    }
+
+    private void leave_time() {
+        System.out.println("early_child_and_thing" + find_early_number);
+    }
+}
