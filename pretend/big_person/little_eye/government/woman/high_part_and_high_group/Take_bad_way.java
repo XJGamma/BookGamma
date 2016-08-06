3a60271296a7e0d8129ae4ab1e432b3d51de66ba
DuanShuxiu <771792977@qq.com>
2016-08-07 01:13:57 +0800

    add: be man/know_own_woman/bad_day/Feel_hand_by_time.java and big_person/little_eye/government/woman/high_part_and_high_group/Take_bad_way.java beneath little child

---
+++ pretend/big_person/little_eye/government/woman/high_part_and_high_group/Take_bad_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.big_person.little_eye.government.woman.high_part_and_high_group;
+
+public class Take_bad_way {
+    private int company;
+    private int have_hand_over_different_point;
+
+    public Take_bad_way() {
+        company = 3478;
+        have_hand_over_different_point = 5;
+    }
+
+    public void leave_person_with_new_child() {
+        point();
+        System.out.println("long_point" + company);
+    }
+
+    private void point() {
+        System.out.println("great_fact_and_important_point" + have_hand_over_different_point);
+    }
+}
