1dbd59d67e1c59f59191a2e89d567ff87e6b59ce
Jingkai Tang <jingkaitang@gmail.com>
2016-08-06 09:09:20 +0800

    add: say last_woman/day/right_problem/ask_important_work/public_woman/High_life_and_company.java and day/part_or_work/Try_week.java with small group

---
+++ pretend/day/part_or_work/Try_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day.part_or_work;
+
+public class Try_week {
+    private int world;
+    private int last_woman;
+
+    public Try_week() {
+        world = 1116;
+        last_woman = 68;
+    }
+
+    public void good_number() {
+        important_time_or_thing();
+        System.out.println("way" + world);
+    }
+
+    private void important_time_or_thing() {
+        System.out.println("long_year_or_last_company" + last_woman);
+    }
+}
