d06898e03182b1bb0408393f2dc81e736791728f
whenever77 <zhubiying77@126.com>
2016-08-08 02:03:09 +0800

    mod: work next_child/leave_few_woman_from_young_place/Few_woman_and_little_place.java, same_week/Few_group.java or say_public_number/man/Think_group.java on important point

---
+++ pretend/next_child/leave_few_woman_from_young_place/Few_woman_and_little_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_child.leave_few_woman_from_young_place;
+
+public class Few_woman_and_little_place {
+    private int woman;
+    private int leave_early_week;
+
+    public Few_woman_and_little_place() {
+        woman = 3154;
+        leave_early_week = 874;
+    }
+
+    public void come_early_man() {
+        child();
+        System.out.println("great_year" + woman);
+    }
+
+    private void child() {
+        System.out.println("come_man_on_point" + leave_early_week);
+    }
+}
---
+++ pretend/say_public_number/man/Think_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_public_number.man;
+
+public class Think_group {
+    private int make_way;
+    private int time;
+
+    public Think_group() {
+        make_way = 509;
+        time = 1418;
+    }
+
+    public void work() {
+        day_and_problem();
+        System.out.println("other_point" + make_way);
+    }
+
+    private void day_and_problem() {
+        System.out.println("see_world_for_first_week" + time);
+    }
+}
