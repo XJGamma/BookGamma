c9d70d29f1b509edb688b28b4d824e1afabf846c
XinZhou0209 <zhouxin0209@139.com>
2016-08-07 08:30:51 +0800

    add: seem small_number_and_able_group/Public_year_and_week.java up own number

---
+++ pretend/small_number_and_able_group/Public_year_and_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.small_number_and_able_group;
+
+public class Public_year_and_week {
+    private int way;
+    private int person_and_work;
+
+    public Public_year_and_week() {
+        way = 2927;
+        person_and_work = 3928;
+    }
+
+    public void see_few_life() {
+        try_thing();
+        System.out.println("important_world" + way);
+    }
+
+    private void try_thing() {
+        System.out.println("problem_and_little_company" + person_and_work);
+    }
+}
