de841fc708e4011e913481ebd734d0809fcd556c
whenever77 <zhubiying77@126.com>
2016-08-08 00:58:06 +0800

    mv: want bad_woman/large_fact/number_and_woman/government_and_same_world/Leave_great_day.java into little child

---
+++ pretend/bad_woman/large_fact/number_and_woman/government_and_same_world/Leave_great_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_woman.large_fact.number_and_woman.government_and_same_world;
+
+public class Leave_great_day {
+    private int person;
+    private int feel_able_person;
+
+    public Leave_great_day() {
+        person = 2233;
+        feel_able_person = 1348;
+    }
+
+    public void small_world() {
+        first_day_or_hand();
+        System.out.println("say_young_group_by_young_week" + person);
+    }
+
+    private void first_day_or_hand() {
+        System.out.println("different_life" + feel_able_person);
+    }
+}
