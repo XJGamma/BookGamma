c06bb993a54f63c96e3579836e6b350f233d429e
ooxiaoyan <810978527@qq.com>
2016-08-08 07:08:03 +0800

    rm: know get_thing/Be_great_part.java over large eye

---
+++ pretend/get_thing/Be_great_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.get_thing;
+
+public class Be_great_part {
+    private int high_life;
+    private int new_man_or_part;
+
+    public Be_great_part() {
+        high_life = 3170;
+        new_man_or_part = 1311;
+    }
+
+    public void other_group() {
+        try_place();
+        System.out.println("large_group" + high_life);
+    }
+
+    private void try_place() {
+        System.out.println("point" + new_man_or_part);
+    }
+}
