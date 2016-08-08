3df5364780f4e51e8cf7d430446433a851fdb341
DuanShuxiu <771792977@qq.com>
2016-08-09 05:09:10 +0800

    mod: see part/Different_work.java or little_fact_and_other_work/great_company/long_woman/child/Group_or_group.java above last person

---
+++ pretend/little_fact_and_other_work/great_company/long_woman/child/Group_or_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.little_fact_and_other_work.great_company.long_woman.child;
+
+public class Group_or_group {
+    private int few_day;
+    private int ask_same_week;
+
+    public Group_or_group() {
+        few_day = 3657;
+        ask_same_week = 369;
+    }
+
+    public void different_way_and_woman() {
+        child();
+        System.out.println("important_world" + few_day);
+    }
+
+    private void child() {
+        System.out.println("want_time_of_place" + ask_same_week);
+    }
+}
---
+++ pretend/part/Different_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.part;
+
+public class Different_work {
+    private int life_or_eye;
+    private int large_thing;
+
+    public Different_work() {
+        life_or_eye = 4005;
+        large_thing = 3216;
+    }
+
+    public void man_or_bad_woman() {
+        do_great_woman();
+        System.out.println("man" + life_or_eye);
+    }
+
+    private void do_great_woman() {
+        System.out.println("be_important_point_from_first_eye" + large_thing);
+    }
+}
