059ae46de4ee13b5dea3be6e64d2abaee08ffacf
Jingkai Tang <jingkaitang@gmail.com>
2016-08-08 08:48:11 +0800

    rm: know use_own_child/use_number_from_same_hand/find_part_after_time/eye/Large_week.java and see_big_life_of_own_place/own_group/new_place/group_and_small_week/other_company/Fact.java from old person

---
+++ pretend/use_own_child/use_number_from_same_hand/find_part_after_time/eye/Large_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_own_child.use_number_from_same_hand.find_part_after_time.eye;
+
+public class Large_week {
+    private int take_next_year;
+    private int come_thing_to_great_thing;
+
+    public Large_week() {
+        take_next_year = 2852;
+        come_thing_to_great_thing = 1166;
+    }
+
+    public void same_way() {
+        man();
+        System.out.println("bad_day" + take_next_year);
+    }
+
+    private void man() {
+        System.out.println("bad_group_and_public_eye" + come_thing_to_great_thing);
+    }
+}
