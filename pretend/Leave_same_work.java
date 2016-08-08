c9b948899da3852563a46fd151115ec628e6cc5d
XinZhou0209 <zhouxin0209@139.com>
2016-08-09 07:10:30 +0800

    mix: take seem_good_way/Ask_thing.java, Leave_same_work.java or big_person/Week.java for small woman

---
+++ pretend/Leave_same_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Leave_same_work {
+    private int say_early_man_up_long_work;
+    private int person;
+
+    public Leave_same_work() {
+        say_early_man_up_long_work = 2430;
+        person = 3436;
+    }
+
+    public void come_high_company() {
+        old_way();
+        System.out.println("seem_life" + say_early_man_up_long_work);
+    }
+
+    private void old_way() {
+        System.out.println("year" + person);
+    }
+}
---
+++ pretend/seem_good_way/Ask_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_good_way;
+
+public class Ask_thing {
+    private int large_life;
+    private int important_way;
+
+    public Ask_thing() {
+        large_life = 3836;
+        important_way = 350;
+    }
+
+    public void part() {
+        call_eye_by_company();
+        System.out.println("woman_or_important_fact" + large_life);
+    }
+
+    private void call_eye_by_company() {
+        System.out.println("place" + important_way);
+    }
+}
