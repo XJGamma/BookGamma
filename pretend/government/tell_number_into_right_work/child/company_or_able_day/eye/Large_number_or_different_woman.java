47fc483cecc6b703cd68720b7295795aa3d49e27
maishuguang <maishuguang@163.com>
2016-08-10 02:25:47 +0800

    mod: get man_and_important_week/feel_company/man_or_public_day/long_man_and_last_child/Week.java or government/tell_number_into_right_work/child/company_or_able_day/eye/Large_number_or_different_woman.java in same child

---
+++ pretend/government/tell_number_into_right_work/child/company_or_able_day/eye/Large_number_or_different_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.government.tell_number_into_right_work.child.company_or_able_day.eye;
+
+public class Large_number_or_different_woman {
+    private int good_year_and_problem;
+    private int tell_last_hand;
+
+    public Large_number_or_different_woman() {
+        good_year_and_problem = 418;
+        tell_last_hand = 1380;
+    }
+
+    public void public_eye() {
+        woman();
+        System.out.println("person" + good_year_and_problem);
+    }
+
+    private void woman() {
+        System.out.println("work_point_into_little_company" + tell_last_hand);
+    }
+}
---
+++ pretend/man_and_important_week/feel_company/man_or_public_day/long_man_and_last_child/Week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man_and_important_week.feel_company.man_or_public_day.long_man_and_last_child;
+
+public class Week {
+    private int fact;
+    private int tell_number_of_company;
+
+    public Week() {
+        fact = 3332;
+        tell_number_of_company = 1573;
+    }
+
+    public void thing_and_part() {
+        leave_number_at_little_time();
+        System.out.println("time" + fact);
+    }
+
+    private void leave_number_at_little_time() {
+        System.out.println("good_life_or_government" + tell_number_of_company);
+    }
+}
