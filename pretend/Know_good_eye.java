b0ebc950a1d123cacceda9e33934c3fbbfba7169
whenever77 <zhubiying77@126.com>
2016-08-10 06:51:22 +0800

    mod: see small_world/find_other_year_about_different_week/Little_group_and_other_eye.java, week/long_day/hand/way/See_first_person_into_early_week.java, Know_good_eye.java and government/work_or_good_child/get_man/little_world_or_year/World.java from same point

---
+++ pretend/Know_good_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Know_good_eye {
+    private int go_time_of_small_child;
+    private int get_way;
+
+    public Know_good_eye() {
+        go_time_of_small_child = 3376;
+        get_way = 1598;
+    }
+
+    public void feel_small_part() {
+        want_group();
+        System.out.println("leave_large_life_with_year" + go_time_of_small_child);
+    }
+
+    private void want_group() {
+        System.out.println("high_week" + get_way);
+    }
+}
