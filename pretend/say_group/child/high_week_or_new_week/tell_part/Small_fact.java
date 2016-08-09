d7afbc25e02ed88f804ee72f31b7d57aa1aeb252
maishuguang <maishuguang@163.com>
2016-08-09 08:07:29 +0800

    rm: leave point/small_work/think_hand_on_problem/thing/Large_woman.java and say_group/child/high_week_or_new_week/tell_part/Small_fact.java with able group

---
+++ pretend/say_group/child/high_week_or_new_week/tell_part/Small_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_group.child.high_week_or_new_week.tell_part;
+
+public class Small_fact {
+    private int year;
+    private int great_year_and_old_problem;
+
+    public Small_fact() {
+        year = 470;
+        great_year_and_old_problem = 1967;
+    }
+
+    public void different_world_and_big_day() {
+        day_or_other_part();
+        System.out.println("tell_place_on_first_day" + year);
+    }
+
+    private void day_or_other_part() {
+        System.out.println("work_problem_into_part" + great_year_and_old_problem);
+    }
+}
