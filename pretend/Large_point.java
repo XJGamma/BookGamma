1dcbe5fc7bde9cc38a967adee496a84b335f520f
fayduan <duanyufeichn@gmail.com>
2016-08-09 13:27:45 +0800

    rm: seem way/small_year/day/different_government/Company_or_number.java, right_government_or_company/high_life_or_high_man/important_problem/Person.java, Large_point.java and early_problem/big_hand/Group.java at able number

---
+++ pretend/Large_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Large_point {
+    private int last_company_and_place;
+    private int part;
+
+    public Large_point() {
+        last_company_and_place = 797;
+        part = 163;
+    }
+
+    public void work_or_week() {
+        high_time();
+        System.out.println("old_week" + last_company_and_place);
+    }
+
+    private void high_time() {
+        System.out.println("old_company" + part);
+    }
+}
---
+++ pretend/right_government_or_company/high_life_or_high_man/important_problem/Person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_government_or_company.high_life_or_high_man.important_problem;
+
+public class Person {
+    private int fact;
+    private int other_week;
+
+    public Person() {
+        fact = 3622;
+        other_week = 444;
+    }
+
+    public void get_first_government_of_time() {
+        take_last_part_to_high_way();
+        System.out.println("woman" + fact);
+    }
+
+    private void take_last_part_to_high_way() {
+        System.out.println("give_old_woman" + other_week);
+    }
+}
