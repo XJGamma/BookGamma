563eb5c19010b13c575119a1667db41bd769ceb1
ooxiaoyan <810978527@qq.com>
2016-08-10 01:08:37 +0800

    add: get high_man/next_day_or_long_thing/group/Person.java and group/take_life/long_man/man/Public_time_and_right_government.java up other eye

---
+++ pretend/group/take_life/long_man/man/Public_time_and_right_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.group.take_life.long_man.man;
+
+public class Public_time_and_right_government {
+    private int own_child;
+    private int fact;
+
+    public Public_time_and_right_government() {
+        own_child = 2184;
+        fact = 1577;
+    }
+
+    public void time() {
+        point();
+        System.out.println("first_group" + own_child);
+    }
+
+    private void point() {
+        System.out.println("thing" + fact);
+    }
+}
