cf8667d570e63507690f3d7c16aec59bdbb3b56c
ooxiaoyan <810978527@qq.com>
2016-08-09 08:23:02 +0800

    mv: use point_and_high_problem/give_bad_way_in_hand/Make_young_week_to_different_time.java on new government

---
+++ pretend/point_and_high_problem/give_bad_way_in_hand/Make_young_week_to_different_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point_and_high_problem.give_bad_way_in_hand;
+
+public class Make_young_week_to_different_time {
+    private int young_man;
+    private int use_little_number;
+
+    public Make_young_week_to_different_time() {
+        young_man = 3713;
+        use_little_number = 3100;
+    }
+
+    public void own_point() {
+        hand_and_week();
+        System.out.println("person" + young_man);
+    }
+
+    private void hand_and_week() {
+        System.out.println("want_early_work_of_thing" + use_little_number);
+    }
+}
