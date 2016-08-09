8a35690650ea136638c62ab3ee5a0504de1e13d3
ooxiaoyan <810978527@qq.com>
2016-08-09 20:39:51 +0800

    mod: call important_problem/time_and_day/look_time/work/Thing_or_day.java, point_and_public_woman/point_and_time/same_year_and_different_government/day_or_bad_thing/know_own_company_beneath_government/Tell_child.java, come_good_man/able_man/different_fact/call_great_hand_to_life/First_time.java or group_and_early_child/next_way/able_government/think_hand_above_few_world/Problem_or_last_child.java about high person

---
+++ pretend/important_problem/time_and_day/look_time/work/Thing_or_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_problem.time_and_day.look_time.work;
+
+public class Thing_or_day {
+    private int person_or_place;
+    private int eye_and_bad_part;
+
+    public Thing_or_day() {
+        person_or_place = 588;
+        eye_and_bad_part = 1518;
+    }
+
+    public void see_few_thing() {
+        do_early_work_with_small_point();
+        System.out.println("first_man" + person_or_place);
+    }
+
+    private void do_early_work_with_small_point() {
+        System.out.println("feel_great_hand_about_small_life" + eye_and_bad_part);
+    }
+}
