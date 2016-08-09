6502f5211ed1387092a396916f89ce9fe59156ba
qianhaoq <362609254@qq.com>
2016-08-09 17:58:01 +0800

    mod: leave Thing_and_eye.java, new_government/seem_day_under_hand/new_government/point/Know_different_day_up_person.java, get_last_day/next_company/Next_time_or_great_work.java or Large_life.java over high life

---
+++ pretend/new_government/seem_day_under_hand/new_government/point/Know_different_day_up_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_government.seem_day_under_hand.new_government.point;
+
+public class Know_different_day_up_person {
+    private int old_week;
+    private int get_new_woman_at_way;
+
+    public Know_different_day_up_person() {
+        old_week = 2961;
+        get_new_woman_at_way = 3887;
+    }
+
+    public void man() {
+        little_man();
+        System.out.println("young_man_or_person" + old_week);
+    }
+
+    private void little_man() {
+        System.out.println("woman" + get_new_woman_at_way);
+    }
+}
