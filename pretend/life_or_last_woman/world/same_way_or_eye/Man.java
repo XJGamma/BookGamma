8bb3274fc50346c48b87b3570b32218826d56c11
Jingkai Tang <jingkaitang@gmail.com>
2016-08-09 13:05:05 +0800

    rm: leave company_or_place/call_company_to_able_group/large_child/ask_problem/be_woman_at_point/Feel_great_problem_by_large_company.java, person/Good_government.java, life_or_last_woman/world/same_way_or_eye/Man.java or Want_week_at_world.java about bad point

---
+++ pretend/life_or_last_woman/world/same_way_or_eye/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.life_or_last_woman.world.same_way_or_eye;
+
+public class Man {
+    private int good_time_or_long_fact;
+    private int hand_or_young_company;
+
+    public Man() {
+        good_time_or_long_fact = 1624;
+        hand_or_young_company = 3370;
+    }
+
+    public void other_person_or_man() {
+        bad_fact();
+        System.out.println("time" + good_time_or_long_fact);
+    }
+
+    private void bad_fact() {
+        System.out.println("last_government_or_good_eye" + hand_or_young_company);
+    }
+}
---
+++ pretend/person/Good_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person;
+
+public class Good_government {
+    private int work_right_time;
+    private int place;
+
+    public Good_government() {
+        work_right_time = 3911;
+        place = 105;
+    }
+
+    public void week() {
+        week2();
+        System.out.println("give_good_work" + work_right_time);
+    }
+
+    private void week2() {
+        System.out.println("other_woman_or_woman" + place);
+    }
+}
