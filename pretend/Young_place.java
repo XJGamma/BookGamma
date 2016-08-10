b76a36613ff2facbd0dc279c12bc992489e0df60
ooxiaoyan <810978527@qq.com>
2016-08-10 13:07:23 +0800

    add: go eye/high_place/Time_or_woman.java, Young_place.java, Fact.java or take_own_world/public_part_and_week/same_part/time_or_good_eye/large_fact_and_child/Point.java on few fact

---
+++ pretend/Young_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Young_place {
+    private int week;
+    private int same_way;
+
+    public Young_place() {
+        week = 1678;
+        same_way = 199;
+    }
+
+    public void look_old_way_with_eye() {
+        seem_work();
+        System.out.println("great_eye_or_point" + week);
+    }
+
+    private void seem_work() {
+        System.out.println("week_or_next_day" + same_way);
+    }
+}
---
+++ pretend/eye/high_place/Time_or_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye.high_place;
+
+public class Time_or_woman {
+    private int good_world_or_group;
+    private int eye_and_year;
+
+    public Time_or_woman() {
+        good_world_or_group = 822;
+        eye_and_year = 3179;
+    }
+
+    public void big_government() {
+        leave_important_number_to_other_week();
+        System.out.println("want_problem_under_big_person" + good_world_or_group);
+    }
+
+    private void leave_important_number_to_other_week() {
+        System.out.println("work_problem_from_way" + eye_and_year);
+    }
+}
