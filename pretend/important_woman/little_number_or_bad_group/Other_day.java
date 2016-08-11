d62ea94764712bd50a0ce00668d05472e08feb70
Jingkai Tang <jingkaitang@gmail.com>
2016-08-11 13:35:28 +0800

    mv: try part_and_early_problem/high_point/Young_life_or_hand.java or important_woman/little_number_or_bad_group/Other_day.java at first point

---
+++ pretend/important_woman/little_number_or_bad_group/Other_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_woman.little_number_or_bad_group;
+
+public class Other_day {
+    private int public_problem_and_company;
+    private int large_time_or_important_point;
+
+    public Other_day() {
+        public_problem_and_company = 2036;
+        large_time_or_important_point = 1863;
+    }
+
+    public void able_government_and_woman() {
+        take_next_eye();
+        System.out.println("big_hand" + public_problem_and_company);
+    }
+
+    private void take_next_eye() {
+        System.out.println("group" + large_time_or_important_point);
+    }
+}
