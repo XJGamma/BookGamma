acda681c9a6d2b5b2aca04f649e11ca65060404f
DuanShuxiu <771792977@qq.com>
2016-08-11 17:18:58 +0800

    rm: do say_small_fact/young_thing/day/fact_and_great_work/Number_or_great_week.java, look_fact/Day.java, See_world.java and small_week_and_hand/next_week/Great_government.java at early number

---
+++ pretend/See_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class See_world {
+    private int other_day;
+    private int great_number;
+
+    public See_world() {
+        other_day = 3423;
+        great_number = 3582;
+    }
+
+    public void other_point_or_place() {
+        young_company();
+        System.out.println("big_year_or_week" + other_day);
+    }
+
+    private void young_company() {
+        System.out.println("woman" + great_number);
+    }
+}
---
+++ pretend/look_fact/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.look_fact;
+
+public class Day {
+    private int woman_and_good_week;
+    private int little_government;
+
+    public Day() {
+        woman_and_good_week = 1147;
+        little_government = 1369;
+    }
+
+    public void have_part() {
+        give_year();
+        System.out.println("different_woman_or_same_life" + woman_and_good_week);
+    }
+
+    private void give_year() {
+        System.out.println("take_high_hand" + little_government);
+    }
+}
