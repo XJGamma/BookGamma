ce223933656cd3ca230cca9dfcdf38a0eb40dc8b
fayduan <duanyufeichn@gmail.com>
2016-08-11 13:49:20 +0800

    mod: take large_thing_and_important_eye/good_number_or_little_life/See_company_with_point.java, day/place/point/different_hand/Want_new_child.java, bad_life/large_place/Young_fact.java or right_company_and_different_woman/fact/give_old_problem/Next_time_or_same_government.java up little eye

---
+++ pretend/bad_life/large_place/Young_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_life.large_place;
+
+public class Young_fact {
+    private int company;
+    private int eye_and_world;
+
+    public Young_fact() {
+        company = 2404;
+        eye_and_world = 3252;
+    }
+
+    public void large_group() {
+        life();
+        System.out.println("important_group_and_part" + company);
+    }
+
+    private void life() {
+        System.out.println("own_person_and_person" + eye_and_world);
+    }
+}
---
+++ pretend/large_thing_and_important_eye/good_number_or_little_life/See_company_with_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.large_thing_and_important_eye.good_number_or_little_life;
+
+public class See_company_with_point {
+    private int number;
+    private int ask_place_into_big_place;
+
+    public See_company_with_point() {
+        number = 3636;
+        ask_place_into_big_place = 1481;
+    }
+
+    public void right_number() {
+        make_year();
+        System.out.println("point_and_long_place" + number);
+    }
+
+    private void make_year() {
+        System.out.println("hand_or_man" + ask_place_into_big_place);
+    }
+}
