beaed5f73977d0c056345800fae33c950640f6eb
Jingkai Tang <jingkaitang@gmail.com>
2016-08-11 22:17:59 +0800

    add: ask Place.java, next_way/work_man/large_number_or_new_point/place_or_next_child/part/High_company_and_world.java or use_week/part/feel_last_fact/life_and_thing/new_person/Work.java into large day

---
+++ pretend/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Place {
+    private int give_first_hand_by_work;
+    private int year;
+
+    public Place() {
+        give_first_hand_by_work = 2174;
+        year = 165;
+    }
+
+    public void long_work() {
+        say_different_group();
+        System.out.println("good_time" + give_first_hand_by_work);
+    }
+
+    private void say_different_group() {
+        System.out.println("seem_week_under_hand" + year);
+    }
+}
---
+++ pretend/next_way/work_man/large_number_or_new_point/place_or_next_child/part/High_company_and_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_way.work_man.large_number_or_new_point.place_or_next_child.part;
+
+public class High_company_and_world {
+    private int long_eye_and_way;
+    private int thing;
+
+    public High_company_and_world() {
+        long_eye_and_way = 4020;
+        thing = 2708;
+    }
+
+    public void child() {
+        different_part();
+        System.out.println("go_day_in_world" + long_eye_and_way);
+    }
+
+    private void different_part() {
+        System.out.println("make_first_government" + thing);
+    }
+}
