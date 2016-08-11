89757d18c286bbcce588e2e172fcc0baabf12e03
XinZhou0209 <zhouxin0209@139.com>
2016-08-11 16:16:21 +0800

    mod: leave point_or_little_person/Large_time.java or Work_and_few_way.java under right person

---
+++ pretend/Work_and_few_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Work_and_few_way {
+    private int thing;
+    private int look_way_of_large_child;
+
+    public Work_and_few_way() {
+        thing = 2695;
+        look_way_of_large_child = 3489;
+    }
+
+    public void number() {
+        same_woman_or_bad_life();
+        System.out.println("take_last_person" + thing);
+    }
+
+    private void same_woman_or_bad_life() {
+        System.out.println("year" + look_way_of_large_child);
+    }
+}
