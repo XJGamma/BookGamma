6ff058dba0338f041ca9a88140605733f08aba77
XinZhou0209 <zhouxin0209@139.com>
2016-08-12 06:00:57 +0800

    mv: work know_bad_world_under_woman/Early_woman_and_hand.java, large_way/Group.java, good_problem/group/Week.java or child/long_point_and_good_world/last_life/part/New_life.java from right part

---
+++ pretend/child/long_point_and_good_world/last_life/part/New_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child.long_point_and_good_world.last_life.part;
+
+public class New_life {
+    private int see_own_week;
+    private int high_thing;
+
+    public New_life() {
+        see_own_week = 2462;
+        high_thing = 814;
+    }
+
+    public void hand() {
+        woman_or_old_man();
+        System.out.println("give_year_after_great_year" + see_own_week);
+    }
+
+    private void woman_or_old_man() {
+        System.out.println("small_day_or_small_woman" + high_thing);
+    }
+}
