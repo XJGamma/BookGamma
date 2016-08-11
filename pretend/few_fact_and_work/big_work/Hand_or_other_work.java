ed6a7344b6ac1e2dfd400e0784ae3ee21855e238
ooxiaoyan <810978527@qq.com>
2016-08-12 03:40:11 +0800

    mv: use go_day/old_world/Woman_or_work.java or few_fact_and_work/big_work/Hand_or_other_work.java in few work

---
+++ pretend/few_fact_and_work/big_work/Hand_or_other_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_fact_and_work.big_work;
+
+public class Hand_or_other_work {
+    private int hand_or_little_day;
+    private int part_and_important_point;
+
+    public Hand_or_other_work() {
+        hand_or_little_day = 3749;
+        part_and_important_point = 2997;
+    }
+
+    public void have_young_time() {
+        first_work();
+        System.out.println("own_year" + hand_or_little_day);
+    }
+
+    private void first_work() {
+        System.out.println("think_high_eye" + part_and_important_point);
+    }
+}
---
+++ pretend/go_day/old_world/Woman_or_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.go_day.old_world;
+
+public class Woman_or_work {
+    private int woman;
+    private int little_point_and_point;
+
+    public Woman_or_work() {
+        woman = 3718;
+        little_point_and_point = 2361;
+    }
+
+    public void high_point() {
+        time();
+        System.out.println("number_and_eye" + woman);
+    }
+
+    private void time() {
+        System.out.println("have_day" + little_point_and_point);
+    }
+}
