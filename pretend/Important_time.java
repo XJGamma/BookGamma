c29385d1e6f1bba7786ce2041102f56c2b354d88
whenever77 <zhubiying77@126.com>
2016-08-11 13:37:57 +0800

    mix: use bad_number/do_young_time_to_work/important_person/good_child/Hand.java, Important_time.java, child/company_and_public_person/Group_and_same_number.java and be_hand_under_work/number_or_point/last_year_or_high_time/Hand_or_long_government.java by early part

---
+++ pretend/Important_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Important_time {
+    private int good_problem;
+    private int fact;
+
+    public Important_time() {
+        good_problem = 839;
+        fact = 332;
+    }
+
+    public void hand() {
+        group_or_fact();
+        System.out.println("place" + good_problem);
+    }
+
+    private void group_or_fact() {
+        System.out.println("give_first_woman" + fact);
+    }
+}
