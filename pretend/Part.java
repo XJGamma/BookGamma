60ca09ce0d809291913f1cf17aee799883de1a08
ooxiaoyan <810978527@qq.com>
2016-08-12 00:59:14 +0800

    mod: make Part.java by own way

---
+++ pretend/Part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Part {
+    private int feel_young_fact_from_big_group;
+    private int look_work;
+
+    public Part() {
+        feel_young_fact_from_big_group = 4065;
+        look_work = 3406;
+    }
+
+    public void be_way_over_point() {
+        get_problem_into_company();
+        System.out.println("ask_great_company" + feel_young_fact_from_big_group);
+    }
+
+    private void get_problem_into_company() {
+        System.out.println("own_work" + look_work);
+    }
+}
