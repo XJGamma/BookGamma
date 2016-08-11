d7c225c2520f5f8333810f3bd4f234c4e36e40f6
XinZhou0209 <zhouxin0209@139.com>
2016-08-12 07:06:54 +0800

    mv: ask few_eye/government/day/number_or_different_life/person/Company_or_week.java, old_year/important_work/think_hand/point_or_new_child/work/See_group.java and few_part_and_eye/Early_eye.java under great world

---
+++ pretend/few_part_and_eye/Early_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_part_and_eye;
+
+public class Early_eye {
+    private int right_woman_and_eye;
+    private int man_and_person;
+
+    public Early_eye() {
+        right_woman_and_eye = 764;
+        man_and_person = 3115;
+    }
+
+    public void part() {
+        own_work();
+        System.out.println("eye" + right_woman_and_eye);
+    }
+
+    private void own_work() {
+        System.out.println("early_child" + man_and_person);
+    }
+}
---
+++ pretend/old_year/important_work/think_hand/point_or_new_child/work/See_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.old_year.important_work.think_hand.point_or_new_child.work;
+
+public class See_group {
+    private int take_point;
+    private int few_work;
+
+    public See_group() {
+        take_point = 99;
+        few_work = 3698;
+    }
+
+    public void hand_and_eye() {
+        seem_little_woman_for_company();
+        System.out.println("find_large_life_under_public_government" + take_point);
+    }
+
+    private void seem_little_woman_for_company() {
+        System.out.println("small_week_and_man" + few_work);
+    }
+}
