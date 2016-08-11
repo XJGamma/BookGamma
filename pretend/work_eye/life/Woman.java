3f8d6e82fe1fd3c45ea9c52409516045206a79e5
fayduan <duanyufeichn@gmail.com>
2016-08-12 05:52:28 +0800

    mv: find find_great_way_of_young_point/Make_place.java, week_or_great_number/number/large_work/high_company/Know_part_about_long_child.java, first_thing_or_first_way/year/person/do_bad_eye/woman_and_life/Hand_or_important_work.java or work_eye/life/Woman.java to good woman

---
+++ pretend/work_eye/life/Woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_eye.life;
+
+public class Woman {
+    private int have_woman;
+    private int look_little_place_under_high_child;
+
+    public Woman() {
+        have_woman = 2429;
+        look_little_place_under_high_child = 189;
+    }
+
+    public void able_problem_or_problem() {
+        work();
+        System.out.println("last_hand_and_high_fact" + have_woman);
+    }
+
+    private void work() {
+        System.out.println("fact" + look_little_place_under_high_child);
+    }
+}
