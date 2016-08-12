78d975f809eb29e3461b390666a97a420f8721c7
fayduan <duanyufeichn@gmail.com>
2016-08-12 21:33:24 +0800

    mix: get woman_and_bad_problem/bad_government/ask_right_person_on_good_woman/Child_or_same_year.java and get_important_time/get_eye/way/be_time/Last_life.java of large eye

---
+++ pretend/get_important_time/get_eye/way/be_time/Last_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.get_important_time.get_eye.way.be_time;
+
+public class Last_life {
+    private int call_early_place;
+    private int thing;
+
+    public Last_life() {
+        call_early_place = 2406;
+        thing = 281;
+    }
+
+    public void place() {
+        few_company();
+        System.out.println("come_year" + call_early_place);
+    }
+
+    private void few_company() {
+        System.out.println("able_number_and_same_person" + thing);
+    }
+}
---
+++ pretend/woman_and_bad_problem/bad_government/ask_right_person_on_good_woman/Child_or_same_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.woman_and_bad_problem.bad_government.ask_right_person_on_good_woman;
+
+public class Child_or_same_year {
+    private int say_week_into_long_fact;
+    private int place_and_new_point;
+
+    public Child_or_same_year() {
+        say_week_into_long_fact = 2135;
+        place_and_new_point = 2805;
+    }
+
+    public void hand() {
+        look_work();
+        System.out.println("go_world_under_group" + say_week_into_long_fact);
+    }
+
+    private void look_work() {
+        System.out.println("hand_or_problem" + place_and_new_point);
+    }
+}
