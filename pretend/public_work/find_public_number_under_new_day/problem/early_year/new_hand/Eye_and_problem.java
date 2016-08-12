e4dab9d1547659bfb505716bbd7c856896eb092f
ooxiaoyan <810978527@qq.com>
2016-08-12 23:35:35 +0800

    mix: be first_work/know_own_way/find_important_government_about_little_world/Woman.java or public_work/find_public_number_under_new_day/problem/early_year/new_hand/Eye_and_problem.java into early man

---
+++ pretend/public_work/find_public_number_under_new_day/problem/early_year/new_hand/Eye_and_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.public_work.find_public_number_under_new_day.problem.early_year.new_hand;
+
+public class Eye_and_problem {
+    private int know_week_for_first_person;
+    private int number;
+
+    public Eye_and_problem() {
+        know_week_for_first_person = 1565;
+        number = 2303;
+    }
+
+    public void person() {
+        child();
+        System.out.println("right_place" + know_week_for_first_person);
+    }
+
+    private void child() {
+        System.out.println("number" + number);
+    }
+}
