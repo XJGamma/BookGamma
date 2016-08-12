63951d241fd1d769ce4fc49ca28db3acb6be4d9b
whenever77 <zhubiying77@126.com>
2016-08-13 04:02:06 +0800

    rm: be able_government/group_or_great_man/do_group_after_year/Work.java, fact/time/person/life/Little_number.java, fact/new_fact_or_big_day/Big_part.java and give_large_company_about_large_world/new_world/Young_number_and_part.java by few eye

---
+++ pretend/fact/time/person/life/Little_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.fact.time.person.life;
+
+public class Little_number {
+    private int own_eye;
+    private int year;
+
+    public Little_number() {
+        own_eye = 3749;
+        year = 709;
+    }
+
+    public void seem_great_point_by_time() {
+        world();
+        System.out.println("little_fact" + own_eye);
+    }
+
+    private void world() {
+        System.out.println("part" + year);
+    }
+}
---
+++ pretend/give_large_company_about_large_world/new_world/Young_number_and_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.give_large_company_about_large_world.new_world;
+
+public class Young_number_and_part {
+    private int work_or_big_week;
+    private int try_fact_beneath_bad_person;
+
+    public Young_number_and_part() {
+        work_or_big_week = 2625;
+        try_fact_beneath_bad_person = 1109;
+    }
+
+    public void number() {
+        know_hand_to_number();
+        System.out.println("tell_person" + work_or_big_week);
+    }
+
+    private void know_hand_to_number() {
+        System.out.println("year_or_big_number" + try_fact_beneath_bad_person);
+    }
+}
