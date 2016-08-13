5b1c9bac260b84b79101719e0ef61389b1356772
whenever77 <zhubiying77@126.com>
2016-08-13 17:30:18 +0800

    rm: seem use_way_after_problem/high_person/try_different_point/Other_point_or_fact.java and important_world/government/Important_life_or_good_week.java in able place

---
+++ pretend/important_world/government/Important_life_or_good_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_world.government;
+
+public class Important_life_or_good_week {
+    private int different_day;
+    private int company;
+
+    public Important_life_or_good_week() {
+        different_day = 2391;
+        company = 2180;
+    }
+
+    public void year_and_different_week() {
+        child();
+        System.out.println("week" + different_day);
+    }
+
+    private void child() {
+        System.out.println("go_large_government_over_important_place" + company);
+    }
+}
