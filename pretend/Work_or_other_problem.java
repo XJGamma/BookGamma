e701e252ab9bca4555aa5ad5d32bf232119d923b
DuanShuxiu <771792977@qq.com>
2016-08-13 12:54:23 +0800

    rm: want Next_thing.java or Work_or_other_problem.java on old child

---
+++ pretend/Work_or_other_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Work_or_other_problem {
+    private int next_day_and_few_hand;
+    private int own_man;
+
+    public Work_or_other_problem() {
+        next_day_and_few_hand = 3161;
+        own_man = 1543;
+    }
+
+    public void bad_woman_and_same_world() {
+        place();
+        System.out.println("tell_child" + next_day_and_few_hand);
+    }
+
+    private void place() {
+        System.out.println("new_number_and_small_world" + own_man);
+    }
+}
