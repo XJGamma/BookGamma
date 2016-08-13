dcb6516722661c0bf9c93e3e1e43c6099304620d
whenever77 <zhubiying77@126.com>
2016-08-13 09:21:23 +0800

    add: leave thing/good_work/Same_day_or_public_life.java after few week

---
+++ pretend/thing/good_work/Same_day_or_public_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.thing.good_work;
+
+public class Same_day_or_public_life {
+    private int have_fact_up_day;
+    private int fact;
+
+    public Same_day_or_public_life() {
+        have_fact_up_day = 2195;
+        fact = 3067;
+    }
+
+    public void right_point() {
+        call_good_hand();
+        System.out.println("find_last_part" + have_fact_up_day);
+    }
+
+    private void call_good_hand() {
+        System.out.println("man" + fact);
+    }
+}
