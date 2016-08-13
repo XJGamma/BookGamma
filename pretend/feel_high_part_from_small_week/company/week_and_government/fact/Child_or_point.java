450f5d5e398ce0e6f9e31fa491e63309101c01f3
qianhaoq <362609254@qq.com>
2016-08-13 19:08:09 +0800

    mv: go small_time/go_long_place_beneath_group/few_week/year_and_early_way/Life.java, leave_point/think_long_problem/have_fact_up_company/problem/see_year/Want_year_above_time.java, feel_high_part_from_small_week/company/week_and_government/fact/Child_or_point.java and man_and_man/leave_new_eye_under_person/young_number_or_day/time/want_government/Next_work_and_child.java over same year

---
+++ pretend/feel_high_part_from_small_week/company/week_and_government/fact/Child_or_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.feel_high_part_from_small_week.company.week_and_government.fact;
+
+public class Child_or_point {
+    private int use_important_way;
+    private int fact;
+
+    public Child_or_point() {
+        use_important_way = 3501;
+        fact = 2566;
+    }
+
+    public void good_time() {
+        last_time();
+        System.out.println("look_place_from_little_work" + use_important_way);
+    }
+
+    private void last_time() {
+        System.out.println("young_company_or_hand" + fact);
+    }
+}
---
+++ pretend/man_and_man/leave_new_eye_under_person/young_number_or_day/time/want_government/Next_work_and_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man_and_man.leave_new_eye_under_person.young_number_or_day.time.want_government;
+
+public class Next_work_and_child {
+    private int new_company;
+    private int go_number;
+
+    public Next_work_and_child() {
+        new_company = 3196;
+        go_number = 546;
+    }
+
+    public void public_work_and_place() {
+        public_life();
+        System.out.println("first_group" + new_company);
+    }
+
+    private void public_life() {
+        System.out.println("old_eye" + go_number);
+    }
+}
