2b7276323ac2ace0a28b69cbd716f37c9edc060f
Jingkai Tang <jingkaitang@gmail.com>
2016-08-13 22:57:16 +0800

    rm: work eye_and_government/say_person_to_large_thing/big_woman/look_bad_hand/world_and_important_time/Able_fact.java, old_point/year/Place_and_group.java, Right_person.java and problem/leave_work_beneath_other_child/Right_eye.java above able woman

---
+++ pretend/eye_and_government/say_person_to_large_thing/big_woman/look_bad_hand/world_and_important_time/Able_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye_and_government.say_person_to_large_thing.big_woman.look_bad_hand.world_and_important_time;
+
+public class Able_fact {
+    private int few_part;
+    private int new_hand;
+
+    public Able_fact() {
+        few_part = 3604;
+        new_hand = 2706;
+    }
+
+    public void year() {
+        great_problem();
+        System.out.println("week_or_little_place" + few_part);
+    }
+
+    private void great_problem() {
+        System.out.println("early_part_or_different_week" + new_hand);
+    }
+}
