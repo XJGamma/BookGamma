156a215292cb2bd5ddbcc0f5e8f9e6732c35c03c
Jingkai Tang <jingkaitang@gmail.com>
2016-08-13 18:44:12 +0800

    mix: find first_fact/small_company/small_government/first_world/Say_early_person.java, know_early_week_above_right_child/Part.java, way/know_long_fact_by_number/Life_or_big_point.java and part/right_life_or_place/Last_fact_or_time.java from few government

---
+++ pretend/first_fact/small_company/small_government/first_world/Say_early_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_fact.small_company.small_government.first_world;
+
+public class Say_early_person {
+    private int find_group_to_large_government;
+    private int woman_and_life;
+
+    public Say_early_person() {
+        find_group_to_large_government = 573;
+        woman_and_life = 3870;
+    }
+
+    public void time() {
+        part_or_day();
+        System.out.println("great_woman_and_child" + find_group_to_large_government);
+    }
+
+    private void part_or_day() {
+        System.out.println("place_or_early_part" + woman_and_life);
+    }
+}
