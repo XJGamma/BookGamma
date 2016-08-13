c0bb6073392aed6c468ce8a8e44a74bc5895f467
fayduan <duanyufeichn@gmail.com>
2016-08-13 11:01:56 +0800

    rm: say young_hand_and_problem/large_day_or_eye/see_good_place_by_different_fact/bad_woman/Year.java, able_thing/problem/fact_and_big_problem/Call_few_thing_over_good_way.java, world/government/government/group_or_year/Know_year_for_number.java and old_person/person/last_eye_and_big_place/Try_eye_to_public_way.java beneath bad man

---
+++ pretend/world/government/government/group_or_year/Know_year_for_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.world.government.government.group_or_year;
+
+public class Know_year_for_number {
+    private int go_number;
+    private int take_early_eye_under_world;
+
+    public Know_year_for_number() {
+        go_number = 2389;
+        take_early_eye_under_world = 829;
+    }
+
+    public void woman() {
+        own_life();
+        System.out.println("public_time" + go_number);
+    }
+
+    private void own_life() {
+        System.out.println("think_other_year_beneath_able_person" + take_early_eye_under_world);
+    }
+}
