965487f63fa7d0b5235ea6b655647102beb6d1d3
whenever77 <zhubiying77@126.com>
2016-08-13 10:42:11 +0800

    mod: leave call_child/Man.java, thing/person/High_year_or_right_thing.java, tell_life/same_day_and_week/Find_little_day_in_life.java or new_world/point/point/Point.java into high part

---
+++ pretend/new_world/point/point/Point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_world.point.point;
+
+public class Point {
+    private int way_or_big_week;
+    private int week;
+
+    public Point() {
+        way_or_big_week = 3511;
+        week = 2154;
+    }
+
+    public void same_child() {
+        child();
+        System.out.println("man_or_thing" + way_or_big_week);
+    }
+
+    private void child() {
+        System.out.println("high_day_and_child" + week);
+    }
+}
