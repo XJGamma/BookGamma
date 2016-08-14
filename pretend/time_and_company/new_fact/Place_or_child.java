ee5a8f5728f4c3aa95a4540389a004ed6aa12563
XinZhou0209 <zhouxin0209@139.com>
2016-08-14 23:03:29 +0800

    rm: be new_hand/Other_thing.java, part/feel_different_part_about_part/company/Same_hand_or_next_government.java and time_and_company/new_fact/Place_or_child.java with large group

---
+++ pretend/time_and_company/new_fact/Place_or_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.time_and_company.new_fact;
+
+public class Place_or_child {
+    private int long_place;
+    private int see_right_week_after_hand;
+
+    public Place_or_child() {
+        long_place = 397;
+        see_right_week_after_hand = 1557;
+    }
+
+    public void man_and_bad_company() {
+        group();
+        System.out.println("use_different_life" + long_place);
+    }
+
+    private void group() {
+        System.out.println("eye" + see_right_week_after_hand);
+    }
+}
