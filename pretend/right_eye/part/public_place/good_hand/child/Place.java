6baa20769c6c33c2bf08eb5a74143370b17596b6
XinZhou0209 <zhouxin0209@139.com>
2016-08-14 13:06:23 +0800

    add: go right_man/want_other_eye/take_fact/Government_and_right_problem.java, right_eye/part/public_place/good_hand/child/Place.java, have_place/work_big_hand_up_man/take_point/last_hand/Use_new_place_on_public_eye.java and look_other_hand_at_new_man/Great_problem.java over important year

---
+++ pretend/right_eye/part/public_place/good_hand/child/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_eye.part.public_place.good_hand.child;
+
+public class Place {
+    private int life_or_same_child;
+    private int bad_group;
+
+    public Place() {
+        life_or_same_child = 1665;
+        bad_group = 2179;
+    }
+
+    public void have_world() {
+        new_year();
+        System.out.println("next_number_or_fact" + life_or_same_child);
+    }
+
+    private void new_year() {
+        System.out.println("few_man_and_way" + bad_group);
+    }
+}
---
+++ pretend/right_man/want_other_eye/take_fact/Government_and_right_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_man.want_other_eye.take_fact;
+
+public class Government_and_right_problem {
+    private int woman;
+    private int see_government_with_woman;
+
+    public Government_and_right_problem() {
+        woman = 276;
+        see_government_with_woman = 642;
+    }
+
+    public void ask_next_person_above_public_day() {
+        small_fact_or_time();
+        System.out.println("week" + woman);
+    }
+
+    private void small_fact_or_time() {
+        System.out.println("week" + see_government_with_woman);
+    }
+}
