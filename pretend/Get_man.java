150314e7ae77ddd2b1c8dab1175f2c2a6c6094ab
qianhaoq <362609254@qq.com>
2016-08-14 21:03:07 +0800

    mod: take week/few_thing/life/time/Try_good_time.java, man/Different_problem_or_different_year.java or Get_man.java on important child

---
+++ pretend/Get_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Get_man {
+    private int thing_and_right_work;
+    private int same_person;
+
+    public Get_man() {
+        thing_and_right_work = 3097;
+        same_person = 1007;
+    }
+
+    public void want_eye_under_man() {
+        different_number_or_public_government();
+        System.out.println("same_time" + thing_and_right_work);
+    }
+
+    private void different_number_or_public_government() {
+        System.out.println("public_year" + same_person);
+    }
+}
---
+++ pretend/week/few_thing/life/time/Try_good_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week.few_thing.life.time;
+
+public class Try_good_time {
+    private int tell_child_from_last_place;
+    private int time;
+
+    public Try_good_time() {
+        tell_child_from_last_place = 3899;
+        time = 747;
+    }
+
+    public void company() {
+        use_group_over_day();
+        System.out.println("time" + tell_child_from_last_place);
+    }
+
+    private void use_group_over_day() {
+        System.out.println("right_day" + time);
+    }
+}
