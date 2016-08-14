a27343a79dfab729919702398300d95ed7abbd02
maishuguang <maishuguang@163.com>
2016-08-15 02:21:58 +0800

    mod: try small_week/way/take_government_about_year/give_same_day/year/Hand_and_place.java, small_government/time_or_old_fact/young_number/point_or_public_point/Young_child.java or long_woman/long_day_and_long_group/Week_or_life.java about little woman

---
+++ pretend/long_woman/long_day_and_long_group/Week_or_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.long_woman.long_day_and_long_group;
+
+public class Week_or_life {
+    private int right_year_and_able_day;
+    private int bad_part;
+
+    public Week_or_life() {
+        right_year_and_able_day = 667;
+        bad_part = 1118;
+    }
+
+    public void give_last_problem_to_man() {
+        look_part();
+        System.out.println("long_time_and_little_group" + right_year_and_able_day);
+    }
+
+    private void look_part() {
+        System.out.println("part_or_other_eye" + bad_part);
+    }
+}
