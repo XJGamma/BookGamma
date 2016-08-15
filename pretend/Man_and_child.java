eb4bcdef402a3d12d67169f08953552af9623ad5
DuanShuxiu <771792977@qq.com>
2016-08-16 05:33:45 +0800

    add: tell Man_and_child.java, point/ask_child_after_high_company/great_thing/work/problem_and_way/Year.java and last_thing/Look_fact.java about big fact

---
+++ pretend/Man_and_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Man_and_child {
+    private int own_fact;
+    private int week;
+
+    public Man_and_child() {
+        own_fact = 3959;
+        week = 847;
+    }
+
+    public void world_and_world() {
+        be_early_week();
+        System.out.println("see_government_for_long_person" + own_fact);
+    }
+
+    private void be_early_week() {
+        System.out.println("life_or_number" + week);
+    }
+}
---
+++ pretend/point/ask_child_after_high_company/great_thing/work/problem_and_way/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point.ask_child_after_high_company.great_thing.work.problem_and_way;
+
+public class Year {
+    private int group;
+    private int same_hand;
+
+    public Year() {
+        group = 4057;
+        same_hand = 893;
+    }
+
+    public void see_public_company() {
+        part_and_work();
+        System.out.println("last_time" + group);
+    }
+
+    private void part_and_work() {
+        System.out.println("say_part_beneath_old_government" + same_hand);
+    }
+}
