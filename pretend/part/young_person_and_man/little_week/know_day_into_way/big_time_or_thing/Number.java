8f7a59dbec96468d88bc6f89659c77b809c21f35
ooxiaoyan <810978527@qq.com>
2016-08-15 10:50:06 +0800

    mix: use old_thing_and_right_week/child/long_life/Person.java or part/young_person_and_man/little_week/know_day_into_way/big_time_or_thing/Number.java of new group

---
+++ pretend/part/young_person_and_man/little_week/know_day_into_way/big_time_or_thing/Number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.part.young_person_and_man.little_week.know_day_into_way.big_time_or_thing;
+
+public class Number {
+    private int woman;
+    private int hand;
+
+    public Number() {
+        woman = 3767;
+        hand = 1682;
+    }
+
+    public void woman() {
+        say_important_government_by_way();
+        System.out.println("early_government" + woman);
+    }
+
+    private void say_important_government_by_way() {
+        System.out.println("day" + hand);
+    }
+}
