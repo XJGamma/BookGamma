d285e4244f7e8773dfb6dcca748f479c966632a9
XinZhou0209 <zhouxin0209@139.com>
2016-08-15 10:38:26 +0800

    mix: know point/work_problem_with_government/early_person/company_or_public_day/Part_and_able_place.java, Long_number_and_year.java, long_hand/Have_high_number.java and child/small_man/Give_good_group_by_big_world.java to next eye

---
+++ pretend/point/work_problem_with_government/early_person/company_or_public_day/Part_and_able_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point.work_problem_with_government.early_person.company_or_public_day;
+
+public class Part_and_able_place {
+    private int time_and_long_week;
+    private int know_company;
+
+    public Part_and_able_place() {
+        time_and_long_week = 3072;
+        know_company = 552;
+    }
+
+    public void day() {
+        say_next_life();
+        System.out.println("leave_important_time_for_last_work" + time_and_long_week);
+    }
+
+    private void say_next_life() {
+        System.out.println("first_number" + know_company);
+    }
+}
