09db9e60a507de00fb3712467bbc3f4f441123b5
ooxiaoyan <810978527@qq.com>
2016-08-15 21:40:08 +0800

    rm: give few_week_and_day/world/go_great_year_by_person/World_and_number.java with young man

---
+++ pretend/few_week_and_day/world/go_great_year_by_person/World_and_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.few_week_and_day.world.go_great_year_by_person;
+
+public class World_and_number {
+    private int leave_last_week_of_large_way;
+    private int work_new_week;
+
+    public World_and_number() {
+        leave_last_week_of_large_way = 2387;
+        work_new_week = 2459;
+    }
+
+    public void way() {
+        child();
+        System.out.println("time" + leave_last_week_of_large_way);
+    }
+
+    private void child() {
+        System.out.println("week" + work_new_week);
+    }
+}
