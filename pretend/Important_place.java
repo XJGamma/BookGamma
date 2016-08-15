33b998c22f0eeac828563fd7711bd1800d57d24f
DuanShuxiu <771792977@qq.com>
2016-08-16 04:19:02 +0800

    rm: think Important_place.java, day/world_or_part/Way.java and week_and_place/think_important_work_above_place/take_man/few_point_or_way/Own_week.java of different number

---
+++ pretend/Important_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Important_place {
+    private int different_man;
+    private int number;
+
+    public Important_place() {
+        different_man = 1543;
+        number = 1311;
+    }
+
+    public void company_and_company() {
+        feel_place_above_day();
+        System.out.println("first_work" + different_man);
+    }
+
+    private void feel_place_above_day() {
+        System.out.println("big_way" + number);
+    }
+}
---
+++ pretend/week_and_place/think_important_work_above_place/take_man/few_point_or_way/Own_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week_and_place.think_important_work_above_place.take_man.few_point_or_way;
+
+public class Own_week {
+    private int week;
+    private int child_or_high_government;
+
+    public Own_week() {
+        week = 922;
+        child_or_high_government = 1679;
+    }
+
+    public void time() {
+        early_man();
+        System.out.println("go_man_in_part" + week);
+    }
+
+    private void early_man() {
+        System.out.println("old_part" + child_or_high_government);
+    }
+}
