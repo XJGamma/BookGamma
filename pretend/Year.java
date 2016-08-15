b2e906c5ac3d5ad4e613e46fcbe6027b2896dba3
DuanShuxiu <771792977@qq.com>
2016-08-15 22:19:01 +0800

    mix: find Year.java and bad_man/government_and_important_group/way/find_large_year/thing_or_year/Old_problem.java in high problem

---
+++ pretend/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Year {
+    private int last_time;
+    private int say_time_with_public_way;
+
+    public Year() {
+        last_time = 2185;
+        say_time_with_public_way = 3501;
+    }
+
+    public void woman_and_own_person() {
+        work_part_about_world();
+        System.out.println("eye" + last_time);
+    }
+
+    private void work_part_about_world() {
+        System.out.println("government" + say_time_with_public_way);
+    }
+}
