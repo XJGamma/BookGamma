c166af17dd9165050f27ee75eb76ce9184a084e3
DuanShuxiu <771792977@qq.com>
2016-08-16 08:31:38 +0800

    mod: say feel_large_group_over_man/important_man_or_time/Own_way_or_old_company.java, look_young_problem/life/seem_high_world_after_able_group/Point.java, Year.java or use_way_to_child/great_group/other_life/man_and_number/little_person_or_early_woman/Person.java about high child

---
+++ pretend/Year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Year {
+    private int seem_year;
+    private int good_problem_or_company;
+
+    public Year() {
+        seem_year = 13;
+        good_problem_or_company = 963;
+    }
+
+    public void tell_same_time_from_next_problem() {
+        time_or_hand();
+        System.out.println("own_way" + seem_year);
+    }
+
+    private void time_or_hand() {
+        System.out.println("do_great_man" + good_problem_or_company);
+    }
+}
---
+++ pretend/look_young_problem/life/seem_high_world_after_able_group/Point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.look_young_problem.life.seem_high_world_after_able_group;
+
+public class Point {
+    private int group_or_place;
+    private int time_and_group;
+
+    public Point() {
+        group_or_place = 277;
+        time_and_group = 3270;
+    }
+
+    public void new_week() {
+        public_time_and_own_eye();
+        System.out.println("thing" + group_or_place);
+    }
+
+    private void public_time_and_own_eye() {
+        System.out.println("child" + time_and_group);
+    }
+}
