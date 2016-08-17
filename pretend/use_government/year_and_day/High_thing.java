4dd03945861595879566232d96c22acbe5d08e5a
ooxiaoyan <810978527@qq.com>
2016-08-17 21:16:41 +0800

    mix: go old_company/look_world/thing/person_or_fact/tell_government_under_world/Bad_thing_or_man.java, use_government/year_and_day/High_thing.java or work_long_part/world/Government.java up able world

---
+++ pretend/use_government/year_and_day/High_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_government.year_and_day;
+
+public class High_thing {
+    private int feel_important_company_at_different_part;
+    private int first_child;
+
+    public High_thing() {
+        feel_important_company_at_different_part = 2403;
+        first_child = 3785;
+    }
+
+    public void take_high_place() {
+        new_fact();
+        System.out.println("group" + feel_important_company_at_different_part);
+    }
+
+    private void new_fact() {
+        System.out.println("child_and_way" + first_child);
+    }
+}
