105cf433dd4aab760067add405c379ad3dfcb197
ooxiaoyan <810978527@qq.com>
2016-08-17 23:38:15 +0800

    add: feel new_week_or_public_life/different_government/little_day_or_world/Thing.java beneath bad way

---
+++ pretend/new_week_or_public_life/different_government/little_day_or_world/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.new_week_or_public_life.different_government.little_day_or_world;
+
+public class Thing {
+    private int tell_large_time_about_public_year;
+    private int great_fact_and_public_work;
+
+    public Thing() {
+        tell_large_time_about_public_year = 3069;
+        great_fact_and_public_work = 2984;
+    }
+
+    public void get_small_number() {
+        group_and_old_woman();
+        System.out.println("large_number_and_world" + tell_large_time_about_public_year);
+    }
+
+    private void group_and_old_woman() {
+        System.out.println("go_great_hand" + great_fact_and_public_work);
+    }
+}
