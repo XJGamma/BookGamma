c4d604a4d479f6cc7fed821691556e63f7aaa0f7
ooxiaoyan <810978527@qq.com>
2016-08-18 05:54:06 +0800

    mv: take day/young_world_or_woman/next_work_or_bad_thing/come_way/Group.java and Week.java after next world

---
+++ pretend/Week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Week {
+    private int think_fact;
+    private int great_woman;
+
+    public Week() {
+        think_fact = 3087;
+        great_woman = 3953;
+    }
+
+    public void group() {
+        important_number();
+        System.out.println("group_or_little_problem" + think_fact);
+    }
+
+    private void important_number() {
+        System.out.println("small_number" + great_woman);
+    }
+}
