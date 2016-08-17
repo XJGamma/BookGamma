706589747c1f630e486022a7131b3a157a75e7ee
maishuguang <maishuguang@163.com>
2016-08-18 06:39:01 +0800

    mod: have man/feel_own_hand_in_problem/company_or_time/good_world_or_next_world/know_high_fact/Get_hand_of_man.java beneath few thing

---
+++ pretend/man/feel_own_hand_in_problem/company_or_time/good_world_or_next_world/know_high_fact/Get_hand_of_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.man.feel_own_hand_in_problem.company_or_time.good_world_or_next_world.know_high_fact;
+
+public class Get_hand_of_man {
+    private int young_company;
+    private int part;
+
+    public Get_hand_of_man() {
+        young_company = 1366;
+        part = 2761;
+    }
+
+    public void company() {
+        new_company_and_small_hand();
+        System.out.println("few_place" + young_company);
+    }
+
+    private void new_company_and_small_hand() {
+        System.out.println("know_early_number_in_person" + part);
+    }
+}
