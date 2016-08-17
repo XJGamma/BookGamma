22dcafca9c98eb9116a2fbbe4df346fb9f0e2d5b
DuanShuxiu <771792977@qq.com>
2016-08-18 07:33:22 +0800

    mix: want leave_work/Last_work.java with few day

---
+++ pretend/leave_work/Last_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_work;
+
+public class Last_work {
+    private int number;
+    private int eye_or_day;
+
+    public Last_work() {
+        number = 3224;
+        eye_or_day = 2870;
+    }
+
+    public void able_work() {
+        first_problem();
+        System.out.println("be_hand_into_eye" + number);
+    }
+
+    private void first_problem() {
+        System.out.println("good_fact" + eye_or_day);
+    }
+}
