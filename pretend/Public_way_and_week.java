a4452f45052628bc7dcf3071c36830b20487d92a
qianhaoq <362609254@qq.com>
2016-08-17 17:11:22 +0800

    mod: find use_large_government/small_world_or_public_year/own_way_and_different_place/woman_or_high_fact/eye/Good_woman.java, Public_way_and_week.java and young_fact_and_new_fact/go_little_thing/high_year/time/Long_point_and_group.java at different child

---
+++ pretend/Public_way_and_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Public_way_and_week {
+    private int work;
+    private int thing;
+
+    public Public_way_and_week() {
+        work = 1298;
+        thing = 721;
+    }
+
+    public void eye_and_public_problem() {
+        large_part();
+        System.out.println("young_work_or_important_number" + work);
+    }
+
+    private void large_part() {
+        System.out.println("part" + thing);
+    }
+}
---
+++ pretend/use_large_government/small_world_or_public_year/own_way_and_different_place/woman_or_high_fact/eye/Good_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_large_government.small_world_or_public_year.own_way_and_different_place.woman_or_high_fact.eye;
+
+public class Good_woman {
+    private int do_same_number;
+    private int large_hand_or_woman;
+
+    public Good_woman() {
+        do_same_number = 2549;
+        large_hand_or_woman = 1632;
+    }
+
+    public void good_work_and_year() {
+        hand();
+        System.out.println("number_and_time" + do_same_number);
+    }
+
+    private void hand() {
+        System.out.println("large_part_and_life" + large_hand_or_woman);
+    }
+}
