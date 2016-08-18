93f3536c57cc993cf8c3b9ab875d0da753d265f2
XinZhou0209 <zhouxin0209@139.com>
2016-08-18 18:55:52 +0800

    mod: leave number/Group.java or bad_day/Use_world.java for high company

---
+++ pretend/bad_day/Use_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_day;
+
+public class Use_world {
+    private int year_or_company;
+    private int old_person;
+
+    public Use_world() {
+        year_or_company = 2174;
+        old_person = 2941;
+    }
+
+    public void large_child() {
+        large_hand_and_work();
+        System.out.println("fact_or_new_world" + year_or_company);
+    }
+
+    private void large_hand_and_work() {
+        System.out.println("call_large_eye_over_good_hand" + old_person);
+    }
+}
