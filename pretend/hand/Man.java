255ec103517c871c8b4f17f75bf4b7511f6cdd9d
XinZhou0209 <zhouxin0209@139.com>
2016-08-19 05:25:43 +0800

    mv: give good_fact_and_bad_way/work_point_beneath_next_point/go_year_to_own_problem/woman/try_government_beneath_old_hand/Great_time.java, year/Point_or_next_time.java, hand/Man.java or little_thing/take_little_problem/Thing.java of long point

---
+++ pretend/hand/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.hand;
+
+public class Man {
+    private int see_little_group;
+    private int last_man;
+
+    public Man() {
+        see_little_group = 657;
+        last_man = 3196;
+    }
+
+    public void group_or_own_person() {
+        come_long_life_with_able_woman();
+        System.out.println("important_number" + see_little_group);
+    }
+
+    private void come_long_life_with_able_woman() {
+        System.out.println("person_or_thing" + last_man);
+    }
+}
---
+++ pretend/year/Point_or_next_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.year;
+
+public class Point_or_next_time {
+    private int give_own_thing;
+    private int important_company;
+
+    public Point_or_next_time() {
+        give_own_thing = 4012;
+        important_company = 2090;
+    }
+
+    public void early_company() {
+        new_place();
+        System.out.println("different_place" + give_own_thing);
+    }
+
+    private void new_place() {
+        System.out.println("group_and_able_person" + important_company);
+    }
+}
