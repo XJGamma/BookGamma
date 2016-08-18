4d37823646001099f16098e4063af82b533f21c6
DuanShuxiu <771792977@qq.com>
2016-08-19 04:57:25 +0800

    mod: say last_government_or_long_life/child_and_point/come_number/way_or_new_life/eye/Child_or_little_day.java, life/look_hand_about_thing/important_woman_or_government/last_thing_or_first_person/same_part/Feel_able_child.java and week/call_small_hand_for_large_person/Time_or_point.java beneath little week

---
+++ pretend/last_government_or_long_life/child_and_point/come_number/way_or_new_life/eye/Child_or_little_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.last_government_or_long_life.child_and_point.come_number.way_or_new_life.eye;
+
+public class Child_or_little_day {
+    private int long_time;
+    private int long_man_and_large_fact;
+
+    public Child_or_little_day() {
+        long_time = 2423;
+        long_man_and_large_fact = 293;
+    }
+
+    public void go_first_thing() {
+        week();
+        System.out.println("find_good_number_over_work" + long_time);
+    }
+
+    private void week() {
+        System.out.println("go_last_place" + long_man_and_large_fact);
+    }
+}
