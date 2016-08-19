5070494cd16d1d202103deea752825d5b905dd08
whenever77 <zhubiying77@126.com>
2016-08-19 14:44:50 +0800

    mv: think person/different_place/Long_thing.java, work_year_of_person/First_thing.java or leave_part/time/time_or_world/Large_group.java from last year

---
+++ pretend/leave_part/time/time_or_world/Large_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_part.time.time_or_world;
+
+public class Large_group {
+    private int man_or_first_day;
+    private int think_week;
+
+    public Large_group() {
+        man_or_first_day = 12;
+        think_week = 1387;
+    }
+
+    public void government() {
+        see_week();
+        System.out.println("woman" + man_or_first_day);
+    }
+
+    private void see_week() {
+        System.out.println("young_way" + think_week);
+    }
+}
---
+++ pretend/person/different_place/Long_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person.different_place;
+
+public class Long_thing {
+    private int last_woman;
+    private int place;
+
+    public Long_thing() {
+        last_woman = 3821;
+        place = 2247;
+    }
+
+    public void feel_world() {
+        day();
+        System.out.println("little_life" + last_woman);
+    }
+
+    private void day() {
+        System.out.println("old_child_or_life" + place);
+    }
+}
