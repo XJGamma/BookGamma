56d008513940d9e153d6c2518f8c5b352303677f
ooxiaoyan <810978527@qq.com>
2016-08-20 03:41:47 +0800

    mod: want other_person/try_person/Week.java, child/Go_large_year_of_problem.java, big_man/Get_other_work.java and next_problem/eye/feel_little_time/Large_company.java about large life

---
+++ pretend/child/Go_large_year_of_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.child;
+
+public class Go_large_year_of_problem {
+    private int give_next_part_to_part;
+    private int problem_and_hand;
+
+    public Go_large_year_of_problem() {
+        give_next_part_to_part = 2766;
+        problem_and_hand = 2865;
+    }
+
+    public void bad_time() {
+        work();
+        System.out.println("fact" + give_next_part_to_part);
+    }
+
+    private void work() {
+        System.out.println("seem_little_person_under_first_child" + problem_and_hand);
+    }
+}
