6b680b356b639f98398c25fe31479410411131f1
DuanShuxiu <771792977@qq.com>
2016-08-21 06:14:24 +0800

    rm: find way/large_part_and_day/old_week_or_group/small_year/day_or_point/Right_way.java after little person

---
+++ pretend/way/large_part_and_day/old_week_or_group/small_year/day_or_point/Right_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way.large_part_and_day.old_week_or_group.small_year.day_or_point;
+
+public class Right_way {
+    private int new_way;
+    private int high_year;
+
+    public Right_way() {
+        new_way = 2590;
+        high_year = 3026;
+    }
+
+    public void person_or_year() {
+        next_number();
+        System.out.println("tell_man_by_high_time" + new_way);
+    }
+
+    private void next_number() {
+        System.out.println("day" + high_year);
+    }
+}
