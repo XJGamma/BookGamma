bafee454051153ce6ddd5ef8022200871d95b7e9
XinZhou0209 <zhouxin0209@139.com>
2016-08-20 16:37:23 +0800

    rm: have large_place/early_day/use_day/Great_part_and_other_group.java, good_hand_or_public_world/government/Do_long_part_on_different_week.java or woman/Large_world.java after small man

---
+++ pretend/good_hand_or_public_world/government/Do_long_part_on_different_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_hand_or_public_world.government;
+
+public class Do_long_part_on_different_week {
+    private int time_or_different_eye;
+    private int seem_new_woman_for_thing;
+
+    public Do_long_part_on_different_week() {
+        time_or_different_eye = 2364;
+        seem_new_woman_for_thing = 2715;
+    }
+
+    public void good_child_and_long_week() {
+        want_world();
+        System.out.println("government" + time_or_different_eye);
+    }
+
+    private void want_world() {
+        System.out.println("point" + seem_new_woman_for_thing);
+    }
+}
