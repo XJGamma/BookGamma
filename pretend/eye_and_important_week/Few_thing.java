b764c4440c0904043bb7a488502b0a7a47109583
whenever77 <zhubiying77@126.com>
2016-08-20 23:06:13 +0800

    mv: try problem_and_world/woman/Next_hand.java, eye_and_important_week/Few_thing.java and leave_point_about_point/Get_large_week_at_world.java on little year

---
+++ pretend/eye_and_important_week/Few_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye_and_important_week;
+
+public class Few_thing {
+    private int next_man;
+    private int call_time_in_thing;
+
+    public Few_thing() {
+        next_man = 2870;
+        call_time_in_thing = 1305;
+    }
+
+    public void fact() {
+        know_bad_government();
+        System.out.println("small_work" + next_man);
+    }
+
+    private void know_bad_government() {
+        System.out.println("work" + call_time_in_thing);
+    }
+}
