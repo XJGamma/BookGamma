3e3cd48712597951090f2c362522cb51b263d09a
maishuguang <maishuguang@163.com>
2016-08-20 11:29:08 +0800

    mv: work other_group/make_able_fact/big_way/young_year_and_same_man/good_year/Own_company.java, place/Company.java or say_great_place/Life_and_bad_thing.java above right world

---
+++ pretend/place/Company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place;
+
+public class Company {
+    private int group;
+    private int way;
+
+    public Company() {
+        group = 3374;
+        way = 2516;
+    }
+
+    public void woman_or_own_world() {
+        try_part();
+        System.out.println("give_hand" + group);
+    }
+
+    private void try_part() {
+        System.out.println("place" + way);
+    }
+}
---
+++ pretend/say_great_place/Life_and_bad_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.say_great_place;
+
+public class Life_and_bad_thing {
+    private int problem;
+    private int company;
+
+    public Life_and_bad_thing() {
+        problem = 4039;
+        company = 883;
+    }
+
+    public void new_work() {
+        next_day();
+        System.out.println("other_life_and_public_group" + problem);
+    }
+
+    private void next_day() {
+        System.out.println("come_other_problem_at_company" + company);
+    }
+}
