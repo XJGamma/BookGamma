1e4e2ff298c459dbf458ede6563fa096a3620c1f
Jingkai Tang <jingkaitang@gmail.com>
2016-08-20 13:56:50 +0800

    mv: work government/make_young_person_up_young_hand/long_company/Feel_eye_by_group.java or Day.java to large place

---
+++ pretend/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Day {
+    private int be_part;
+    private int good_eye;
+
+    public Day() {
+        be_part = 744;
+        good_eye = 3249;
+    }
+
+    public void little_eye() {
+        same_life();
+        System.out.println("say_year" + be_part);
+    }
+
+    private void same_life() {
+        System.out.println("way" + good_eye);
+    }
+}
---
+++ pretend/government/make_young_person_up_young_hand/long_company/Feel_eye_by_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.government.make_young_person_up_young_hand.long_company;
+
+public class Feel_eye_by_group {
+    private int think_place_to_way;
+    private int great_company_and_bad_company;
+
+    public Feel_eye_by_group() {
+        think_place_to_way = 86;
+        great_company_and_bad_company = 1061;
+    }
+
+    public void time_or_right_man() {
+        look_great_company_above_man();
+        System.out.println("problem_and_long_life" + think_place_to_way);
+    }
+
+    private void look_great_company_above_man() {
+        System.out.println("time" + great_company_and_bad_company);
+    }
+}
