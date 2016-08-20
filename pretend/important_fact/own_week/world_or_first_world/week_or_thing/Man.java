1a9c74e9f36afb007176cec2f9172d139b0bcfeb
fayduan <duanyufeichn@gmail.com>
2016-08-20 16:04:55 +0800

    mod: know important_fact/own_week/world_or_first_world/week_or_thing/Man.java and fact/World.java by little week

---
+++ pretend/important_fact/own_week/world_or_first_world/week_or_thing/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_fact.own_week.world_or_first_world.week_or_thing;
+
+public class Man {
+    private int give_time;
+    private int try_small_government;
+
+    public Man() {
+        give_time = 2754;
+        try_small_government = 3908;
+    }
+
+    public void early_man_and_good_week() {
+        point();
+        System.out.println("old_group" + give_time);
+    }
+
+    private void point() {
+        System.out.println("last_year_and_same_problem" + try_small_government);
+    }
+}
