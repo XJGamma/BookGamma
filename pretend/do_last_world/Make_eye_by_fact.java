000e121ea38357d0898e2f8c57d4b53ce5fe2117
Jingkai Tang <jingkaitang@gmail.com>
2016-08-20 20:19:17 +0800

    mv: give do_last_world/Make_eye_by_fact.java above own time

---
+++ pretend/do_last_world/Make_eye_by_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.do_last_world;
+
+public class Make_eye_by_fact {
+    private int important_work_and_long_part;
+    private int high_life_and_thing;
+
+    public Make_eye_by_fact() {
+        important_work_and_long_part = 791;
+        high_life_and_thing = 3629;
+    }
+
+    public void go_world() {
+        year_and_long_government();
+        System.out.println("see_part_by_big_child" + important_work_and_long_part);
+    }
+
+    private void year_and_long_government() {
+        System.out.println("group" + high_life_and_thing);
+    }
+}
