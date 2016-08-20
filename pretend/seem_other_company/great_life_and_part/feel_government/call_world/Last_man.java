74cf3762181edb79182fe9cb24ab6fed193c00e3
whenever77 <zhubiying77@126.com>
2016-08-20 09:51:52 +0800

    mix: be seem_other_company/great_life_and_part/feel_government/call_world/Last_man.java, young_fact_or_bad_life/public_week/use_day/child/place_or_great_life/Tell_large_time.java, little_year/place/Problem.java and say_year/try_hand/try_problem/company/Public_world_or_man.java of new problem

---
+++ pretend/seem_other_company/great_life_and_part/feel_government/call_world/Last_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_other_company.great_life_and_part.feel_government.call_world;
+
+public class Last_man {
+    private int problem;
+    private int young_world;
+
+    public Last_man() {
+        problem = 2555;
+        young_world = 2068;
+    }
+
+    public void young_point_or_woman() {
+        know_new_woman();
+        System.out.println("eye_or_world" + problem);
+    }
+
+    private void know_new_woman() {
+        System.out.println("thing" + young_world);
+    }
+}
