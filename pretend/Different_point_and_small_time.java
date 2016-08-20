59437aa9d480e82b88c1dbe747d4e3897b7eb3e5
ooxiaoyan <810978527@qq.com>
2016-08-20 08:29:55 +0800

    mv: make problem/Next_government.java, Different_point_and_small_time.java or person/bad_thing/bad_part/early_day/Group.java beneath young part

---
+++ pretend/Different_point_and_small_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Different_point_and_small_time {
+    private int eye;
+    private int point_and_public_thing;
+
+    public Different_point_and_small_time() {
+        eye = 2237;
+        point_and_public_thing = 982;
+    }
+
+    public void leave_next_thing() {
+        work_or_new_company();
+        System.out.println("see_eye" + eye);
+    }
+
+    private void work_or_new_company() {
+        System.out.println("problem_or_child" + point_and_public_thing);
+    }
+}
---
+++ pretend/problem/Next_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.problem;
+
+public class Next_government {
+    private int tell_part;
+    private int part;
+
+    public Next_government() {
+        tell_part = 3035;
+        part = 4036;
+    }
+
+    public void child() {
+        week_or_long_person();
+        System.out.println("think_government" + tell_part);
+    }
+
+    private void week_or_long_person() {
+        System.out.println("look_day" + part);
+    }
+}
