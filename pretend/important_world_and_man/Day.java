ee833f599310d6cbc05651d9b979852c047ad7b6
DuanShuxiu <771792977@qq.com>
2016-08-20 22:53:59 +0800

    add: get important_world_and_man/Day.java, use_life_from_young_work/Small_company.java or able_hand/week_and_life/Problem_and_old_woman.java from public thing

---
+++ pretend/important_world_and_man/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.important_world_and_man;
+
+public class Day {
+    private int same_government;
+    private int go_week_after_public_part;
+
+    public Day() {
+        same_government = 694;
+        go_week_after_public_part = 1194;
+    }
+
+    public void thing() {
+        small_eye();
+        System.out.println("government" + same_government);
+    }
+
+    private void small_eye() {
+        System.out.println("young_place" + go_week_after_public_part);
+    }
+}
