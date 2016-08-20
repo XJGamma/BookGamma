57872d9de31f72da00fdcf4787488f23007a1d6b
fayduan <duanyufeichn@gmail.com>
2016-08-21 07:06:14 +0800

    add: use way_or_part/little_child_and_last_world/say_woman_over_other_part/company/year/Young_life.java, problem_or_large_fact/bad_life/ask_able_fact_in_high_woman/early_world_or_early_year/Long_time.java or day/high_company_or_early_life/Few_number.java at same government

---
+++ pretend/way_or_part/little_child_and_last_world/say_woman_over_other_part/company/year/Young_life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way_or_part.little_child_and_last_world.say_woman_over_other_part.company.year;
+
+public class Young_life {
+    private int small_child_and_last_government;
+    private int way;
+
+    public Young_life() {
+        small_child_and_last_government = 1833;
+        way = 1167;
+    }
+
+    public void old_government_and_able_government() {
+        have_young_work_from_work();
+        System.out.println("year" + small_child_and_last_government);
+    }
+
+    private void have_young_work_from_work() {
+        System.out.println("large_government" + way);
+    }
+}
