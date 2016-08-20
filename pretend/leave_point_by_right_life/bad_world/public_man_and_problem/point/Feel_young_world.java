0c630ff7aefffb75579b4cab4863c35207b30a42
whenever77 <zhubiying77@126.com>
2016-08-20 09:55:34 +0800

    mod: look tell_number_from_work/Fact.java, leave_point_by_right_life/bad_world/public_man_and_problem/point/Feel_young_world.java or fact/public_company_or_long_point/Get_hand_with_thing.java under high week

---
+++ pretend/leave_point_by_right_life/bad_world/public_man_and_problem/point/Feel_young_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.leave_point_by_right_life.bad_world.public_man_and_problem.point;
+
+public class Feel_young_world {
+    private int important_thing;
+    private int group;
+
+    public Feel_young_world() {
+        important_thing = 3671;
+        group = 1267;
+    }
+
+    public void use_man_under_great_world() {
+        place();
+        System.out.println("child" + important_thing);
+    }
+
+    private void place() {
+        System.out.println("week" + group);
+    }
+}
