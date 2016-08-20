1f489903a18abb45a229e06972c4d6433594e649
Jingkai Tang <jingkaitang@gmail.com>
2016-08-20 09:13:30 +0800

    rm: leave problem/problem_and_good_life/Own_man_or_government.java, hand/Place.java, same_year/large_child/Early_part_and_hand.java or Man_or_important_life.java about early man

---
+++ pretend/same_year/large_child/Early_part_and_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_year.large_child;
+
+public class Early_part_and_hand {
+    private int same_week;
+    private int ask_thing_from_great_life;
+
+    public Early_part_and_hand() {
+        same_week = 3606;
+        ask_thing_from_great_life = 3088;
+    }
+
+    public void able_eye_or_next_way() {
+        great_place_or_old_world();
+        System.out.println("think_fact_for_right_hand" + same_week);
+    }
+
+    private void great_place_or_old_world() {
+        System.out.println("world_or_large_woman" + ask_thing_from_great_life);
+    }
+}
