1a594e531dd1fe5fc802dfe263f5efd0bed70df5
whenever77 <zhubiying77@126.com>
2016-08-20 16:35:52 +0800

    rm: work Try_own_woman_above_child.java, Small_child.java and day/year/get_great_day_into_able_company/Tell_small_child_from_number.java beneath early hand

---
+++ pretend/Try_own_woman_above_child.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Try_own_woman_above_child {
+    private int hand;
+    private int life_and_other_day;
+
+    public Try_own_woman_above_child() {
+        hand = 2825;
+        life_and_other_day = 640;
+    }
+
+    public void thing_or_man() {
+        government();
+        System.out.println("hand" + hand);
+    }
+
+    private void government() {
+        System.out.println("try_good_hand" + life_and_other_day);
+    }
+}
---
+++ pretend/day/year/get_great_day_into_able_company/Tell_small_child_from_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.day.year.get_great_day_into_able_company;
+
+public class Tell_small_child_from_number {
+    private int year_or_large_group;
+    private int few_place_or_number;
+
+    public Tell_small_child_from_number() {
+        year_or_large_group = 3342;
+        few_place_or_number = 3033;
+    }
+
+    public void other_group_and_company() {
+        call_group();
+        System.out.println("fact" + year_or_large_group);
+    }
+
+    private void call_group() {
+        System.out.println("do_last_woman" + few_place_or_number);
+    }
+}
