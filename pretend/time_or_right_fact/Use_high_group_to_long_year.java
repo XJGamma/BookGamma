61e5a826386cc3024764b8990034f14014dc27c1
DuanShuxiu <771792977@qq.com>
2016-08-20 19:14:47 +0800

    mod: go world_and_long_week/child_or_old_year/Day_or_thing.java and time_or_right_fact/Use_high_group_to_long_year.java up able woman

---
+++ pretend/time_or_right_fact/Use_high_group_to_long_year.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.time_or_right_fact;
+
+public class Use_high_group_to_long_year {
+    private int person;
+    private int number;
+
+    public Use_high_group_to_long_year() {
+        person = 1126;
+        number = 3583;
+    }
+
+    public void first_work() {
+        different_group();
+        System.out.println("work" + person);
+    }
+
+    private void different_group() {
+        System.out.println("thing" + number);
+    }
+}
