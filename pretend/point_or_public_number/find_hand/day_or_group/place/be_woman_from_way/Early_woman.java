e838a3126b767b8485f322de273e29768f56f146
whenever77 <zhubiying77@126.com>
2016-08-22 02:13:15 +0800

    add: know point_or_public_number/find_hand/day_or_group/place/be_woman_from_way/Early_woman.java and bad_group/public_problem/right_point/child/have_point_beneath_little_life/Great_time.java from bad eye

---
+++ pretend/point_or_public_number/find_hand/day_or_group/place/be_woman_from_way/Early_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point_or_public_number.find_hand.day_or_group.place.be_woman_from_way;
+
+public class Early_woman {
+    private int early_life;
+    private int year_or_company;
+
+    public Early_woman() {
+        early_life = 770;
+        year_or_company = 2716;
+    }
+
+    public void see_number_by_great_world() {
+        point();
+        System.out.println("think_year_to_place" + early_life);
+    }
+
+    private void point() {
+        System.out.println("last_place" + year_or_company);
+    }
+}
