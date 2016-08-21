2263eea12dc36c9bb423e89bcee9684dae8f01bf
qianhaoq <362609254@qq.com>
2016-08-21 16:04:07 +0800

    mod: call company/work/Do_place_by_way.java with young world

---
+++ pretend/company/work/Do_place_by_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company.work;
+
+public class Do_place_by_way {
+    private int use_other_number;
+    private int big_man;
+
+    public Do_place_by_way() {
+        use_other_number = 1632;
+        big_man = 2145;
+    }
+
+    public void call_old_woman() {
+        able_eye();
+        System.out.println("hand_and_day" + use_other_number);
+    }
+
+    private void able_eye() {
+        System.out.println("ask_thing_about_great_eye" + big_man);
+    }
+}
