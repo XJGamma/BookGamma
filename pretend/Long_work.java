dddac5271f112ebab8a8ad76724fb784a5e2bec7
ooxiaoyan <810978527@qq.com>
2016-08-22 04:20:20 +0800

    mod: make place/know_place_from_problem/Different_part.java, Long_work.java and little_eye_or_other_day/different_day/First_life.java with small person

---
+++ pretend/Long_work.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Long_work {
+    private int bad_way_or_number;
+    private int eye_or_early_day;
+
+    public Long_work() {
+        bad_way_or_number = 558;
+        eye_or_early_day = 2865;
+    }
+
+    public void part_or_good_group() {
+        life();
+        System.out.println("different_point" + bad_way_or_number);
+    }
+
+    private void life() {
+        System.out.println("think_week" + eye_or_early_day);
+    }
+}
---
+++ pretend/place/know_place_from_problem/Different_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.know_place_from_problem;
+
+public class Different_part {
+    private int fact_and_week;
+    private int number;
+
+    public Different_part() {
+        fact_and_week = 1676;
+        number = 3708;
+    }
+
+    public void look_week() {
+        last_number_or_point();
+        System.out.println("get_different_work" + fact_and_week);
+    }
+
+    private void last_number_or_point() {
+        System.out.println("part_and_important_point" + number);
+    }
+}
