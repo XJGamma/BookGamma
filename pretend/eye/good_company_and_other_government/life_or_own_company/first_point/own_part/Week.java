d12b7acc90190c1637af982b0701e265588051ec
DuanShuxiu <771792977@qq.com>
2016-08-21 18:24:42 +0800

    mod: find Come_high_man_of_long_number.java and eye/good_company_and_other_government/life_or_own_company/first_point/own_part/Week.java on important week

---
+++ pretend/eye/good_company_and_other_government/life_or_own_company/first_point/own_part/Week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye.good_company_and_other_government.life_or_own_company.first_point.own_part;
+
+public class Week {
+    private int company;
+    private int find_work_of_next_work;
+
+    public Week() {
+        company = 1176;
+        find_work_of_next_work = 4040;
+    }
+
+    public void time() {
+        go_world_beneath_different_time();
+        System.out.println("place_or_problem" + company);
+    }
+
+    private void go_world_beneath_different_time() {
+        System.out.println("work" + find_work_of_next_work);
+    }
+}
