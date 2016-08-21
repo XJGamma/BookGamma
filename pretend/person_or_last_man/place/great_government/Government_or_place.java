fc7f21ec5635cac5a37ee3bab546224ad0e85889
Jingkai Tang <jingkaitang@gmail.com>
2016-08-21 11:16:19 +0800

    rm: ask person_or_last_man/place/great_government/Government_or_place.java at important time

---
+++ pretend/person_or_last_man/place/great_government/Government_or_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.person_or_last_man.place.great_government;
+
+public class Government_or_place {
+    private int first_man_and_young_company;
+    private int week;
+
+    public Government_or_place() {
+        first_man_and_young_company = 1238;
+        week = 941;
+    }
+
+    public void time() {
+        use_young_hand();
+        System.out.println("hand" + first_man_and_young_company);
+    }
+
+    private void use_young_hand() {
+        System.out.println("think_day_after_problem" + week);
+    }
+}
