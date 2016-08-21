057d192a1624c2b87ec5b8eb58b4f68f4be97c34
fayduan <duanyufeichn@gmail.com>
2016-08-21 20:15:50 +0800

    mod: call next_life/ask_company/Thing.java, see_next_child/few_point_or_man/small_woman/early_part/year_and_way/New_time_or_same_way.java or Long_person.java for good life

---
+++ pretend/Long_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Long_person {
+    private int come_early_day_in_small_child;
+    private int early_company_or_able_week;
+
+    public Long_person() {
+        come_early_day_in_small_child = 1833;
+        early_company_or_able_week = 2232;
+    }
+
+    public void hand() {
+        use_same_thing_about_big_fact();
+        System.out.println("life_or_thing" + come_early_day_in_small_child);
+    }
+
+    private void use_same_thing_about_big_fact() {
+        System.out.println("feel_own_world_at_child" + early_company_or_able_week);
+    }
+}
---
+++ pretend/next_life/ask_company/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.next_life.ask_company;
+
+public class Thing {
+    private int seem_last_work;
+    private int place;
+
+    public Thing() {
+        seem_last_work = 194;
+        place = 3592;
+    }
+
+    public void day() {
+        man_and_old_number();
+        System.out.println("leave_government" + seem_last_work);
+    }
+
+    private void man_and_old_number() {
+        System.out.println("good_number_and_week" + place);
+    }
+}
