93bd32cce0376f494d6bb2169c6d36cdf3777579
ooxiaoyan <810978527@qq.com>
2016-08-23 02:45:13 +0800

    mv: want way/part_or_day/Large_year_or_little_time.java and young_eye/child_and_problem/Early_week.java on last world

---
+++ pretend/way/part_or_day/Large_year_or_little_time.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way.part_or_day;
+
+public class Large_year_or_little_time {
+    private int year_or_big_way;
+    private int other_week_or_eye;
+
+    public Large_year_or_little_time() {
+        year_or_big_way = 2051;
+        other_week_or_eye = 3816;
+    }
+
+    public void problem() {
+        ask_woman_above_group();
+        System.out.println("seem_bad_way" + year_or_big_way);
+    }
+
+    private void ask_woman_above_group() {
+        System.out.println("small_company_and_world" + other_week_or_eye);
+    }
+}
