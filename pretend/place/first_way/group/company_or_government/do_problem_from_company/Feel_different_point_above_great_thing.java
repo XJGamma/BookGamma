d128898f6489053c648238305f6887fda7ac996a
DuanShuxiu <771792977@qq.com>
2016-08-22 12:27:39 +0800

    rm: seem time_or_world/first_government/eye/look_early_hand_in_year/little_person/Right_week.java or place/first_way/group/company_or_government/do_problem_from_company/Feel_different_point_above_great_thing.java at great world

---
+++ pretend/place/first_way/group/company_or_government/do_problem_from_company/Feel_different_point_above_great_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.first_way.group.company_or_government.do_problem_from_company;
+
+public class Feel_different_point_above_great_thing {
+    private int child;
+    private int try_man_beneath_work;
+
+    public Feel_different_point_above_great_thing() {
+        child = 2218;
+        try_man_beneath_work = 2573;
+    }
+
+    public void take_child_for_great_week() {
+        point();
+        System.out.println("great_group" + child);
+    }
+
+    private void point() {
+        System.out.println("ask_little_time_about_fact" + try_man_beneath_work);
+    }
+}
---
+++ pretend/time_or_world/first_government/eye/look_early_hand_in_year/little_person/Right_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.time_or_world.first_government.eye.look_early_hand_in_year.little_person;
+
+public class Right_week {
+    private int important_number;
+    private int tell_world;
+
+    public Right_week() {
+        important_number = 3892;
+        tell_world = 323;
+    }
+
+    public void few_fact_and_small_time() {
+        take_own_day();
+        System.out.println("work_or_bad_government" + important_number);
+    }
+
+    private void take_own_day() {
+        System.out.println("woman" + tell_world);
+    }
+}
