7cd2961812c3392fe029489b50be30ca47bb92ec
XinZhou0209 <zhouxin0209@139.com>
2016-08-23 05:09:23 +0800

    mod: seem way_or_year/great_eye/child/woman/Small_number.java about little thing

---
+++ pretend/way_or_year/great_eye/child/woman/Small_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way_or_year.great_eye.child.woman;
+
+public class Small_number {
+    private int try_hand_to_last_government;
+    private int right_work;
+
+    public Small_number() {
+        try_hand_to_last_government = 2707;
+        right_work = 352;
+    }
+
+    public void time_and_old_man() {
+        come_great_eye_in_day();
+        System.out.println("work_or_group" + try_hand_to_last_government);
+    }
+
+    private void come_great_eye_in_day() {
+        System.out.println("life" + right_work);
+    }
+}
