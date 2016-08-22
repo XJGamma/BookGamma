03adec31b45acb0c2979bb88f00edaf95e05ebfe
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-08-22 21:31:03 +0800

    修改：登录布局

--- app/src/main/java/cn/edu/xjtu/se/bookgamma/WelcomeActivity.java
+++ app/src/main/java/cn/edu/xjtu/se/bookgamma/WelcomeActivity.java
@@ -2,20 +2,26 @@ package cn.edu.xjtu.se.bookgamma;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by DUAN Yufei on 2016/6/16.
  */
 public class WelcomeActivity extends Activity {
+
+    private EditText etUser;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnReg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_welcome);
 
 /*        new Handler().postDelayed(new Runnable() {
