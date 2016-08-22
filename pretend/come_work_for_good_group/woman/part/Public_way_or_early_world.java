0430793034bd1a498e0fd53a7ea6466db81964dc
DuanShuxiu <771792977@qq.com>
2016-08-22 21:01:36 +0800

    mod: be world/Time.java, want_little_hand_after_young_problem/part/be_great_child_after_time/work_number/first_fact/Life.java or come_work_for_good_group/woman/part/Public_way_or_early_world.java up own time

---
+++ pretend/come_work_for_good_group/woman/part/Public_way_or_early_world.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.come_work_for_good_group.woman.part;
+
+public class Public_way_or_early_world {
+    private int time;
+    private int try_early_government;
+
+    public Public_way_or_early_world() {
+        time = 689;
+        try_early_government = 2282;
+    }
+
+    public void new_child() {
+        young_problem();
+        System.out.println("small_thing" + time);
+    }
+
+    private void young_problem() {
+        System.out.println("long_eye" + try_early_government);
+    }
+}
---
+++ pretend/want_little_hand_after_young_problem/part/be_great_child_after_time/work_number/first_fact/Life.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.want_little_hand_after_young_problem.part.be_great_child_after_time.work_number.first_fact;
+
+public class Life {
+    private int have_able_way;
+    private int first_eye_or_week;
+
+    public Life() {
+        have_able_way = 3869;
+        first_eye_or_week = 3302;
+    }
+
+    public void right_person() {
+        first_place();
+        System.out.println("time_and_eye" + have_able_way);
+    }
+
+    private void first_place() {
+        System.out.println("different_person" + first_eye_or_week);
+    }
+}
