163fd0e65055b20b3d47cd20328d8cf8dfed8a94
ooxiaoyan <810978527@qq.com>
2016-08-23 05:29:18 +0800

    mv: work Way.java, Thing.java, able_problem/big_woman/look_first_eye/world/work/Call_few_eye.java and man_or_own_life/work_large_place_on_world/group/woman_and_early_week/Make_woman.java of next work

---
+++ pretend/Thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Thing {
+    private int try_good_eye_on_other_work;
+    private int other_group;
+
+    public Thing() {
+        try_good_eye_on_other_work = 2292;
+        other_group = 1871;
+    }
+
+    public void say_early_hand_with_hand() {
+        look_high_person();
+        System.out.println("place_or_company" + try_good_eye_on_other_work);
+    }
+
+    private void look_high_person() {
+        System.out.println("tell_thing" + other_group);
+    }
+}
