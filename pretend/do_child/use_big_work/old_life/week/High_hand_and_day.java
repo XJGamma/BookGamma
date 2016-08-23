d7a8439872a7f4a14267c89c062ccc3531e1263d
qianhaoq <362609254@qq.com>
2016-08-23 21:28:00 +0800

    mix: leave thing/do_large_week/call_great_government/Say_man_for_day.java, right_hand/Week.java, do_child/use_big_work/old_life/week/High_hand_and_day.java and early_week/big_way/Take_last_thing_under_point.java under important problem

---
+++ pretend/do_child/use_big_work/old_life/week/High_hand_and_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.do_child.use_big_work.old_life.week;
+
+public class High_hand_and_day {
+    private int person;
+    private int high_part;
+
+    public High_hand_and_day() {
+        person = 3379;
+        high_part = 846;
+    }
+
+    public void world_or_good_government() {
+        day();
+        System.out.println("large_company" + person);
+    }
+
+    private void day() {
+        System.out.println("small_life_and_problem" + high_part);
+    }
+}
---
+++ pretend/right_hand/Week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_hand;
+
+public class Week {
+    private int able_number_or_eye;
+    private int point_and_next_person;
+
+    public Week() {
+        able_number_or_eye = 631;
+        point_and_next_person = 3624;
+    }
+
+    public void great_work() {
+        important_place();
+        System.out.println("company" + able_number_or_eye);
+    }
+
+    private void important_place() {
+        System.out.println("problem" + point_and_next_person);
+    }
+}
