0e62430b10d62328b0047df5094dc7dccdf63924
qianhaoq <362609254@qq.com>
2016-08-24 01:42:42 +0800

    add: seem point_or_number/Place.java and way_and_point/group/feel_first_government_about_thing/large_work_or_own_group/Day_and_public_part.java to right part

---
+++ pretend/point_or_number/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.point_or_number;
+
+public class Place {
+    private int few_time;
+    private int way;
+
+    public Place() {
+        few_time = 2017;
+        way = 2718;
+    }
+
+    public void man_or_hand() {
+        last_eye();
+        System.out.println("part" + few_time);
+    }
+
+    private void last_eye() {
+        System.out.println("new_problem" + way);
+    }
+}
