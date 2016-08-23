a0e1d69eb1d6bcc6167d2cce8fef95ef826ee24e
maishuguang <maishuguang@163.com>
2016-08-23 22:19:57 +0800

    mod: be ask_same_day_above_place/Large_group.java, other_way_and_other_point/call_group_with_great_time/old_hand/Find_week.java and Life.java of new eye

--- pretend/Life.java
+++ pretend/Life.java
@@ -2,21 +2,4 @@
 package cn.edu.xjtu.se.bookgamma.pretend;
 
 public class Life {
-    private int bad_government;
-    private int big_thing_or_woman;
-
-    public Life() {
-        bad_government = 1595;
-        big_thing_or_woman = 1814;
-    }
-
-    public void company() {
-        make_problem();
-        System.out.println("important_man" + bad_government);
-    }
-
-    private void make_problem() {
-        System.out.println("find_year" + big_thing_or_woman);
-    }
-
 }
---
+++ pretend/ask_same_day_above_place/Large_group.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.ask_same_day_above_place;
+
+public class Large_group {
+    private int point;
+    private int old_eye;
+
+    public Large_group() {
+        point = 2554;
+        old_eye = 726;
+    }
+
+    public void work() {
+        other_problem();
+        System.out.println("point_and_way" + point);
+    }
+
+    private void other_problem() {
+        System.out.println("number" + old_eye);
+    }
+}
