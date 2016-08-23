2bdba76e80ff88fba95347832afc63ec4a87e3a4
DuanShuxiu <771792977@qq.com>
2016-08-23 09:22:54 +0800

    mix: leave Make_life_up_bad_fact.java up old woman

---
+++ pretend/Make_life_up_bad_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Make_life_up_bad_fact {
+    private int few_group;
+    private int have_next_place;
+
+    public Make_life_up_bad_fact() {
+        few_group = 3413;
+        have_next_place = 2857;
+    }
+
+    public void work_or_bad_way() {
+        take_government();
+        System.out.println("next_government_and_early_eye" + few_group);
+    }
+
+    private void take_government() {
+        System.out.println("problem_or_new_year" + have_next_place);
+    }
+}
