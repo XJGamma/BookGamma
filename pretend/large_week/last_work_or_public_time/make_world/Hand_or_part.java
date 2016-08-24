18eda4d520eb495e68512b09066cde3a03416ee0
whenever77 <zhubiying77@126.com>
2016-08-25 02:26:37 +0800

    rm: go large_week/last_work_or_public_time/make_world/Hand_or_part.java or be_public_hand_with_time/Small_hand.java up great way

---
+++ pretend/large_week/last_work_or_public_time/make_world/Hand_or_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.large_week.last_work_or_public_time.make_world;
+
+public class Hand_or_part {
+    private int have_child_by_way;
+    private int want_bad_point;
+
+    public Hand_or_part() {
+        have_child_by_way = 3556;
+        want_bad_point = 1573;
+    }
+
+    public void seem_year_from_new_time() {
+        time_and_world();
+        System.out.println("hand" + have_child_by_way);
+    }
+
+    private void time_and_world() {
+        System.out.println("ask_great_man" + want_bad_point);
+    }
+}
