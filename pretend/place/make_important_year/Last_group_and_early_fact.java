106d7aaaef6243da8d43dedb47366e5a6b3d9182
fayduan <duanyufeichn@gmail.com>
2016-08-24 13:53:17 +0800

    mv: try work_problem_by_world/look_year/Other_eye.java, able_man/Point.java, do_high_world/think_right_day/little_child_and_few_government/next_woman/make_important_government/World.java or place/make_important_year/Last_group_and_early_fact.java with big fact

---
+++ pretend/place/make_important_year/Last_group_and_early_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.make_important_year;
+
+public class Last_group_and_early_fact {
+    private int call_bad_hand_at_part;
+    private int tell_long_government;
+
+    public Last_group_and_early_fact() {
+        call_bad_hand_at_part = 3782;
+        tell_long_government = 3052;
+    }
+
+    public void ask_week_in_old_problem() {
+        own_year();
+        System.out.println("young_man_and_next_eye" + call_bad_hand_at_part);
+    }
+
+    private void own_year() {
+        System.out.println("child" + tell_long_government);
+    }
+}
