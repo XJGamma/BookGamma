b1bfa797e8af186aa5b29a246946459163f620fe
XinZhou0209 <zhouxin0209@139.com>
2016-08-25 04:56:06 +0800

    mv: feel thing/group_and_company/make_problem_by_bad_hand/be_company_beneath_few_company/Want_next_person.java, have_own_number_under_person/first_company_or_right_thing/Way.java and Good_group.java by high eye

---
+++ pretend/have_own_number_under_person/first_company_or_right_thing/Way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.have_own_number_under_person.first_company_or_right_thing;
+
+public class Way {
+    private int new_child_and_good_eye;
+    private int next_day;
+
+    public Way() {
+        new_child_and_good_eye = 294;
+        next_day = 751;
+    }
+
+    public void fact_and_public_hand() {
+        big_world_and_right_group();
+        System.out.println("problem_and_fact" + new_child_and_good_eye);
+    }
+
+    private void big_world_and_right_group() {
+        System.out.println("time" + next_day);
+    }
+}
