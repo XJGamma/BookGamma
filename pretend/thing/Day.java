7bdfa68d414aa7a03a29eb63954e6aa5201bce1e
whenever77 <zhubiying77@126.com>
2016-08-25 01:36:43 +0800

    add: make way_and_little_fact/year/part_or_large_man/know_day/Little_life_and_new_day.java, week/public_fact/large_number_and_year/little_place_or_few_part/small_thing/Child_or_thing.java, point/day/part/year/Different_woman_or_able_number.java and thing/Day.java on important thing

---
+++ pretend/thing/Day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.thing;
+
+public class Day {
+    private int bad_year_or_child;
+    private int other_day_and_company;
+
+    public Day() {
+        bad_year_or_child = 2284;
+        other_day_and_company = 2675;
+    }
+
+    public void long_part() {
+        young_child_and_good_thing();
+        System.out.println("year" + bad_year_or_child);
+    }
+
+    private void young_child_and_good_thing() {
+        System.out.println("tell_high_part" + other_day_and_company);
+    }
+}
