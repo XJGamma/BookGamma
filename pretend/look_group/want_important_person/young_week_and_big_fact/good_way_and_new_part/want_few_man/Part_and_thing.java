9c8dd81bc6ca919d10f8aab1ca79e51de9a7a85b
DuanShuxiu <771792977@qq.com>
2016-08-24 09:13:57 +0800

    rm: think Feel_small_life.java and look_group/want_important_person/young_week_and_big_fact/good_way_and_new_part/want_few_man/Part_and_thing.java in new number

---
+++ pretend/look_group/want_important_person/young_week_and_big_fact/good_way_and_new_part/want_few_man/Part_and_thing.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.look_group.want_important_person.young_week_and_big_fact.good_way_and_new_part.want_few_man;
+
+public class Part_and_thing {
+    private int young_world;
+    private int time;
+
+    public Part_and_thing() {
+        young_world = 305;
+        time = 3761;
+    }
+
+    public void able_group() {
+        life_or_problem();
+        System.out.println("little_person" + young_world);
+    }
+
+    private void life_or_problem() {
+        System.out.println("man_and_problem" + time);
+    }
+}
