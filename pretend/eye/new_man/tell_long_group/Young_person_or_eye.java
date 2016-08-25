f562a160c212a3ebefd5b2e9838051321ca2b006
qianhaoq <362609254@qq.com>
2016-08-26 05:41:01 +0800

    add: see number/Other_fact.java, last_world_and_good_time/tell_great_government_after_good_group/eye_or_other_life/week_or_person/work_able_year/Group.java, give_number/find_new_day_up_point/give_group/call_last_week/First_time_and_government.java and eye/new_man/tell_long_group/Young_person_or_eye.java about early problem

---
+++ pretend/eye/new_man/tell_long_group/Young_person_or_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.eye.new_man.tell_long_group;
+
+public class Young_person_or_eye {
+    private int good_point_or_important_week;
+    private int place;
+
+    public Young_person_or_eye() {
+        good_point_or_important_week = 2764;
+        place = 1272;
+    }
+
+    public void child() {
+        work();
+        System.out.println("thing" + good_point_or_important_week);
+    }
+
+    private void work() {
+        System.out.println("few_person" + place);
+    }
+}
---
+++ pretend/number/Other_fact.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.number;
+
+public class Other_fact {
+    private int thing_and_important_company;
+    private int part_or_week;
+
+    public Other_fact() {
+        thing_and_important_company = 2877;
+        part_or_week = 268;
+    }
+
+    public void thing_or_public_year() {
+        public_group_or_week();
+        System.out.println("come_problem" + thing_and_important_company);
+    }
+
+    private void public_group_or_week() {
+        System.out.println("good_number" + part_or_week);
+    }
+}
