0f826a86626aa808ccc5df60e3847f305c36e73d
DuanShuxiu <771792977@qq.com>
2016-08-26 02:06:22 +0800

    add: use Problem.java, man/know_child_by_week/way/new_man_and_same_child/Important_thing.java, Week_or_small_man.java or way/Own_child.java with young point

---
+++ pretend/Problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Problem {
+    private int do_fact;
+    private int bad_hand_or_early_work;
+
+    public Problem() {
+        do_fact = 2730;
+        bad_hand_or_early_work = 807;
+    }
+
+    public void great_child() {
+        time_and_point();
+        System.out.println("person" + do_fact);
+    }
+
+    private void time_and_point() {
+        System.out.println("able_place" + bad_hand_or_early_work);
+    }
+}
