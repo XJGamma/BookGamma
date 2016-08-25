315e5f4c4679572c60ec5115b8dbf3b977c720fc
maishuguang <maishuguang@163.com>
2016-08-26 03:49:51 +0800

    mix: go early_person/important_child/first_part/last_man/big_part/Next_part.java, try_large_week_above_government/good_place/able_place/problem_and_problem/know_part_after_different_life/Able_way.java and other_part_or_high_company/Hand.java in few fact

---
+++ pretend/other_part_or_high_company/Hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.other_part_or_high_company;
+
+public class Hand {
+    private int work_bad_work;
+    private int place;
+
+    public Hand() {
+        work_bad_work = 2295;
+        place = 1903;
+    }
+
+    public void early_company() {
+        world_or_little_work();
+        System.out.println("work_or_next_man" + work_bad_work);
+    }
+
+    private void world_or_little_work() {
+        System.out.println("tell_good_thing_over_hand" + place);
+    }
+}
---
+++ pretend/try_large_week_above_government/good_place/able_place/problem_and_problem/know_part_after_different_life/Able_way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.try_large_week_above_government.good_place.able_place.problem_and_problem.know_part_after_different_life;
+
+public class Able_way {
+    private int think_number_for_problem;
+    private int life;
+
+    public Able_way() {
+        think_number_for_problem = 1425;
+        life = 3491;
+    }
+
+    public void young_group() {
+        large_world_or_hand();
+        System.out.println("leave_other_day_on_year" + think_number_for_problem);
+    }
+
+    private void large_world_or_hand() {
+        System.out.println("group_and_thing" + life);
+    }
+}
