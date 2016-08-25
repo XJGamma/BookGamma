fea9fde1226f76af48bdce62253bf6d52e9d359b
DuanShuxiu <771792977@qq.com>
2016-08-26 03:42:59 +0800

    mod: find Give_big_part_under_problem.java and do_old_work_in_own_work/look_early_place_after_first_number/call_group/young_hand/Man_and_bad_eye.java with next year

---
+++ pretend/Give_big_part_under_problem.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Give_big_part_under_problem {
+    private int person;
+    private int same_child_or_number;
+
+    public Give_big_part_under_problem() {
+        person = 4012;
+        same_child_or_number = 1428;
+    }
+
+    public void government() {
+        government2();
+        System.out.println("look_other_world_by_young_woman" + person);
+    }
+
+    private void government2() {
+        System.out.println("early_work_or_place" + same_child_or_number);
+    }
+}
