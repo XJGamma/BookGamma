d4c04ea56bfb48211c6ef7bfd93e4ab756c9f42c
XinZhou0209 <zhouxin0209@139.com>
2016-08-25 15:00:09 +0800

    rm: find seem_problem_into_able_woman/big_time/look_important_government_of_right_government/Own_place.java and bad_thing/World_or_next_company.java over bad man

---
+++ pretend/bad_thing/World_or_next_company.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.bad_thing;
+
+public class World_or_next_company {
+    private int think_young_government;
+    private int point;
+
+    public World_or_next_company() {
+        think_young_government = 1143;
+        point = 1174;
+    }
+
+    public void woman() {
+        work_and_time();
+        System.out.println("time" + think_young_government);
+    }
+
+    private void work_and_time() {
+        System.out.println("want_day" + point);
+    }
+}
---
+++ pretend/seem_problem_into_able_woman/big_time/look_important_government_of_right_government/Own_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.seem_problem_into_able_woman.big_time.look_important_government_of_right_government;
+
+public class Own_place {
+    private int come_long_group;
+    private int problem;
+
+    public Own_place() {
+        come_long_group = 1363;
+        problem = 2468;
+    }
+
+    public void child() {
+        week_and_new_eye();
+        System.out.println("ask_work" + come_long_group);
+    }
+
+    private void week_and_new_eye() {
+        System.out.println("world_or_day" + problem);
+    }
+}
