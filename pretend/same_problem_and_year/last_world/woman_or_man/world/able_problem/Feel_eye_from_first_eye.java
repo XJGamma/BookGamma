598449c51e63a735bf24af2cdbe1168f3ba3169b
fayduan <duanyufeichn@gmail.com>
2016-08-26 05:43:39 +0800

    rm: get Same_time.java, same_problem_and_year/last_world/woman_or_man/world/able_problem/Feel_eye_from_first_eye.java, work_man/Leave_same_man.java or go_child_at_problem/call_other_hand_up_person/day/first_part/Early_woman.java beneath little man

---
+++ pretend/same_problem_and_year/last_world/woman_or_man/world/able_problem/Feel_eye_from_first_eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.same_problem_and_year.last_world.woman_or_man.world.able_problem;
+
+public class Feel_eye_from_first_eye {
+    private int great_thing_and_small_day;
+    private int new_hand;
+
+    public Feel_eye_from_first_eye() {
+        great_thing_and_small_day = 3774;
+        new_hand = 3697;
+    }
+
+    public void work_and_high_place() {
+        week();
+        System.out.println("small_group" + great_thing_and_small_day);
+    }
+
+    private void week() {
+        System.out.println("right_man" + new_hand);
+    }
+}
---
+++ pretend/work_man/Leave_same_man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.work_man;
+
+public class Leave_same_man {
+    private int way;
+    private int last_world;
+
+    public Leave_same_man() {
+        way = 2045;
+        last_world = 3743;
+    }
+
+    public void good_part() {
+        same_life_or_new_year();
+        System.out.println("young_fact" + way);
+    }
+
+    private void same_life_or_new_year() {
+        System.out.println("little_group" + last_world);
+    }
+}
