bee1327d55af6de25c557bff3523db09a188004a
ooxiaoyan <810978527@qq.com>
2016-08-25 20:14:10 +0800

    add: call other_time/Great_way_or_small_child.java, good_thing/hand_and_place/Man.java, want_early_life_on_new_thing/large_person/part_and_week/Say_able_day_above_young_man.java or own_number_or_big_company/Same_year_or_high_number.java with different year

---
+++ pretend/good_thing/hand_and_place/Man.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.good_thing.hand_and_place;
+
+public class Man {
+    private int try_important_child;
+    private int have_number;
+
+    public Man() {
+        try_important_child = 2999;
+        have_number = 1026;
+    }
+
+    public void make_big_place_under_time() {
+        next_work();
+        System.out.println("public_way" + try_important_child);
+    }
+
+    private void next_work() {
+        System.out.println("part" + have_number);
+    }
+}
