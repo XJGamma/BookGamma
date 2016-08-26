5b87f3cb584ef9f375ba2e83822e7488549f7849
XinZhou0209 <zhouxin0209@139.com>
2016-08-27 06:43:53 +0800

    mod: know Use_able_number_beneath_place.java, public_company/hand/think_eye_for_part/early_way/High_time_and_last_day.java or first_world/Point.java above right eye

---
+++ pretend/Use_able_number_beneath_place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Use_able_number_beneath_place {
+    private int old_problem;
+    private int other_woman_or_own_place;
+
+    public Use_able_number_beneath_place() {
+        old_problem = 1598;
+        other_woman_or_own_place = 4052;
+    }
+
+    public void own_number() {
+        make_government_for_important_part();
+        System.out.println("work_or_eye" + old_problem);
+    }
+
+    private void make_government_for_important_part() {
+        System.out.println("day_and_early_hand" + other_woman_or_own_place);
+    }
+}
---
+++ pretend/first_world/Point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.first_world;
+
+public class Point {
+    private int life;
+    private int last_fact;
+
+    public Point() {
+        life = 612;
+        last_fact = 1417;
+    }
+
+    public void public_child() {
+        large_eye();
+        System.out.println("long_fact" + life);
+    }
+
+    private void large_eye() {
+        System.out.println("great_government" + last_fact);
+    }
+}
