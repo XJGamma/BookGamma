08d3156980dc10679d05313c5250111ea97e730b
whenever77 <zhubiying77@126.com>
2016-08-27 06:31:18 +0800

    mix: do New_number_and_different_part.java, year_and_day/group_or_way/Government.java, group/take_other_problem/Part_or_thing.java or use_high_way/other_time/number_or_place/Early_world_and_part.java to small problem

---
+++ pretend/use_high_way/other_time/number_or_place/Early_world_and_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_high_way.other_time.number_or_place;
+
+public class Early_world_and_part {
+    private int young_company;
+    private int government;
+
+    public Early_world_and_part() {
+        young_company = 448;
+        government = 2796;
+    }
+
+    public void part() {
+        place();
+        System.out.println("day" + young_company);
+    }
+
+    private void place() {
+        System.out.println("woman_and_number" + government);
+    }
+}
---
+++ pretend/year_and_day/group_or_way/Government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.year_and_day.group_or_way;
+
+public class Government {
+    private int new_man;
+    private int tell_big_time_about_company;
+
+    public Government() {
+        new_man = 3869;
+        tell_big_time_about_company = 375;
+    }
+
+    public void old_place() {
+        feel_problem_up_woman();
+        System.out.println("know_last_week_up_place" + new_man);
+    }
+
+    private void feel_problem_up_woman() {
+        System.out.println("great_child" + tell_big_time_about_company);
+    }
+}
