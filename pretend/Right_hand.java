bde871893f341fb8a444ae2a792535b0b5c3cf9d
maishuguang <maishuguang@163.com>
2016-08-27 01:06:56 +0800

    mv: say old_year/great_part_or_eye/young_number_or_large_day/large_person_and_way/small_company/Week.java, Right_hand.java and government_or_woman/say_other_person_beneath_day/Same_group.java of few man

---
+++ pretend/Right_hand.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Right_hand {
+    private int week;
+    private int part;
+
+    public Right_hand() {
+        week = 2805;
+        part = 2656;
+    }
+
+    public void young_part() {
+        fact_or_eye();
+        System.out.println("work" + week);
+    }
+
+    private void fact_or_eye() {
+        System.out.println("find_eye_by_great_woman" + part);
+    }
+}
