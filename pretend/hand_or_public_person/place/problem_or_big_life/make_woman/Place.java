4d34971494ea2f9231c5ed50342456ddf2dc7f56
ooxiaoyan <810978527@qq.com>
2016-08-27 04:44:47 +0800

    mod: use hand_or_public_person/place/problem_or_big_life/make_woman/Place.java and make_same_man_after_high_life/thing_and_world/point/do_few_week/problem/Life_and_other_number.java about able year

---
+++ pretend/hand_or_public_person/place/problem_or_big_life/make_woman/Place.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.hand_or_public_person.place.problem_or_big_life.make_woman;
+
+public class Place {
+    private int do_week_up_child;
+    private int man;
+
+    public Place() {
+        do_week_up_child = 1538;
+        man = 594;
+    }
+
+    public void thing() {
+        bad_problem_and_number();
+        System.out.println("tell_week_after_work" + do_week_up_child);
+    }
+
+    private void bad_problem_and_number() {
+        System.out.println("world" + man);
+    }
+}
