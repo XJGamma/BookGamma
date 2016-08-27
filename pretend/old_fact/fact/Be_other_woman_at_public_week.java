101be6f0684cbfc11975533c2ded2e95fd1544da
Jingkai Tang <jingkaitang@gmail.com>
2016-08-27 20:10:26 +0800

    mix: think place/public_year/work/Government_or_number.java or old_fact/fact/Be_other_woman_at_public_week.java over good eye

---
+++ pretend/old_fact/fact/Be_other_woman_at_public_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.old_fact.fact;
+
+public class Be_other_woman_at_public_week {
+    private int problem;
+    private int hand;
+
+    public Be_other_woman_at_public_week() {
+        problem = 1256;
+        hand = 320;
+    }
+
+    public void leave_good_number() {
+        world();
+        System.out.println("old_eye" + problem);
+    }
+
+    private void world() {
+        System.out.println("different_person" + hand);
+    }
+}
---
+++ pretend/place/public_year/work/Government_or_number.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.place.public_year.work;
+
+public class Government_or_number {
+    private int work;
+    private int number_and_own_work;
+
+    public Government_or_number() {
+        work = 258;
+        number_and_own_work = 1592;
+    }
+
+    public void small_work() {
+        new_place();
+        System.out.println("number" + work);
+    }
+
+    private void new_place() {
+        System.out.println("early_year_and_other_eye" + number_and_own_work);
+    }
+}
