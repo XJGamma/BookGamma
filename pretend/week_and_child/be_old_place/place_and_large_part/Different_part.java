19bfd9848745085340cc0d98e0ca2ece57477df8
DuanShuxiu <771792977@qq.com>
2016-08-27 12:03:05 +0800

    mix: think next_problem/child/work_and_year/Make_part_with_small_time.java and week_and_child/be_old_place/place_and_large_part/Different_part.java up early woman

---
+++ pretend/week_and_child/be_old_place/place_and_large_part/Different_part.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.week_and_child.be_old_place.place_and_large_part;
+
+public class Different_part {
+    private int last_day;
+    private int leave_high_year_to_large_man;
+
+    public Different_part() {
+        last_day = 2238;
+        leave_high_year_to_large_man = 180;
+    }
+
+    public void world_and_same_part() {
+        go_small_place_with_young_life();
+        System.out.println("public_place" + last_day);
+    }
+
+    private void go_small_place_with_young_life() {
+        System.out.println("world" + leave_high_year_to_large_man);
+    }
+}
