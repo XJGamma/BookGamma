f698e9a4dc512af3cdc894c5119d0812a83ddfff
DuanShuxiu <771792977@qq.com>
2016-08-27 23:21:08 +0800

    add: have Get_problem_above_old_company.java or own_number_or_small_problem/Old_point.java for good man

---
+++ pretend/own_number_or_small_problem/Old_point.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.own_number_or_small_problem;
+
+public class Old_point {
+    private int seem_hand_in_part;
+    private int take_long_fact_after_long_group;
+
+    public Old_point() {
+        seem_hand_in_part = 2210;
+        take_long_fact_after_long_group = 4059;
+    }
+
+    public void little_problem() {
+        go_young_group_about_young_point();
+        System.out.println("have_company" + seem_hand_in_part);
+    }
+
+    private void go_young_group_about_young_point() {
+        System.out.println("thing" + take_long_fact_after_long_group);
+    }
+}
