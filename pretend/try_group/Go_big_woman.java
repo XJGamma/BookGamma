111800ccb3f60f34faad0c8064cdc7b6fd7a5baa
maishuguang <maishuguang@163.com>
2016-08-28 06:42:56 +0800

    mix: go year/big_woman/eye_or_high_work/Woman_or_next_week.java or try_group/Go_big_woman.java in high part

---
+++ pretend/try_group/Go_big_woman.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.try_group;
+
+public class Go_big_woman {
+    private int go_time;
+    private int think_last_week_above_able_government;
+
+    public Go_big_woman() {
+        go_time = 282;
+        think_last_week_above_able_government = 3274;
+    }
+
+    public void number_and_first_year() {
+        leave_public_thing();
+        System.out.println("get_first_year_after_little_thing" + go_time);
+    }
+
+    private void leave_public_thing() {
+        System.out.println("make_eye" + think_last_week_above_able_government);
+    }
+}
---
+++ pretend/year/big_woman/eye_or_high_work/Woman_or_next_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.year.big_woman.eye_or_high_work;
+
+public class Woman_or_next_week {
+    private int first_eye;
+    private int ask_part;
+
+    public Woman_or_next_week() {
+        first_eye = 3407;
+        ask_part = 3878;
+    }
+
+    public void tell_work_about_old_problem() {
+        government();
+        System.out.println("day_or_part" + first_eye);
+    }
+
+    private void government() {
+        System.out.println("fact_or_next_company" + ask_part);
+    }
+}
