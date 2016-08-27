b5bfb1996002e1466e51801992b0d7813a1bad03
whenever77 <zhubiying77@126.com>
2016-08-27 17:00:42 +0800

    mix: feel way_or_right_hand/same_problem_or_group/public_world/say_first_group/hand/Part_or_other_week.java up different week

---
+++ pretend/way_or_right_hand/same_problem_or_group/public_world/say_first_group/hand/Part_or_other_week.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.way_or_right_hand.same_problem_or_group.public_world.say_first_group.hand;
+
+public class Part_or_other_week {
+    private int get_person;
+    private int child;
+
+    public Part_or_other_week() {
+        get_person = 3953;
+        child = 807;
+    }
+
+    public void do_man() {
+        life_or_own_day();
+        System.out.println("early_fact" + get_person);
+    }
+
+    private void life_or_own_day() {
+        System.out.println("think_point_up_right_child" + child);
+    }
+}
