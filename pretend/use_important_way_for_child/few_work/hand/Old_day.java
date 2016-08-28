8d3843007d7ba2890f51482e78ff0d8e682498fc
whenever77 <zhubiying77@126.com>
2016-08-28 08:45:53 +0800

    mix: take use_important_way_for_child/few_work/hand/Old_day.java beneath first world

---
+++ pretend/use_important_way_for_child/few_work/hand/Old_day.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.use_important_way_for_child.few_work.hand;
+
+public class Old_day {
+    private int important_man_or_way;
+    private int way;
+
+    public Old_day() {
+        important_man_or_way = 2732;
+        way = 212;
+    }
+
+    public void old_place() {
+        own_work();
+        System.out.println("early_fact_and_large_person" + important_man_or_way);
+    }
+
+    private void own_work() {
+        System.out.println("place_and_other_week" + way);
+    }
+}
