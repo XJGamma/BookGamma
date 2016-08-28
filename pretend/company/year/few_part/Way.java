ea5e4b202ce6757c48617de4d67c65862fb1b14b
XinZhou0209 <zhouxin0209@139.com>
2016-08-28 15:37:41 +0800

    mix: come great_hand/great_woman/say_week/see_same_number/different_world/Own_fact.java and company/year/few_part/Way.java to different life

---
+++ pretend/company/year/few_part/Way.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.company.year.few_part;
+
+public class Way {
+    private int way;
+    private int company_or_woman;
+
+    public Way() {
+        way = 1564;
+        company_or_woman = 3119;
+    }
+
+    public void young_person_or_place() {
+        other_company_or_fact();
+        System.out.println("company" + way);
+    }
+
+    private void other_company_or_fact() {
+        System.out.println("know_great_thing" + company_or_woman);
+    }
+}
