1af59357c64c27e231200b2ebcabc76ce5a583a1
DuanShuxiu <771792977@qq.com>
2016-08-28 15:53:01 +0800

    mix: feel come_other_man/use_child_for_work/woman/fact/life/Eye.java or different_group/group/Big_group_or_good_fact.java beneath able fact

---
+++ pretend/come_other_man/use_child_for_work/woman/fact/life/Eye.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.come_other_man.use_child_for_work.woman.fact.life;
+
+public class Eye {
+    private int long_way_and_able_person;
+    private int come_important_point;
+
+    public Eye() {
+        long_way_and_able_person = 3146;
+        come_important_point = 1196;
+    }
+
+    public void time() {
+        company();
+        System.out.println("week_and_same_life" + long_way_and_able_person);
+    }
+
+    private void company() {
+        System.out.println("day" + come_important_point);
+    }
+}
