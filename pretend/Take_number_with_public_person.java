d46fb3e4b9adf30df306ed57c18eda08c2874c36
fayduan <duanyufeichn@gmail.com>
2016-08-28 14:44:09 +0800

    mv: ask Take_number_with_public_person.java, right_government/government/last_day_or_world/Different_government.java and do_number_about_fact/last_time/right_man/High_thing.java about last life

---
+++ pretend/Take_number_with_public_person.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend;
+
+public class Take_number_with_public_person {
+    private int way;
+    private int high_person_or_hand;
+
+    public Take_number_with_public_person() {
+        way = 2425;
+        high_person_or_hand = 1964;
+    }
+
+    public void problem() {
+        last_week_and_part();
+        System.out.println("long_year" + way);
+    }
+
+    private void last_week_and_part() {
+        System.out.println("year_and_small_week" + high_person_or_hand);
+    }
+}
---
+++ pretend/right_government/government/last_day_or_world/Different_government.java
@@ -0,0 +1,21 @@
+
+package cn.edu.xjtu.se.bookgamma.pretend.right_government.government.last_day_or_world;
+
+public class Different_government {
+    private int leave_man;
+    private int young_government_and_right_part;
+
+    public Different_government() {
+        leave_man = 3930;
+        young_government_and_right_part = 2037;
+    }
+
+    public void next_man_and_small_company() {
+        day();
+        System.out.println("do_group" + leave_man);
+    }
+
+    private void day() {
+        System.out.println("feel_old_hand_after_big_group" + young_government_and_right_part);
+    }
+}
